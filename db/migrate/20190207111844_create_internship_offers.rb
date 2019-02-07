cffd4f2e6b8fd79d80abced3f47bd7924fa9325a
Brice <brice.durand2@gmail.com>
2019-02-07 15:18:53 +0100

    add internship offer model

---
+++ db/migrate/20190207111844_create_internship_offers.rb
@@ -0,0 +1,25 @@
+class CreateInternshipOffers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :internship_offers do |t|
+      t.string  :title, null: false
+      t.text    :description, null: false
+      t.string  :sector
+      t.boolean :can_be_applied_for, default: true
+      t.date    :week_day_start
+      t.date    :week_day_end
+      t.date    :excluded_weeks, array: true
+      t.integer :max_candidates
+      t.integer :max_weeks
+
+      t.string  :tutor_name
+      t.string  :tutor_phone
+      t.string  :tutor_email
+      t.string  :employer_website
+      t.text    :employer_description
+      t.string  :supervisor_email
+      t.boolean :is_public
+
+      t.timestamps
+    end
+  end
+end
