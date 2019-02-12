2ff021b062f1b2e61a9f83234418d354a391b8ba
Brice <brice.durand2@gmail.com>
2019-02-12 12:18:42 +0100

    Add some validations for required fields on server and client side

--- app/models/internship_offer.rb
+++ app/models/internship_offer.rb
@@ -1,5 +1,7 @@
 class InternshipOffer < ApplicationRecord
 
+  validates :title, :description, :sector, :can_be_applied_for, :tutor_name, :tutor_phone, :supervisor_email, presence: true
+
   def available_all_year?
     week_day_start.blank? && week_day_end.blank?
   end
