1467ce09ae39482f109572183cfacc5e84cb1fd1
Martin <martin@sharypic.com>
2019-02-15 14:08:21 +0100

    tech(w3c): start validating existing routes

--- test/w3c/home_test.rb
+++ test/w3c/home_test.rb
@@ -21,9 +21,27 @@ class HomeValidationTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "the truth" do
-    w3c_validate!(report_as: 'home') do
-      get "/"
+  test 'root_path' do
+    w3c_validate!(report_as: 'root_path') do
+      get root_path
     end
   end
+
+  test 'internship_offers_path'  do
+    w3c_validate!(report_as: 'internship_offers_path') do
+      get internship_offers_path
+    end
+  end
+
+  test 'new_internship_offer_path'  do
+    w3c_validate!(report_as: 'new_internship_offer_path') do
+      get new_internship_offer_path
+    end
+  end
+
+  # test 'edit_internship_offer_path'  do
+  #   w3c_validate!(report_as: 'edit_internship_offer_path') do
+  #     get edit_internship_offer_path(internship_offers(:one))
+  #   end
+  # end
 end
