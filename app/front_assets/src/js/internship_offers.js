ca5d23895815a5eb35d7effbab3c31b9739f3530
Brice <brice.durand2@gmail.com>
2019-02-15 17:44:14 +0100

    Disable some fields depending on context

--- app/front_assets/src/js/internship_offers.js
+++ app/front_assets/src/js/internship_offers.js
@@ -9,5 +9,10 @@ $(document).on('turbolinks:load', function() {
     $('#internship_offer_can_be_applied_for_false').change(function() {
         toggleMaxCandidatesVisibility();
     });
+
+    $('#all_year_long').change(function () {
+        $('#internship_offer_week_ids').prop('disabled', function(i, v) { return !v; });
+        $('#excluded_week_ids').prop('disabled', function(i, v) { return !v; });
+    });
 });
 
