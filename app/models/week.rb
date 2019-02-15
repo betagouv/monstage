cc172400380bcc0dbd3d3469cc76244f35e1da38
Brice <brice.durand2@gmail.com>
2019-02-15 15:14:20 +0100

    wip refactoring

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,4 +1,12 @@
 class Week < ApplicationRecord
   has_many :internship_offer_weeks
   has_many :internship_offers, through: :internship_offer_weeks
+    # .where(number: Date.today.cweek..Date.new(current_year, 5, 1).cweek)
+  # scope :window_from_january_until_may, -> {
+  #   where(year: Date.now.year)
+  # }
+  #
+  # scope :window_from_septembre_until_may_next_year, -> {
+  #   where(year: Date.now.year).where(number: Date.new(current_year, 9, 1).cweek..Date.new(current_year+1, 5, 1).cweek)
+  # }
 end
