aea0da73133c37468320494d6d636ddcb2f92ce4
Brice <brice.durand2@gmail.com>
2019-02-15 12:28:09 +0100

    Put proper content in week select boxes when publishing an offer

--- app/controllers/internship_offers_controller.rb
+++ app/controllers/internship_offers_controller.rb
@@ -24,8 +24,21 @@ class InternshipOffersController < ApplicationController
   def new
     @internship_offer = InternshipOffer.new
 
-    @current_year = Date.today.year
+    today = Date.today
+    current_year = today.year
+    current_month = today.month
 
+    if current_month < 5 # Before May, offers are valid for the current year
+      first_of_may_week_number = Date.new(current_year, 5, 1).cweek
+      @current_weeks = Week.where("year = ? AND week > ? AND week <= ?", current_year, today.cweek, first_of_may_week_number)
+    else # After May, offers are valid for next year
+      first_week = if current_month < 9 # Between May and September, the first week should be the first week of september
+                     Date.new(current_year, 9, 1).cweek
+                   else # otherwise it is from the current week
+                     today.cweek
+                   end
+      @current_weeks = Week.where("year = ? AND week > ?", current_year, first_week).or(Week.where("year = ? AND week < ?", current_year + 1, Date.new(current_year+1, 5, 1).cweek))
+    end
 
     @sectors = ['Aérien, Aéronautique et Aéroportuaire',
                 "Agriculture, Elevage, Pêche",
@@ -82,6 +95,7 @@ class InternshipOffersController < ApplicationController
     params.require(:internship_offer)
         .permit(:title, :description, :sector, :can_be_applied_for, :week_day_start, :week_day_end, :excluded_weeks,
                 :max_candidates, :max_weeks, :tutor_name, :tutor_phone, :tutor_email, :employer_website,
-                :employer_description, :employer_street, :employer_zipcode, :employer_city, :supervisor_email, :is_public)
+                :employer_description, :employer_street, :employer_zipcode, :employer_city, :supervisor_email, :is_public,
+                week_ids: [])
   end
 end
