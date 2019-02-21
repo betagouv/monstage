3e60fa2bf279c14c1f72b6192c25074d64b4ea63
Martin <martin@sharypic.com>
2019-02-21 11:00:25 +0100

    fix(mock): missing yield

--- test/support/session_manager_test_helper.rb
+++ test/support/session_manager_test_helper.rb
@@ -1,10 +1,9 @@
 module SessionManagerTestHelper
   def sign_in(as:)
     mock = Minitest::Mock.new()
-    mock.expect :current_user, as
+    mock.expect :change_or_restore_current_user, as
     SessionManager.stub :new, mock do
-      session_manager = SessionManager.new
-      session_manager.current_user
+      yield
     end
   end
 end
