2e11a90c0c5b2660af977ae5e87d142666218227
Martin <martin@sharypic.com>
2019-02-21 10:09:41 +0100

    tech(MiniTest): use SpecReporter for more verbose test outpout

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
+  gem 'minitest-reporters'
   gem 'capybara'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require_relative './support/session_manager_test_helper'
+require "minitest/reporters"
 require 'minitest/autorun'
 require 'rails/test_help'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
