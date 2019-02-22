44a323e6850c06900f5660cf47a85fa049a775f9
Brice <brice.durand2@gmail.com>
2019-02-22 10:09:44 +0100

    Fix field name in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ populate_week_reference
 
 def populate_operators
   ["Clubs régionaux  d'entreprises pour l'insertion (CREPI)", "Dégun sans stage (Ecole centrale de Marseille)", "Fondation Agir contre l'Exclusion (FACE)", "JOB IRL", "Les entreprises pour la cité (LEPC)", "Un stage et après !", "Tous en stage", "Viens voir mon taf"].each do |operator|
-    User.find_or_create_by(operator: operator)
+    User.find_or_create_by(operator_name: operator)
   end
 end
 
