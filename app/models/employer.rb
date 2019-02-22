61beae19f810cff87dd002147dad769a45a0bbb4
Brice <brice.durand2@gmail.com>
2019-02-22 14:46:58 +0100

    Add user types

---
+++ app/models/employer.rb
@@ -0,0 +1,3 @@
+class Employer < User
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_092730) do
+ActiveRecord::Schema.define(version: 2019_02_22_105911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_02_21_092730) do
     t.string "operator_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "weeks", force: :cascade do |t|
