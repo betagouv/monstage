7dae5ae9de554ab9e0f596a869ba9988ac6a9327
Martin <martin@sharypic.com>
2019-02-22 10:00:03 +0100

    tech(school): add school model

--- db/migrate/20190222073540_create_schools_table.rb
+++ db/migrate/20190222073540_create_schools_table.rb
@@ -3,8 +3,9 @@ class CreateSchoolsTable < ActiveRecord::Migration[5.2]
     create_table :schools_tables do |t|
       t.string :name
       t.string :city
-      t.string :region
+      t.string :departement
       t.string :postal_code
+      t.string :code_uai
       t.st_point :coordinates, geographic: true
       t.index :coordinates, using: :gist
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
-FIRST_YEAR = 2019
-LAST_YEAR = 2050
-
-FIRST_WEEK = 1
-LAST_WEEK = 53 # A 53 week exisits! https://fr.wikipedia.org/wiki/Semaine_53
+require 'csv'
 
 def populate_week_reference
-  FIRST_YEAR.upto(LAST_YEAR) do |year|
-    FIRST_WEEK.upto(LAST_WEEK) do |week| # number of the week
+  first_year = 2019
+  last_year = 2050
+
+  first_week = 1
+  last_week = 53 # A 53 week exisits! https://fr.wikipedia.org/wiki/Semaine_53
+
+  first_year.upto(last_year) do |year|
+    first_week.upto(last_week) do |week| # number of the week
       begin
-        if week == LAST_WEEK
+        if week == last_week
           Date.commercial(year, week, 1)
           puts "Special year #{year}, this one have 53 weeks"
         end
@@ -22,13 +24,22 @@ def populate_week_reference
     end
   end
 end
-
-populate_week_reference
-
 def populate_operators
   ["Clubs régionaux  d'entreprises pour l'insertion (CREPI)", "Dégun sans stage (Ecole centrale de Marseille)", "Fondation Agir contre l'Exclusion (FACE)", "JOB IRL", "Les entreprises pour la cité (LEPC)", "Un stage et après !", "Tous en stage", "Viens voir mon taf"].each do |operator|
     User.find_or_create_by(operator_name: operator)
   end
 end
 
+def populate_schools
+  CSV.foreach(Rails.root.join('db/college-rep-plus.csv'), {headers: { col_sep: ','}}) do |row, i|
+    puts row["Département"].inspect
+    puts row["Commune"].inspect
+    puts row["Code UAI"].inspect
+    puts row["ETABLISSEMENT"].inspect
+    puts i.inspect
+  end
+end
+
+populate_week_reference
 populate_operators
+populate_schools
