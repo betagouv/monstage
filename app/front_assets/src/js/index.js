d35a5796ae4df8e369d6dd622a57fbb8766f2d6d
Martin <martin@sharypic.com>
2019-02-23 14:39:46 +0100

    fix(typo): importing credentials files, stimulus handles erb nicely

--- app/front_assets/src/js/index.js
+++ app/front_assets/src/js/index.js
@@ -2,7 +2,6 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-import AlgoliaCredentials from './algolia_credentials.js.erb';
 
 Rails.start();
 Turbolinks.start();
