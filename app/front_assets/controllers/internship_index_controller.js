05ee47d9e9137a61d85fb2ee8fbf42aad649f5ad
Brice <brice.durand2@gmail.com>
2019-02-27 16:21:55 +0100

    Add filter depending on selected sector

--- app/front_assets/controllers/internship_index_controller.js
+++ app/front_assets/controllers/internship_index_controller.js
@@ -3,7 +3,18 @@ import Turbolinks from 'turbolinks';
 
 // should be a link, but have to check with Brice why ...
 export default class extends Controller {
-  visit() {
-    Turbolinks.visit(this.data.get('href'))
+  static targets = [ "offer" ];
+  visit(event) {
+    Turbolinks.visit(event.currentTarget.dataset["internshipIndexHref"])
   }
+
+  filterOffersBySectors(event) {
+    let sector = event.target.options[event.target.selectedIndex].value;
+
+    $(this.offerTargets).each(function (index, offer) {
+      let shouldBeHidden = sector !== "" && $(offer).data('sector') !== sector;
+      $(offer).toggleClass('d-none', shouldBeHidden);
+    });
+  }
+
 }
