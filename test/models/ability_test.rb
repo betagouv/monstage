eaf5b8d79ad8e26272a8baa2a05aa7903c94fa00
Brice <brice.durand2@gmail.com>
2019-03-01 09:52:08 +0100

    fix ability tests

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AbilityTest < ActiveSupport::TestCase
   test "MockUser::Visitor" do
-    ability = Ability.new(MockUser::Student)
+    ability = Ability.new(MockUser::Visitor)
     assert(ability.can?(:read, InternshipOffer.new),
            'visitors should be able to consult internships')
     assert(ability.cannot?(:manage, InternshipOffer.new),
@@ -10,13 +10,13 @@ class AbilityTest < ActiveSupport::TestCase
   end
 
   test "MockUser::Student" do
-    ability = Ability.new(MockUser::Visitor)
+    ability = Ability.new(MockUser::Student)
     assert(ability.can?(:read, InternshipOffer.new),
-           'visitors should be able to consult internship offers')
+           'students should be able to consult internship offers')
     assert(ability.can?(:apply, InternshipOffer.new),
-           'visitors should be able to apply for internship offers')
+           'students should be able to apply for internship offers')
     assert(ability.cannot?(:manage, InternshipOffer.new),
-           'visitors should not be able to con manage internships')
+           'students should not be able to con manage internships')
   end
 
   test "MockUser::Employer" do
@@ -25,7 +25,7 @@ class AbilityTest < ActiveSupport::TestCase
            'employers should be able to create internships')
     assert(ability.can?(:update, InternshipOffer.new),
            'employers should be able to update internships')
-    assert(ability.can?(:desctroy, InternshipOffer.new),
+    assert(ability.can?(:destroy, InternshipOffer.new),
            'employers should be able to destroy internships')
   end
 end
