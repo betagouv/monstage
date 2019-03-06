6875d57ba3998293fe66caa63106cb2752478e37
Brice <brice.durand2@gmail.com>
2019-03-06 18:02:38 +0100

    Make sure that a school manager's email is like @ac-

--- app/models/school_manager.rb
+++ app/models/school_manager.rb
@@ -1,3 +1,3 @@
 class SchoolManager < User
-
+  validates :email, format: /\A[^@\s]+@ac-[^@\s]+\z/
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "School manager creation" do
+    school_manager = SchoolManager.create(email: 'chef@etablissement.com', password: 'tototo', password_confirmation: 'tototo')
+
+    assert school_manager.invalid?
+    assert_not_empty school_manager.errors[:email]
+
+    school_manager = SchoolManager.create(email: 'chef@ac-etablissement.com', password: 'tototo', password_confirmation: 'tototo')
+
+    assert school_manager.valid?
+  end
 end
