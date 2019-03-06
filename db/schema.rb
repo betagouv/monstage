476e8663f461b07e43fe5af230934d695c4411fc
Martin <martin@sharypic.com>
2019-03-06 16:39:40 +0100

    fix(migration): rename migration adding school_id to users otherwise we lose school id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_110022) do
+ActiveRecord::Schema.define(version: 2019_03_06_110023) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -85,10 +86,10 @@ ActiveRecord::Schema.define(version: 2019_03_06_110022) do
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "school_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.bigint "school_id"
     t.index ["school_id"], name: "index_users_on_school_id"
   end
 
