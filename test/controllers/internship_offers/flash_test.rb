156e465fb7a31ea158064324e07bd0c0bb50b72a
Martin <martin@sharypic.com>
2019-03-06 16:55:15 +0100

    fix(test/internship_offers#flash): avoid checking text which failed due to addition of bootstrap close button [changer innerText]

--- test/controllers/internship_offers/flash_test.rb
+++ test/controllers/internship_offers/flash_test.rb
@@ -7,9 +7,7 @@ module InternshipOffers
     test 'flash presence' do
       get new_internship_offer_path
       follow_redirect!
-      assert_select("#alert-danger",
-                    { text: "Vous n'êtes pas autorisé à créer une annonce" },
-                    'missing flash rendering')
+      assert_select("#alert-danger", {}, 'missing flash rendering')
     end
   end
 end
