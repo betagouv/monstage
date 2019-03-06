e259c8e83febbb385dbb0bc1b15d9c4e623d860f
Martin <martin@sharypic.com>
2019-03-06 16:43:56 +0100

    fix(fixtures.users): add email / password to fixtures otherwise we create invalid users

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :user do
     first_name { 'Jean Claude' }
     last_name { 'Dus' }
+    sequence(:email) {|n| "jean#{n}-claude@dus.fr" }
+    password { 'ooooyeahhhh' }
 
     factory :student, class: 'Student', parent: :user do
       type { 'Student' }
