c180baea3996fb8adee6bf2aacf0f5fd283b5af0
Martin <martin@sharypic.com>
2019-03-06 15:35:23 +0100

    feat(internship_controller#index): fetch nearby offers for student in school

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,10 @@
 class Student < User
   belongs_to :school, optional: true
+  delegate :coordinates, to: :school, allow_nil: true
+
+  def targeted_internship_offers
+    return InternshipOffer.all unless coordinates
+    InternshipOffer.nearby(latitude: coordinates.latitude,
+                           longitude: coordinates.longitude)
+  end
 end
