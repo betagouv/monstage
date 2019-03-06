83c80b61c9caf8cbe8c3a2f6a444325015fd7c69
Brice <brice.durand2@gmail.com>
2019-03-06 16:19:59 +0100

    Override RegistrationsController to be able to add type during signup

---
+++ app/models/school_manager.rb
@@ -0,0 +1,3 @@
+class SchoolManager < User
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+  }
   resources :internship_offers
 
   root to: "internship_offers#index"
