f3b2c165d23ccc5e88039e79028584cc69790dd8
Martin <martin@sharypic.com>
2019-03-07 18:02:11 +0100

    clean(fa5): use cdn version otherwise it fails on ci

--- app/front_assets/bootapp.js
+++ app/front_assets/bootapp.js
@@ -3,14 +3,10 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
-import { dom, library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
 
 Rails.start();
 Turbolinks.start();
 const application = Application.start()
 const context = require.context('controllers', true, /.js$/)
 application.load(definitionsFromContext(context))
-dom.watch()
-library.add(fas)
 
