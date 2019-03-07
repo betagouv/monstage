eff5006e54a83b72aae4339da7bddc46eaf3a056
Brice <brice.durand2@gmail.com>
2019-03-07 16:58:08 +0100

    move registration tests to account

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -8,18 +8,4 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
   test "GET new responds with success" do
     assert_response :success
   end
-
-  test "GET #new render list of schools" do
-    create(:school)
-    assert_select 'select[name="user[school_id]"] option', School.count
-  end
-
-  test "create school manager with school and weeks" do
-    school = create(:school)
-
-    post '/users', params: { user: { type: 'SchoolManager', school_id: school.id }, internship_weeks:[ weeks(:week_2019_1).id, weeks(:week_2019_2).id ]}
-
-    assert_not_empty school.weeks
-    assert_equal 2, school.weeks.count
-  end
 end
