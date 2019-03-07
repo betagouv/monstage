0ab9a763c54bf7ec4932b28f88f8745b2b99e929
Martin <martin@sharypic.com>
2019-03-07 18:20:34 +0100

    fix(registrations_controller_test): drop refactored method

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -5,10 +5,4 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
     get new_user_registration_path
     assert_response :success
   end
-
-  test 'GET #new render list of schools' do
-    get new_user_registration_path
-    create(:school)
-    assert_select 'select[name="user[school_id]"] option', School.count
-  end
 end
