f299df535691eaee6dfb8741430c5aad951a7727
Martin <martin@sharypic.com>
2019-03-08 18:12:24 +0100

    fix(account_controller#show): make it accessible for Employer

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,18 +19,21 @@ class Ability
   end
 
   def student_abilities(user:)
+    can :show, :account
     can :read, InternshipOffer
     can :apply, InternshipOffer
     can [:show, :edit, :update], User
   end
 
   def school_manager_abilities(user:)
+    can :show, :account
     can [:create, :new, :edit, :update], ClassRoom
     can [:show, :edit, :update], User
     can [:edit, :update], School
   end
 
   def employer_abilities(user:)
+    can :show, :account
     can [:create, :read, :update, :destroy], InternshipOffer
   end
 end
