ec24be3735e11ee2e7f079baba51a9512daf60fd
Brice <brice.durand2@gmail.com>
2019-03-13 15:13:26 +0100

    More tests

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -6,6 +6,22 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to users_choose_profile_path
   end
 
+  test 'GET choose_profile' do
+    get users_choose_profile_path
+
+    assert_select "a[href=?]", "/users/sign_up?as=Student"
+    assert_select "a[href=?]", "/users/sign_up?as=Employer"
+    assert_select "a[href=?]", "/users/sign_up?as=SchoolManager"
+  end
+
+  test 'GET new as a SchoolManager' do
+    get new_user_registration_path(as: 'SchoolManager')
+
+    assert_response :success
+    assert_select 'input', { value: 'SchoolManager', hidden: 'hidden' }
+    assert_select 'label', 'Courriel professionnel'
+  end
+
   test 'POST create School Manager responds with success' do
     assert_difference("SchoolManager.count") do
       post user_registration_path(params: { user: { email: 'test@ac-edu.fr',
