3be7745d87eb6baafab687857562d3ef92dfaacb
Brice <brice.durand2@gmail.com>
2019-03-19 11:38:52 +0100

    more testing

--- test/controllers/internship_applications_controller_test.rb
+++ test/controllers/internship_applications_controller_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class InternshipApplicationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "POST #create internship application" do
+    internship_offer = create(:internship_offer)
+    student = create(:student)
+
+    valid_params = { internship_application: { internship_offer_week_id: internship_offer.internship_offer_weeks.first.id,
+                                               motivation: 'Je suis trop motivé wesh',
+                                               user_id: student.id
+                                             }}
+    assert_difference('InternshipApplication.count', 1) do
+      post(internship_offer_internship_applications_path(internship_offer), params: valid_params)
+    end
+    assert_redirected_to internship_offers_path
+
+    created_internship_application = InternshipApplication.last
+    assert_equal internship_offer.internship_offer_weeks.first.id, created_internship_application.internship_offer_week.id
+    assert_equal 'Je suis trop motivé wesh', created_internship_application.motivation
+    assert_equal student.id, created_internship_application.student.id
+  end
+
+  test "POST #create internship application failed" do
+    internship_offer = create(:internship_offer)
+    student = create(:student)
+
+    valid_internship_application_params = { internship_offer_week_id: internship_offer.internship_offer_weeks.first.id,
+                                               motivation: 'Je suis trop motivé wesh',
+                                               user_id: student.id
+                                             }
+
+    assert_no_difference('InternshipApplication.count') do
+      post(internship_offer_internship_applications_path(internship_offer),
+           params: { internship_application: valid_internship_application_params.except(:motivation)})
+    end
+    assert_redirected_to internship_offer_path(internship_offer)
+
+    assert_no_difference('InternshipApplication.count') do
+      post(internship_offer_internship_applications_path(internship_offer),
+           params: { internship_application: valid_internship_application_params.except(:internship_offer_week_id)})
+    end
+    assert_redirected_to internship_offer_path(internship_offer)
+
+    assert_no_difference('InternshipApplication.count') do
+      post(internship_offer_internship_applications_path(internship_offer),
+           params: { internship_application: valid_internship_application_params.except(:user_id)})
+    end
+    assert_redirected_to internship_offer_path(internship_offer)
+  end
+
 end
