c084495feabf56476efd6e634a84009be711d5f5
Brice <brice.durand2@gmail.com>
2019-03-20 19:36:17 +0100

    Validate or refute offer from show page

--- app/controllers/internship_applications_controller.rb
+++ app/controllers/internship_applications_controller.rb
@@ -12,6 +12,18 @@ class InternshipApplicationsController < ApplicationController
     end
   end
 
+  def update
+    @internship_application = InternshipApplication.find(params[:id])
+    authorize! :update, @internship_application
+    if params[:approve] == 'true'
+      @internship_application.approve!
+    else
+      @internship_application.reject!
+    end
+
+    redirect_to @internship_application.internship_offer, flash: { success: 'Candidature mis à jour avec succès' }
+  end
+
   private
 
   def internship_application_params
