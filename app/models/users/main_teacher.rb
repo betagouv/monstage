d6b51bb82cb9fcdec7becf30355c890cbbec54a3
Martin <martin@sharypic.com>
2019-03-20 17:18:30 +0100

    feat(school_manager): prevent to create account when school does not have a school_manager, also prevent to change school to join a school without a school manager

--- app/models/users/main_teacher.rb
+++ app/models/users/main_teacher.rb
@@ -1,14 +1,23 @@
 module Users
   class MainTeacher < User
-    delegate :school_manager, to: :school
-    belongs_to :school, inverse_of: :main_teachers
-
+    belongs_to :school
+    has_one :school_manager, through: :school
     belongs_to :class_room, optional: true
 
     validates :first_name,
               :last_name,
               presence: true
+    validates :school_manager, presence: true,
+                               if: :school_id_changed?
+    validates :school_manager, presence: true,
+                               on: :create
+
+    before_update :notify_school_manager, if: :school_id_changed?
+    before_create :notify_school_manager, if: :school_id_changed?
 
     include NearbyIntershipOffersQueryable
+
+    def notify_school_manager
+    end
   end
 end
--- test/models/users/main_teacher_test.rb
+++ test/models/users/main_teacher_test.rb
@@ -9,16 +9,29 @@ module Users
       assert_includes errors, :first_name
       assert_includes errors, :last_name
       assert_includes errors, :email
+      assert_includes errors, :school_manager
     end
 
     test "creation succeed" do
+      school = create(:school)
+      school_manager = create(:school_manager, school: school)
       main_teacher = Users::MainTeacher.new(email: 'chef@ac-etablissement.com',
                                             password: 'tototo',
                                             password_confirmation: 'tototo',
                                             first_name: 'Chef',
                                             last_name: 'Etablissement',
-                                            school: build(:school))
+                                            school: school)
       assert main_teacher.valid?
+    end
+
+    test "change school requires school_manager" do
+      school_1 = create(:school)
+      school_2 = create(:school)
+      school_manager = create(:school_manager, school: school_1)
+      main_teacher = create(:main_teacher, school: school_1)
+      main_teacher.school = school_2
+      assert_not main_teacher.valid?
+      assert_includes main_teacher.errors.keys, :school_manager
     end
 
 
@@ -28,6 +41,11 @@ module Users
       main_teacher = create(:main_teacher, school: school)
 
       assert_equal main_teacher.school_manager, school_manager
+    end
+
+    test "i18n" do
+      assert_equal "Chef d'établissement",
+                   MainTeacher.human_attribute_name(:school_manager)
     end
   end
 end
