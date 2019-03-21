1633671f7a5fa67dadc0ee600da85ab6fd29960d
Brice <brice.durand2@gmail.com>
2019-03-21 16:36:05 +0100

    more testing

---
+++ test/mailers/student_mailer_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class StudentMailerTest < ActionMailer::TestCase
+  test "email sent when internship application is approved" do
+    internship_application = create(:internship_application)
+
+    email = StudentMailer.with(internship_application: internship_application).internship_application_approved_email
+
+    email.deliver_now
+    assert_emails 1
+    assert_equal [internship_application.internship_offer.employer.email], email.from
+    assert_equal [internship_application.student.email], email.to
+  end
+
+  test "email sent when internship application is declined" do
+    internship_application = create(:internship_application)
+
+    email = StudentMailer.with(internship_application: internship_application).internship_application_rejected_email
+
+    email.deliver_now
+    assert_emails 1
+    assert_equal ['ne-pas-repondre@monstagede3e.fr'], email.from
+    assert_equal [internship_application.student.email], email.to
+  end
+end
