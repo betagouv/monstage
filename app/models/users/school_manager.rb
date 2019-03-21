f88cc8f92c27e30bef533a6acb5b8e093e95a611
Martin <martin@sharypic.com>
2019-03-21 12:56:03 +0100

    feat(school_manager): replicate id on school table [for has_one school_manager through school on teacher/main_teachers classes]

--- app/models/users/school_manager.rb
+++ app/models/users/school_manager.rb
@@ -4,8 +4,15 @@ module Users
     has_one :school
     has_many :main_teachers, through: :school
 
+    before_save :replicate_school_id
 
     include NearbyIntershipOffersQueryable
+
+    def replicate_school_id
+      return unless school_id_changed?
+      School.where(id: school_id)
+            .update(school_manager_id: id)
+    end
 
     def after_sign_in_path
       return Rails.application.routes.url_helpers.account_path if school.blank? || school.weeks.empty?
