f38ea28a2cb088d1e459261c1b71ca01be1a53c7
Martin <martin@sharypic.com>
2019-03-22 09:32:26 +0100

    feat(internship#show): hide link to external website url when internship_offer.employer_website is absent

--- test/controllers/internship_offers/show_test.rb
+++ test/controllers/internship_offers/show_test.rb
@@ -11,5 +11,21 @@ module InternshipOffers
       assert_response :success
       assert_select "form[id=?]", "new_internship_application"
     end
+
+    test 'GET #show website url when present' do
+      internship_offer = create(:internship_offer, employer_website: 'http://google.com')
+      get internship_offer_path(internship_offer)
+
+      assert_response :success
+      assert_select "a.external[href=?]", internship_offer.employer_website
+    end
+
+    test 'GET #show does not show website url when absent' do
+      internship_offer = create(:internship_offer, employer_website: nil)
+      get internship_offer_path(internship_offer)
+
+      assert_response :success
+      assert_select "a.external", 0
+    end
   end
 end
