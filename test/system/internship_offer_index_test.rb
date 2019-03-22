0fc053ce4727a383ad9c8b52a6f30f3358103cce
Martin <martin@sharypic.com>
2019-03-22 11:56:46 +0100

    feat(internship#index): update title with expected offer count

---
+++ test/system/internship_offer_index_test.rb
@@ -0,0 +1,40 @@
+require "application_system_test_case"
+
+class StudentFilterOffersTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  def assert_presence_of(internship_offer:)
+    assert_selector "a[href='#{internship_offer_path(internship_offer)}']",
+                    count: 1
+  end
+
+  def assert_absence_of(internship_offer:)
+    assert_no_selector "a[href='#{internship_offer_path(internship_offer)}']"
+  end
+
+
+  test "navigation & interaction works until employer creation" do
+    school = create(:school)
+    student = create(:student, school: school)
+    internship_offer_hello_1 = create(:internship_offer, sector: "Hello")
+    internship_offer_hello_2 = create(:internship_offer, sector: "Hello")
+    internship_offer_world = create(:internship_offer, sector: "World")
+    sign_in(student)
+
+    InternshipOffer.stub :nearby, InternshipOffer.all do
+      InternshipOffer.stub :by_weeks, InternshipOffer.all do
+        visit internship_offers_path
+
+        assert_presence_of(internship_offer: internship_offer_hello_1)
+        assert_presence_of(internship_offer: internship_offer_hello_2)
+        assert_presence_of(internship_offer: internship_offer_world)
+
+        select("Hello", from: "internship-offer-sector-filter")
+        assert_presence_of(internship_offer: internship_offer_hello_1)
+        assert_presence_of(internship_offer: internship_offer_hello_2)
+        assert_absence_of(internship_offer: internship_offer_world)
+        assert_selector "#internship-offers-count", text: "2 offres de stage"
+      end
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-require_relative './support/session_manager_test_helper'
 require "minitest/reporters"
 require 'minitest/autorun'
 require 'rails/test_help'
@@ -9,7 +8,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
-
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
