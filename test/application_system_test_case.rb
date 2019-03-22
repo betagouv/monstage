dfd31ad917479a5174d1a5add5523823652e51c8
Martin <martin@sharypic.com>
2019-03-22 18:59:21 +0100

    fix(test:system): rolled back header

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   def visit_signup
     visit "/"
-    click_on "Me connecter"
-    page.find("a[href='#{users_choose_profile_path}'").click
+    click_on "Inscription"
+    # page.find("a[href='#{users_choose_profile_path}'").click
   end
 end
