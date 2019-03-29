a888456a8ca92026d31e8e808ab77472c655b924
Martin <martin@sharypic.com>
2019-03-29 16:44:57 +0100

    fix(migration): rename move block_applications_count from internship application to internship offer weeks

--- db/migrate/20190329142701_add_blocked_applications_count_to_internship_applications.rb
+++ db/migrate/20190329142701_add_blocked_applications_count_to_internship_applications.rb
@@ -1,5 +1,5 @@
 class AddBlockedApplicationsCountToInternshipApplications < ActiveRecord::Migration[5.2]
   def change
-    add_column :internship_applications, :blocked_applications_count, :integer, default: 0, null: false
+    add_column :internship_offer_weeks, :blocked_applications_count, :integer, default: 0, null: false
   end
 end
