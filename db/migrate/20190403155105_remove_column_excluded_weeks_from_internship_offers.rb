baa1e62061d08a4875fd060d2f45655ee6bbaee6
Martin <martin@sharypic.com>
2019-04-03 18:00:23 +0200

    clean(db): add not null constraints where needed

---
+++ db/migrate/20190403155105_remove_column_excluded_weeks_from_internship_offers.rb
@@ -0,0 +1,18 @@
+class RemoveColumnExcludedWeeksFromInternshipOffers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :internship_offers, :excluded_weeks
+    remove_column :internship_offers, :week_day_start
+    remove_column :internship_offers, :week_day_end
+    change_column :internship_offers, :max_internship_week_number, :integer, null: false, default: 1
+    change_column :internship_offers, :sector_id, :integer, null: false
+    change_column :internship_offers, :is_public, :boolean, null: false
+    change_column :internship_offers, :tutor_name, :string, null: false
+    change_column :internship_offers, :tutor_phone, :string, null: false
+    change_column :internship_offers, :tutor_email, :string, null: false
+    change_column :internship_offers, :employer_name, :string, null: false
+    change_column_null :internship_offers, :employer_description, false, "OCTO"
+    change_column_null :internship_offers, :employer_street, false, "Opéra"
+    change_column_null :internship_offers, :employer_city, false, "Paris"
+    change_column_null :internship_offers, :employer_zipcode, false, "75015"
+  end
+end
