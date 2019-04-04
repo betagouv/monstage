64e4c51d1b062f6252b0c08806486e3b4ba17401
Martin <martin@sharypic.com>
2019-04-04 21:34:25 +0200

    feat(ux): some minor improvements

--- app/controllers/internship_applications_controller.rb
+++ app/controllers/internship_applications_controller.rb
@@ -3,10 +3,7 @@ class InternshipApplicationsController < ApplicationController
   before_action :find_internship_offer, only: [:index, :update]
 
   def index
-    @internship_applications = @internship_offer.internship_applications
-                                                .order(updated_at: :desc)
-                                                .page(params[:page])
-
+    set_intership_applications
     authorize! :read, @internship_offer
     authorize! :index, InternshipApplication
   end
@@ -27,13 +24,23 @@ class InternshipApplicationsController < ApplicationController
     @internship_application = @internship_offer.internship_applications.find(params[:id])
     authorize! :update, @internship_offer, InternshipApplication
     @internship_application.send(params[:transition]) if valid_transition?
-    redirect_to @internship_application.internship_offer, flash: { success: 'Candidature mis à jour avec succès' }
+    redirect_to internship_offer_internship_applications_path(@internship_application.internship_offer),
+                flash: { success: 'Candidature mis à jour avec succès' }
+  rescue AASM::InvalidTransition => e
+    redirect_to internship_offer_internship_applications_path(@internship_application.internship_offer),
+                flash: { danger: 'Cette candidature a déjà été traitée' }
   end
 
   private
 
   def valid_transition?
     %w[approve! reject! signed! cancel!].include?(params[:transition])
+  end
+
+  def set_intership_applications
+    @internship_applications = @internship_offer.internship_applications
+                                                .order(updated_at: :desc)
+                                                .page(params[:page])
   end
 
   def find_internship_offer
