4c840aab0a7d7b9af824463161753be32f82752d
Martin <martin@sharypic.com>
2019-04-05 11:51:34 +0200

    tech(perf): leave for later if needed

--- app/libs/internship_application_counters_hook.rb
+++ app/libs/internship_application_counters_hook.rb
@@ -7,6 +7,7 @@ class InternshipApplicationCountersHook
     update_internship_offer_week_counters
   end
 
+  # PERF: maybe optimization path to group those queries?
   def update_internship_offer_counters
     internship_offer.update(
       blocked_weeks_count: internship_offer.internship_offer_weeks
@@ -19,6 +20,10 @@ class InternshipApplicationCountersHook
     )
   end
 
+  # PERF: can be optimized with one query
+  # w8 for perf issue [if it ever occures]
+  # select sum(aasm_state == convention_signed) as convention_signed_count
+  #        sum(aasm_state == approved) as approved_count )
   def update_internship_offer_week_counters
     internship_offer_week.update(
       blocked_applications_count: internship_offer_week.internship_applications
