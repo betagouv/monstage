f453ce5ad096a3f724f5d7d927ff78b098ac2299
Martin <martin@sharypic.com>
2019-04-05 15:57:32 +0200

    clean(pr): remvoe dead code

--- test/models/internship_application_test.rb
+++ test/models/internship_application_test.rb
@@ -9,14 +9,6 @@ class InternshipApplicationTest < ActiveSupport::TestCase
     assert internship_application.valid?
   end
 
-  test "creating an application increment internship_offer.total_applications_count" do
-    internship_offer = create(:internship_offer, max_candidates: 1)
-    internship_offer_week = internship_offer.internship_offer_weeks.first
-    internship_application = create(:internship_application, internship_offer_week: internship_offer_week)
-    internship_offer.reload
-    assert_equal 1, internship_offer.total_applications_count
-  end
-
   test "cannot create application if offer is full" do
     max_candidates = 1
     internship_offer_week = build(:internship_offer_week, blocked_applications_count: max_candidates,
@@ -25,39 +17,6 @@ class InternshipApplicationTest < ActiveSupport::TestCase
                                                  internship_offer_weeks: [internship_offer_week])
     internship_application = build(:internship_application, internship_offer_week: internship_offer_week)
     refute internship_application.valid?
-  end
-
-  test "internship_application increment internship_offer.total_applications_count" do
-    internship_offer = create(:internship_offer, max_candidates: 1)
-    internship_offer_week = internship_offer.internship_offer_weeks.first
-    internship_application = create(:internship_application, internship_offer_week: internship_offer_week)
-    internship_application = create(:internship_application, internship_offer_week: internship_offer_week)
-    internship_offer.reload
-    assert_equal 2, internship_offer.total_applications_count
-  end
-
-  test "internship_application.signed! increment internship_offer_weeks.blocked_applications_count" do
-    internship_offer = create(:internship_offer, max_candidates: 1)
-    internship_offer_week = internship_offer.internship_offer_weeks.first
-    internship_application = create(:internship_application, internship_offer_week: internship_offer_week,
-                                                             aasm_state: :approved)
-    internship_offer_week.reload
-    assert_equal 0, internship_offer_week.blocked_applications_count
-    internship_application.signed!
-    internship_offer_week.reload
-    assert_equal 1, internship_offer_week.blocked_applications_count
-  end
-
-  test "internship_application.reject! deos not increment internship_offer_weeks.blocked_applications_count" do
-    internship_offer = create(:internship_offer, max_candidates: 1)
-    internship_offer_week = internship_offer.internship_offer_weeks.first
-    internship_application = create(:internship_application, internship_offer_week: internship_offer_week)
-    internship_offer_week.reload
-
-    assert_equal 0, internship_offer_week.blocked_applications_count
-    internship_application.reject!
-    internship_offer_week.reload
-    assert_equal 0, internship_offer_week.blocked_applications_count
   end
 
   test 'transition from submited to approved send approved email' do
