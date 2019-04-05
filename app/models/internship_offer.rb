0c35d7b8ccb9719b3bd7912229817aa275a9da59
Martin <martin@sharypic.com>
2019-04-05 10:41:28 +0200

    fix(internship_offer#edit): was not re-using max_internship_week_number

--- app/models/internship_offer.rb
+++ app/models/internship_offer.rb
@@ -75,6 +75,7 @@ class InternshipOffer < ApplicationRecord
 
   def init
    self.max_candidates ||= 1
+   self.max_internship_week_number ||= 1
   rescue ActiveModel::MissingAttributeError
     puts "counter culture error"
   end
