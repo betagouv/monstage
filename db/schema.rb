ca7bde14a9a6fd97ff55ada7644fcb7083a647ff
Martin <martin@sharypic.com>
2019-04-05 17:12:42 +0200

    fix(test): bump to last migration version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_113420) do
+ActiveRecord::Schema.define(version: 2019_04_05_113421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
