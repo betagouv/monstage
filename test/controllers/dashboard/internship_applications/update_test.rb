25fc39399de10cba68e7bcd075d9e419a7bf07cf
Brice <brice.durand2@gmail.com>
2019-04-11 14:44:21 +0200

    fix tests

--- test/controllers/dashboard/internship_applications/update_test.rb
+++ test/controllers/dashboard/internship_applications/update_test.rb
@@ -11,11 +11,11 @@ module InternshipApplications
       sign_in(internship_application.internship_offer.employer)
 
       assert_enqueued_emails 1 do
-        patch(internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
+        patch(dashboard_internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
               params: { transition: :approve! })
       end
 
-      assert_redirected_to internship_offer_internship_applications_path(internship_application.internship_offer)
+      assert_redirected_to dashboard_internship_offer_internship_applications_path(internship_application.internship_offer)
 
       assert InternshipApplication.last.approved?
     end
@@ -26,11 +26,11 @@ module InternshipApplications
       sign_in(internship_application.internship_offer.employer)
 
       assert_enqueued_emails 1 do
-        patch(internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
+        patch(dashboard_internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
               params: { transition: :reject! })
       end
 
-      assert_redirected_to internship_offer_internship_applications_path(internship_application.internship_offer)
+      assert_redirected_to dashboard_internship_offer_internship_applications_path(internship_application.internship_offer)
 
       assert InternshipApplication.last.rejected?
     end
@@ -41,13 +41,13 @@ module InternshipApplications
       sign_in(internship_application.internship_offer.employer)
 
       assert_enqueued_emails 0 do
-        patch(internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
+        patch(dashboard_internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
               params: { transition: :cancel! })
       end
       internship_application.reload
 
       assert internship_application.rejected?
-      assert_redirected_to internship_offer_internship_applications_path(internship_application.internship_offer)
+      assert_redirected_to dashboard_internship_offer_internship_applications_path(internship_application.internship_offer)
     end
 
     test "PATCH #update with signed! does not send email, change aasm_state" do
@@ -56,12 +56,12 @@ module InternshipApplications
       sign_in(internship_application.internship_offer.employer)
 
       assert_enqueued_emails 0 do
-        patch(internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
+        patch(dashboard_internship_offer_internship_application_path(internship_application.internship_offer, internship_application),
               params: { transition: :signed! })
       end
       internship_application.reload
       assert internship_application.convention_signed?
-      assert_redirected_to internship_offer_internship_applications_path(internship_application.internship_offer)
+      assert_redirected_to dashboard_internship_offer_internship_applications_path(internship_application.internship_offer)
     end
   end
 end
