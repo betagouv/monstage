2198489cc6ce13354a5d862d5aa916cebe7e7713
Martin <martin@sharypic.com>
2019-04-18 17:07:31 +0200

    feat(dashboard/student): begin with spec rendering navbar and timeline

--- test/controllers/dashboard/students/internship_applications_controller_test.rb
+++ test/controllers/dashboard/students/internship_applications_controller_test.rb
@@ -5,10 +5,17 @@ module Dashboard
     class InternshipApplicationsControllerTest < ActionDispatch::IntegrationTest
       include Devise::Test::IntegrationHelpers
 
-      test 'index has timeline' do
+      test 'index responds with 200' do
         student = create(:student)
         get dashboard_students_internship_applications_path(student)
         assert_response :success
+      end
+
+      test 'index render navbar, timeline' do
+        student = create(:student)
+        get dashboard_students_internship_applications_path(student)
+        assert_template 'dashboard/students/_navbar'
+        assert_template 'dashboard/students/_timeline'
       end
     end
   end
