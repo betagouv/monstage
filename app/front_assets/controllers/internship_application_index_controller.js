c6697ee0696d976a12de1d8f1de0ce9e202c67d4
Martin <martin@sharypic.com>
2019-04-24 16:55:35 +0200

    feat(dashboard/internship_offers/internship_applications#index): show motivation up to two lines, then truncate with ellipsis [also move week up]

--- app/front_assets/controllers/internship_application_index_controller.js
+++ app/front_assets/controllers/internship_application_index_controller.js
@@ -3,13 +3,15 @@ import { toggleElement, isVisible } from "../utils/dom";
 
 export default class extends Controller {
   static targets = [ "collapsible",
-                     "linkIconContainer" ]
+                     "linkIconContainer",
+                     "motivation" ]
 
   toggle() {
     const $collapsibleTarget = $(this.collapsibleTarget);
     const $linkIconContainer = $(this.linkIconContainerTarget);
 
     toggleElement($collapsibleTarget);
+    $(this.motivationTarget).toggleClass('text-truncate-max-height-50')
     if (isVisible($collapsibleTarget)) {
       $linkIconContainer.html(`<i class="fas fa-2x fa-chevron-right"></i>`)
     } else {
