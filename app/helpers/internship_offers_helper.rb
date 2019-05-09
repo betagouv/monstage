06de435b4b1b2547d23bc76109fb9f500e9eeede
Martin <martin@sharypic.com>
2019-05-09 15:13:29 +0200

    refactor(sectors): again, rework sector list

--- app/helpers/internship_offers_helper.rb
+++ app/helpers/internship_offers_helper.rb
@@ -1,4 +1,14 @@
 module InternshipOffersHelper
+  def current_sector_name
+    return Sector.find(params[:sector_id]).name if params[:sector_id]
+    'Tous'
+  end
+
+  def current_sector_url
+    return Sector.find(params[:sector_id]).external_url if params[:sector_id]
+    'http://www.onisep.fr/Decouvrir-les-metiers/Des-metiers-par-secteur'
+  end
+
   def options_for_group_names
     administration_list.map { |admin| [admin, { 'data-sector' => 'public'}] } +
         group_list.map { |admin| [admin, { 'data-sector' => 'private'}] }
