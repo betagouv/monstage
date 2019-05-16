69bcf72f4d7e5e9d2ef2e9eab7a70a1433635f99
Martin <martin@sharypic.com>
2019-05-16 18:35:56 +0200

    feat(timeline): pimp my timeline

--- app/helpers/internship_applications_helper.rb
+++ app/helpers/internship_applications_helper.rb
@@ -1,4 +1,20 @@
 module InternshipApplicationsHelper
+  def timeline_step_1_ready?(internship_application)
+    internship_application.aasm_state != 'drafted'
+  end
+
+  def timeline_step_2_ready?(internship_application)
+    internship_application.aasm_state == 'submitted' ||
+    internship_application.aasm_state == 'approved' ||
+    internship_application.aasm_state == 'rejected' ||
+    internship_application.aasm_state == 'convention_signed'
+
+  end
+
+  def timeline_step_3_ready?(internship_application)
+    internship_application.aasm_state == 'convention_signed'
+  end
+
   def is_resume_empty?(internship_application)
     !has_resume_educational_background?(internship_application) &&
     !has_resume_other?(internship_application) &&
