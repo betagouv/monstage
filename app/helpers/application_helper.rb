934dff04fd922cbcda18f759581ff0f5d39b02a3
Martin <martin@sharypic.com>
2019-05-17 15:22:19 +0200

    feat(homepage): new homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
     ""
   end
 
+  def body_class_name
+    return 'homepage' if homepage?
+    ""
+  end
+
+  def homepage?
+    current_page?(root_path)
+  end
+
   def current_controller?(controller_name)
     controller.controller_name.to_s == controller_name.to_s
   end
