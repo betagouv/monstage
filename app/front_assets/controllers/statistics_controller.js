f94b601dc8e501cdbc4d68c94fa959bb0cf82db4
Brice <brice.durand2@gmail.com>
2019-05-20 17:30:25 +0200

    navigation path, not the cleanest thing

--- app/front_assets/controllers/statistics_controller.js
+++ app/front_assets/controllers/statistics_controller.js
@@ -4,10 +4,19 @@ import Turbolinks from 'turbolinks';
 export default class extends Controller {
 
   filterByDepartment(event) {
-    const department = $(event.target).val()
+    this.changeURLFromEvent(event, "department")
+  }
+
+  filterByGroupName(event) {
+    this.changeURLFromEvent(event, "group_name")
+  }
+
+  changeURLFromEvent(event, param) {
+    const param_value = $(event.target).val()
     const search_params = new URLSearchParams(window.location.search)
-    search_params.set('department', department)
+    search_params.set(param, param_value)
 
     Turbolinks.visit(`${window.location.origin}${window.location.pathname}?${search_params.toString()}`)
+
   }
 }
