f28d5caa41661548c26a3f935ddaaa6e8c6f697a
Brice <brice.durand2@gmail.com>
2019-05-21 18:19:19 +0200

    less logic code in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def statistiques
     @offers = InternshipOffer.during_current_year
+    @departments = InternshipOffer.pluck(:department).uniq.reject(&:blank?)
+    @groups = InternshipOffer.pluck(:group_name).uniq.reject(&:blank?)
   end
 
 end
