8ed090b8a6ca8e44f320172fd005f761a1e9939e
Martin <martin@sharypic.com>
2019-05-23 11:53:23 +0200

    ui(statistiques): implem some bits of design on stats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,15 @@ module ApplicationHelper
   def homepage?
     current_page?(root_path)
   end
+  def statistics?
+    current_controller?(:pages) && current_action?(:statistiques)
+  end
 
   def current_controller?(controller_name)
     controller.controller_name.to_s == controller_name.to_s
+  end
+
+  def current_action?(action_name)
+    controller.action_name.to_s == action_name.to_s
   end
 end
