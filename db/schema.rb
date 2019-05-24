276a4d1ec6a5d21155601f0f4866e5b23fae1a87
Martin <martin@sharypic.com>
2019-05-24 17:34:54 +0200

    feat(design.cover): use #333 with .4 opacity for overlay

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_092527) do
+ActiveRecord::Schema.define(version: 2019_05_24_153049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
