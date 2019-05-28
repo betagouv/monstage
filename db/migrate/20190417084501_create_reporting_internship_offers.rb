8dbd9e1f170dab6b42754848c51bfff1323668ea
Brice <brice.durand2@gmail.com>
2019-05-28 15:23:04 +0200

    Fix more migrations

--- db/migrate/20190417084501_create_reporting_internship_offers.rb
+++ db/migrate/20190417084501_create_reporting_internship_offers.rb
@@ -2,6 +2,7 @@
 
 class CreateReportingInternshipOffers < ActiveRecord::Migration[5.2]
   def change
-    create_view :reporting_internship_offers
+    # REMOVED because scenic gem is gone
+    # create_view :reporting_internship_offers
   end
 end
--- db/migrate/20190523072433_drop_materialized_view.rb
+++ db/migrate/20190523072433_drop_materialized_view.rb
@@ -4,7 +4,8 @@ class DropMaterializedView < ActiveRecord::Migration[5.2]
   def change
     remove_index :reporting_internship_offers, :sector_name
     remove_index :reporting_internship_offers, :publicly_name
-    drop_view :reporting_internship_offers, materialized: true
+    # REMOVED because scenic gem is gone
+    # drop_view :reporting_internship_offers, materialized: true
     add_index :internship_offers, :department
     add_index :internship_offers, :group_name
   end
