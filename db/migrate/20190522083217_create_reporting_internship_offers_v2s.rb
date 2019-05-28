1f62d9399936c4e0e9fe83be1e1852a9433cdc2f
Brice <brice.durand2@gmail.com>
2019-05-28 15:56:49 +0200

    remove view migration

--- db/migrate/20190522083217_create_reporting_internship_offers_v2s.rb
+++ db/migrate/20190522083217_create_reporting_internship_offers_v2s.rb
@@ -2,12 +2,12 @@
 
 class CreateReportingInternshipOffersV2s < ActiveRecord::Migration[5.2]
   def up
-    drop_view :reporting_internship_offers
-    create_view :reporting_internship_offers, materialized: true
+    # drop_view :reporting_internship_offers
+    # create_view :reporting_internship_offers, materialized: true
   end
 
   def down
-    drop_view :reporting_internship_offers
-    create_view :reporting_internship_offers
+    # drop_view :reporting_internship_offers
+    # create_view :reporting_internship_offers
   end
 end
