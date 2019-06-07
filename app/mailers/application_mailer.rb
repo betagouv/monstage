97940ca05e7590a45428f6db31c96edee31d22e7
Martin <martin@sharypic.com>
2019-06-07 17:08:33 +0200

    feat(mailer.from): enhance formatting and naming of from [nice name]

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: Proc.new { ApplicationMailer.from }
+  default from: Proc.new {  ApplicationMailer.formatted_email("Mon Stage de 3ème") }
   layout 'mailer'
 
   def self.from
     host_or_default = ENV.fetch('HOST') { 'https://test.example.com' }
     domain_without_www = URI(host_or_default).host.gsub('www.', '')
     "ne-pas-repondre@#{domain_without_www}"
+  end
+
+  def self.formatted_email(display_name)
+    address = Mail::Address.new(self.from)
+    address.display_name = display_name
+    address.format
   end
 end
