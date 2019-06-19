d114cad36b7e74c74c5ce6e3f918d5d6af184eec
Martin <martin@sharypic.com>
2019-06-19 10:22:38 +0200

    fix(serializers): add missing files

--- db/migrate/20190615210642_remove_constraints_on_internship_offer.rb
+++ db/migrate/20190615210642_remove_constraints_on_internship_offer.rb
@@ -3,6 +3,5 @@ class RemoveConstraintsOnInternshipOffer < ActiveRecord::Migration[5.2]
     change_column :internship_offers, :tutor_name, :string, null: true
     change_column :internship_offers, :tutor_phone, :string, null: true
     change_column :internship_offers, :tutor_email, :string, null: true
-
   end
 end
