50236bacff6bea28a295e6287b66a41b7f7f3dc8
Martin <martin@sharypic.com>
2019-06-28 12:52:27 +0200

    feat(dashboard/internship_offer#index): ensure api internship offers are accessible on dashboard/internship_offers#index for operators

--- test/controllers/dashboard/internship_offers/index_test.rb
+++ test/controllers/dashboard/internship_offers/index_test.rb
@@ -5,22 +5,6 @@ require 'test_helper'
 module Dashboard
   class InternshipOffersControllerTest < ActionDispatch::IntegrationTest
     include Devise::Test::IntegrationHelpers
-    #
-    # navigation checks
-    #
-    test 'GET #show as Employer displays internship_applications link' do
-      internship_offer = create(:internship_offer)
-      sign_in(internship_offer.employer)
-      get dashboard_internship_offer_path(internship_offer)
-      assert_response :success
-      assert_select 'a[href=?]', edit_dashboard_internship_offer_path(internship_offer),
-                    count: 1
-      assert_select 'a[href=?]', dashboard_internship_offer_path(internship_offer),
-                    count: 1
-      assert_select 'a[href=?]', dashboard_internship_offer_internship_applications_path(internship_offer),
-                    text: '0 candidatures',
-                    count: 1
-    end
 
     test 'GET #index as Employer displays internship_applications link' do
       internship_offer = create(:internship_offer)
@@ -47,6 +31,16 @@ module Dashboard
       assert_select 'a[href=?]', edit_dashboard_internship_offer_path(internship_offer_delegated_to_opereator),
                     count: 0
       assert_select 'a[href=?]', dashboard_internship_offer_path(internship_offer_delegated_to_opereator),
+                    count: 1
+    end
+
+    test 'GET #index as Operator displays api_internship_offers' do
+      operator = create(:user_operator)
+      internship_offer = create(:api_internship_offer, employer: operator)
+      sign_in(operator)
+      get dashboard_internship_offers_path
+      assert_response :success
+      assert_select "tr.test-internship-offer-#{internship_offer.id}",
                     count: 1
     end
   end
