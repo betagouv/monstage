9eb6722aabc9d55206bd7c9433b5dddb9752f88f
Martin <martin@sharypic.com>
2019-07-03 09:25:01 +0200

    refactor(reporting/internship_offer): add total_report_count to aggregates functions, some renaming/cleanup

--- app/models/reporting/internship_offer.rb
+++ app/models/reporting/internship_offer.rb
@@ -10,6 +10,7 @@ module Reporting
     delegate :name, to: :sector, prefix: true
 
     AGGREGATE_FUNCTIONS = {
+      report_total_count: 'sum(max_internship_week_number * max_candidates)',
       total_applications_count: 'sum(total_applications_count)',
       total_male_applications_count: 'sum(total_male_applications_count)',
       total_female_applications_count: 'sum(total_applications_count) - sum(total_male_applications_count)',
@@ -18,12 +19,13 @@ module Reporting
       total_female_convention_signed_applications_count: 'sum(convention_signed_applications_count) - sum(total_male_convention_signed_applications_count)'
     }.freeze
 
-    def self.aggregate_functions_to_SQL
+    def self.aggregate_functions_to_sql_select
       AGGREGATE_FUNCTIONS.map do |attribute_name, group_function|
         "#{group_function} as #{attribute_name}"
       end
     end
 
+    # what's the rails way?
     def self.i18n_attribute(attribute_name)
       I18n.t(
         [
@@ -36,12 +38,10 @@ module Reporting
     end
 
     def self.csv_headers(headers:{})
-      headers = headers.merge({ report_total_count: i18n_attribute(:report_total_count) })
       AGGREGATE_FUNCTIONS.keys.inject(headers) do |headers, column_name|
         headers[column_name] = i18n_attribute(column_name)
         headers
       end
-      headers
     end
 
     scope :during_current_year, lambda {
@@ -69,8 +69,7 @@ module Reporting
 
     scope :grouped_by_sector, lambda {
       select('sector_id',
-             'sum(max_internship_week_number * max_candidates) as report_total_count',
-             *self.aggregate_functions_to_SQL)
+             *self.aggregate_functions_to_sql_select)
         .includes(:sector)
         .group(:sector_id)
         .order(:sector_id)
@@ -78,16 +77,14 @@ module Reporting
 
     scope :grouped_by_publicy, lambda {
       select('is_public',
-             'sum(max_internship_week_number * max_candidates) as report_total_count',
-             *self.aggregate_functions_to_SQL)
+             *self.aggregate_functions_to_sql_select)
         .group(:is_public)
         .order(:is_public)
     }
 
     scope :grouped_by_group, lambda {
       select('internship_offers.group',
-             'sum(max_internship_week_number * max_candidates) as report_total_count',
-             *self.aggregate_functions_to_SQL)
+             *self.aggregate_functions_to_sql_select)
         .where(is_public: true)
         .group('internship_offers.group')
         .order('internship_offers.group')
