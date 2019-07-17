454c40ab6b7d3d59a42bbbea34141624f997f844
Brice <brice.durand2@gmail.com>
2019-07-17 11:46:30 +0200

    fix feedback tests

--- test/controllers/feedbacks_controller_test.rb
+++ test/controllers/feedbacks_controller_test.rb
@@ -10,19 +10,19 @@ class FeedbacksControllerTest < ActionDispatch::IntegrationTest
 
   test 'should get index as god' do
     sign_in(create(:god))
-    get feedbacks_url
+    get dashboard_feedbacks_url
     assert_response :success
   end
 
   test 'should not get index as student' do
     sign_in(create(:student))
-    get feedbacks_url
+    get dashboard_feedbacks_url
     assert_redirected_to root_path
   end
 
   test 'should create feedback' do
     assert_difference('Feedback.count') do
-      post feedbacks_url, params: { feedback: { comment: 'hello', email: 'martin@sharypic.com' } }
+      post dashboard_feedbacks_url, params: { feedback: { comment: 'hello', email: 'martin@sharypic.com' } }
     end
 
     assert_redirected_to root_path
@@ -32,16 +32,16 @@ class FeedbacksControllerTest < ActionDispatch::IntegrationTest
     sign_in(create(:god))
 
     assert_difference('Feedback.count', -1) do
-      delete feedback_url(@feedback)
+      delete dashboard_feedback_url(@feedback)
     end
 
-    assert_redirected_to feedbacks_url
+    assert_redirected_to dashboard_feedbacks_url
   end
 
   test 'should not destroy feedback as student' do
     sign_in(create(:student))
     assert_no_difference('Feedback.count') do
-      delete feedback_url(@feedback)
+      delete dashboard_feedback_url(@feedback)
       assert_redirected_to root_path
     end
   end
