4e1c22b8f8f531bf968f7b75d45588ed5cea2e31
Martin <martin@sharypic.com>
2019-08-07 11:08:23 +0200

    fix(infra.production.deploy.promote.sh): typo in migrate

--- infra/production/deploy/promote.sh
+++ infra/production/deploy/promote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 
-heroku pipelines:promote  -a betagouv-monstage-staging
-heroku run db:migrate -a betagouv-monstage-prod
+heroku pipelines:promote -a betagouv-monstage-staging
+heroku run rails db:migrate -a betagouv-monstage-prod
