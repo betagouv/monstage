9f74bf50711f10cbe214ba9eb5986c779c0e4950
Martin <martin@sharypic.com>
2019-08-09 17:01:15 +0200

    test(internship_offer#form): ensure various default for ux purpose

--- test/controllers/dashboard/internship_offers/edit_test.rb
+++ test/controllers/dashboard/internship_offers/edit_test.rb
@@ -48,6 +48,38 @@ module InternshipOffers
       end
     end
 
+    test 'GET #edit with default fields' do
+      operator = create(:operator)
+      employer = create(:employer)
+      sign_in(employer)
+      internship_offer = create(:internship_offer, is_public: true,
+                                                   operators: [operator],
+                                                   max_candidates: 1,
+                                                   tutor_name: 'fourtin mourcade',
+                                                   tutor_email: 'fourtin@mour.cade',
+                                                   employer: employer)
+
+      get edit_dashboard_internship_offer_path(internship_offer.to_param)
+      assert_response :success
+      assert_select "#internship_offer_is_public_true[checked]", count: 1 #"ensure user select kind of group"
+      assert_select "#internship_offer_is_public_false[checked]", count: 0 #"ensure user select kind of group"
+      assert_select ".form-group-select-group.d-none", count: 0
+      assert_select ".form-group-select-group", count: 1
+
+      assert_select "#internship_offer_with_operator_true[checked]", count: 1
+      assert_select "#internship_offer_with_operator_false[checked]", count: 0
+      assert_select "#internship_offer_with_operator_unknown[checked]", count: 0
+      assert_select ".operators-check-boxes.d-none", count: 0
+      assert_select ".operators-check-boxes", count: 1
+
+      assert_select "#internship_type_true[checked]", count: 1
+      assert_select "#internship_type_false[checked]", count: 0
+
+      assert_select '#internship_offer_tutor_name[value="fourtin mourcade"]'
+      assert_select '#internship_offer_tutor_email[value="fourtin@mour.cade"]'
+      assert_select 'a.btn-back[href=?]', dashboard_internship_offer_path(internship_offer)
+    end
+
     test 'GET #edit as Operator with disabled fields if applications exist' do
       user_operator = create(:user_operator)
       sign_in(user_operator)
--- test/controllers/dashboard/internship_offers/new_test.rb
+++ test/controllers/dashboard/internship_offers/new_test.rb
@@ -22,6 +22,28 @@ module InternshipOffers
       end
     end
 
+    test 'GET #new as employer show default values' do
+      employer = create(:employer)
+      sign_in(employer)
+      get new_dashboard_internship_offer_path
+
+      assert_select "#internship_offer_is_public_true[checked]", count: 0 #"ensure user select kind of group"
+      assert_select "#internship_offer_is_public_false[checked]", count: 0 #"ensure user select kind of group"
+      assert_select ".form-group-select-group.d-none", count: 1
+
+      assert_select "#internship_offer_with_operator_true[checked]", count: 0
+      assert_select "#internship_offer_with_operator_false[checked]", count: 0
+      assert_select "#internship_offer_with_operator_unknown[checked]", count: 1
+      assert_select ".operators-check-boxes.d-none", count: 1
+
+      assert_select "#internship_type_true[checked]", count: 0
+      assert_select "#internship_type_false[checked]", count: 0
+
+      assert_select "#internship_offer_tutor_name[value=\"#{employer.name}\"]"
+      assert_select "#internship_offer_tutor_email[value=\"#{employer.email}\"]"
+      assert_select 'a.btn-back[href=?]', dashboard_internship_offers_path
+    end
+
     test 'GET #edit as Operator with disabled fields if applications exist' do
       user_operator = create(:user_operator)
       sign_in(user_operator)
