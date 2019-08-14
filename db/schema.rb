79bf0cff19942561c1dc44731e9d26b3ac60f8ff
Martin <martin@sharypic.com>
2019-08-14 14:43:43 +0200

    tech(student.custom_track): add field to keep track of flagged student in custom education tracks (cordee de reussite, parcours d'excellence)

--- db/schema.rb
+++ db/schema.rb
@@ -201,6 +201,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_122943) do
     t.bigint "operator_id"
     t.string "api_token"
     t.text "handicap"
+    t.boolean "custom_track", default: false, null: false
     t.index ["api_token"], name: "index_users_on_api_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
--- db/structure.sql
+++ db/structure.sql
@@ -564,7 +564,8 @@ CREATE TABLE public.users (
     has_parental_consent boolean DEFAULT false,
     operator_id bigint,
     api_token character varying,
-    handicap text
+    handicap text,
+    custom_track boolean DEFAULT false NOT NULL
 );
 
 
@@ -1278,6 +1279,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190802142452'),
 ('20190802163449'),
 ('20190807122943'),
-('20190814075600');
+('20190814075600'),
+('20190814124142');
 
 
