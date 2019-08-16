2193662ad2866a6c34e0a61b200a2c036079f3d5
Martin <martin@sharypic.com>
2019-08-16 09:47:16 +0200

    feat(internship_offers.show_details): add collapsible list to show weeks when needed

---
+++ app/front_assets/controllers/collapsible_list_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus"
+import { showElement, hideElement } from "../utils/dom";
+
+export default class extends Controller {
+  static targets = ["hiddenElement",
+                    "showMoreButton",
+                    "lastVisibleElement"]
+
+  showMore(event) {
+    $(this.element).find('.last-visible')
+                   .removeClass('last-visible')
+    hideElement($(this.showMoreButtonTarget));
+    $(this.hiddenElementTargets).each((i, el) => {
+      showElement($(el))
+    })
+    event.preventDefault()
+  }
+}
