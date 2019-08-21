8a00cd9d531321b163224a22de5efdca70ff2985
Martin <martin@sharypic.com>
2019-08-21 15:57:31 +0200

    feat(home.myfutur): add my futur logo and sort by alnum

--- app/front_assets/images/index.js
+++ app/front_assets/images/index.js
@@ -1,32 +1,33 @@
-import './favicon.png'
-import './logo-mon-stage-3e.svg'
-import './logo-beta-gouv-fr.png'
+import './favicon.png';
+import './logo-mon-stage-3e.svg';
+import './logo-beta-gouv-fr.png';
 
-import './pages/logo_onisep.jpg'
-import './pages/logo_cget_texte.png'
+import './pages/logo_onisep.jpg';
+import './pages/logo_cget_texte.png';
 
-import './homepage/icon-documents-utiles.svg'
-import './homepage/benefices/icon-accompagner-les-eleves.png'
-import './homepage/benefices/icon-offrir-un-stage.png'
-import './homepage/benefices/icon-trouver-un-stage.png'
+import './homepage/icon-documents-utiles.svg';
+import './homepage/benefices/icon-accompagner-les-eleves.png';
+import './homepage/benefices/icon-offrir-un-stage.png';
+import './homepage/benefices/icon-trouver-un-stage.png';
 
-import './homepage/partenaires/Logo-onisep.jpg'
-import './homepage/partenaires/Logo-FACE.jpg'
-import './homepage/partenaires/Logo-degunsanstage.jpg'
-import './homepage/partenaires/Logo-jobIRL.jpg'
-import './homepage/partenaires/Logo-le-reseau.jpg'
-import './homepage/partenaires/Logo-les-entreprises-pour-la-cite.jpg'
-import './homepage/partenaires/Logo-tous-en-stage.jpg'
-import './homepage/partenaires/Logo-un-stage-et-apres.jpg'
-import './homepage/partenaires/Logo-viens-voir-mon-taf.jpg'
-import './homepage/partenaires/Logo-jndj.png'
-import './homepage/partenaires/Logo-united-way.png'
+import './homepage/partenaires/Logo-onisep.jpg';
+import './homepage/partenaires/Logo-face.jpg';
+import './homepage/partenaires/Logo-degunsanstage.jpg';
+import './homepage/partenaires/Logo-jobirl.jpg';
+import './homepage/partenaires/Logo-le-reseau.jpg';
+import './homepage/partenaires/Logo-les-entreprises-pour-la-cite.jpg';
+import './homepage/partenaires/Logo-myfuture.png';
+import './homepage/partenaires/Logo-tous-en-stage.jpg';
+import './homepage/partenaires/Logo-un-stage-et-apres.jpg';
+import './homepage/partenaires/Logo-viens-voir-mon-taf.jpg';
+import './homepage/partenaires/Logo-jndj.png';
+import './homepage/partenaires/Logo-united-way.png';
 
-import './homepage/sponsors/Logo-MCTRCT-CGET.png'
-import './homepage/sponsors/Logo-ministere-education-nationale.png'
+import './homepage/sponsors/Logo-MCTRCT-CGET.png';
+import './homepage/sponsors/Logo-ministere-education-nationale.png';
 
-import './documents-utiles/CarnetStagePro_USEA_WEB-2.jpg'
-import './documents-utiles/RienNeSertDeCourirPageBD.jpg'
-import './documents-utiles/USEA-Carnet-Stage-Education.jpg'
-import './documents-utiles/USEA-Cherchez-la-chercheuse.jpg'
-import './documents-utiles/versLinfini.jpg'
+import './documents-utiles/CarnetStagePro_USEA_WEB-2.jpg';
+import './documents-utiles/RienNeSertDeCourirPageBD.jpg';
+import './documents-utiles/USEA-Carnet-Stage-Education.jpg';
+import './documents-utiles/USEA-Cherchez-la-chercheuse.jpg';
+import './documents-utiles/versLinfini.jpg';
