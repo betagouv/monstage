79ea633f869667de9f4a33de5026078be5254841
Martin <martin@sharypic.com>
2019-08-21 14:49:32 +0200

    feat(services.counter_manager): update to reset new counters columns

--- app/libs/services/counter_manager.rb
+++ app/libs/services/counter_manager.rb
@@ -8,7 +8,10 @@ module Services
         blocked_weeks_count: 0,
         total_applications_count: 0,
         convention_signed_applications_count: 0,
-        approved_applications_count: 0
+        approved_applications_count: 0,
+        total_male_applications_count: 0,
+        total_male_convention_signed_applications_count: 0,
+        total_custom_track_convention_signed_applications_count: 0
       )
       InternshipOfferWeek.update_all(
         approved_applications_count: 0,
