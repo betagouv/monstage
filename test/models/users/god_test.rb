213204a13fca7ca7d62421c224ec4ffe8b3d72d2
Martin <martin@sharypic.com>
2019-08-21 23:30:53 +0200

    fix(test): god changes his dashboard url / after sign in path

--- test/models/users/god_test.rb
+++ test/models/users/god_test.rb
@@ -6,7 +6,7 @@ module Users
     test 'god.after_sign_in_path redirects to dashboard_schools' do
       god = build(:god)
       assert_equal(god.after_sign_in_path,
-                   Rails.application.routes.url_helpers.dashboard_schools_path)
+                   Rails.application.routes.url_helpers.dashboard_schools_path(visible: true))
     end
   end
 end
