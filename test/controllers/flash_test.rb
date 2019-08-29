0a408c7636d908561eedc18d42176ce3d269ac5d
Martin <martin@sharypic.com>
2019-08-29 18:40:07 +0200

    feat(footer.help): add link to help file

--- test/controllers/flash_test.rb
+++ test/controllers/flash_test.rb
@@ -2,12 +2,10 @@
 
 require 'test_helper'
 
-module InternshipOffers
-  class FlashTest < ActionDispatch::IntegrationTest
-    test 'flash presence' do
-      get new_dashboard_internship_offer_path
-      follow_redirect!
-      assert_select('#alert-warning', {}, 'missing flash rendering')
-    end
+class FlashTest < ActionDispatch::IntegrationTest
+  test 'flash presence' do
+    get new_dashboard_internship_offer_path
+    follow_redirect!
+    assert_select('#alert-warning', {}, 'missing flash rendering')
   end
 end
---
+++ test/controllers/footer_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class FooterTest < ActionDispatch::IntegrationTest
+  test 'presence of footer links within valid rg2a footer' do
+    get root_path
+
+    assert_select('a[href=?]',
+                  'https://beta.gouv.fr/startups/monstage.html')
+    assert_select('a[href=?]', mentions_legales_path)
+    assert_select('a[href=?]', conditions_d_utilisation_path)
+    assert_select('a[href=?]', accessibilite_path)
+    assert_select('a[href=?]', accessibilite_path)
+    assert_select(
+      'a[href=?]',
+      "#{root_url}/documents_utiles/Manuel-Utilisation-Plateforme.pdf"
+    )
+  end
+end
