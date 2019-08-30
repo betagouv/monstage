b31c6043eb4bc75f88f92e9fa805fc1b1987a6fe
Martin <martin@sharypic.com>
2019-08-30 14:17:37 +0200

    tech(dev): sometimes sti failed to locate users classes, make it more robust

---
+++ config/initializers/preload_user_sti.rb
@@ -0,0 +1,7 @@
+# see: https://weblog.rubyonrails.org/2019/2/22/zeitwerk-integration-in-rails-6-beta-2/
+autoloader = Rails.autoloaders.main
+sti_leaves = %w[employer god main_teacher operator other school_manager student teacher]
+
+sti_leaves.each do |leaf|
+  autoloader.preload("#{Rails.root}/app/models/users/#{leaf}.rb")
+end
