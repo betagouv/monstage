337db8f66cd234ffff2b7f52d0d5e18457e81fd8
Martin <martin@sharypic.com>
2019-09-12 18:55:17 +0200

    test(listable): test head/tail of listable

--- test/controllers/internship_offers/show_test.rb
+++ test/controllers/internship_offers/show_test.rb
@@ -236,7 +236,7 @@ module InternshipOffers
       end
     end
 
-    test 'GET #show offers next/previous navigation' do
+    test 'GET #show offers show next/previous navigation in list' do
       previous_out = create(:internship_offer, title: "previous_out")
       previous_in_page = create(:internship_offer, title: "previous")
       current = create(:internship_offer, title: "current")
@@ -250,8 +250,44 @@ module InternshipOffers
           get internship_offer_path(current)
 
           assert_response :success
-          assert_select 'a[href=?]', internship_offer_path(next_in_page)
           assert_select 'a[href=?]', internship_offer_path(previous_in_page)
+          assert_select 'a[href=?]', internship_offer_path(next_in_page)
+        end
+      end
+    end
+
+    test 'GET #show offers show next and not previous when no previous' do
+      current = create(:internship_offer, title: "current")
+      next_in_page = create(:internship_offer, title: "next")
+      next_out = create(:internship_offer, title: "next_out")
+      student = create(:student, school: create(:school))
+
+      InternshipOffer.stub :nearby, InternshipOffer.all do
+        InternshipOffer.stub :by_weeks, InternshipOffer.all do
+          sign_in(student)
+          get internship_offer_path(current)
+
+          assert_response :success
+          assert_select 'a.link-previous', count: 0
+          assert_select 'a[href=?]', internship_offer_path(next_in_page)
+        end
+      end
+    end
+
+    test 'GET #show offers show previous and not next when no not' do
+      previous_out = create(:internship_offer, title: "previous_out")
+      previous_in_page = create(:internship_offer, title: "previous")
+      current = create(:internship_offer, title: "current")
+      student = create(:student, school: create(:school))
+
+      InternshipOffer.stub :nearby, InternshipOffer.all do
+        InternshipOffer.stub :by_weeks, InternshipOffer.all do
+          sign_in(student)
+          get internship_offer_path(current)
+
+          assert_response :success
+          assert_select 'a[href=?]', internship_offer_path(previous_in_page)
+          assert_select 'a.link-next', count: 0
         end
       end
     end
