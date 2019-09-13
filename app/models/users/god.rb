af29e1a615e54af4ba1be22871c9f81c122f592f
Martin <martin@sharypic.com>
2019-09-13 13:27:51 +0200

    feat(god): switch to rails admin

--- app/models/users/god.rb
+++ app/models/users/god.rb
@@ -3,7 +3,7 @@
 module Users
   class God < User
     include UserAdmin
-    
+
     include InternshipOffersScopes::ByCoordinates
     scope :targeted_internship_offers, ->(user:, coordinates:) {
       query = InternshipOffer.kept
@@ -12,7 +12,11 @@ module Users
     }
 
     def custom_dashboard_path
-      url_helpers.dashboard_schools_path(visible: true)
+      url_helpers.rails_admin_path
+    end
+
+    def dashboard_name
+      'Admin'
     end
   end
 end
