74160139e1a638f17d602ff4b236da33b565149a
Martin <martin@sharypic.com>
2019-09-13 14:57:34 +0200

    fix(test): update god after_signin_path

--- test/models/users/god_test.rb
+++ test/models/users/god_test.rb
@@ -3,10 +3,10 @@
 require 'test_helper'
 module Users
   class GodTest < ActiveSupport::TestCase
-    test 'god.after_sign_in_path redirects to dashboard_schools' do
+    test 'god.after_sign_in_path redirects to rails admin' do
       god = build(:god)
       assert_equal(god.after_sign_in_path,
-                   Rails.application.routes.url_helpers.dashboard_schools_path(visible: true))
+                   Rails.application.routes.url_helpers.rails_admin_path)
     end
   end
 end
