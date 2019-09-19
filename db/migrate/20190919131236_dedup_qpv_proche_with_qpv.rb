9fb81f65d8d755fdd928e895afbbe735fa0fe24b
Martin <martin@sharypic.com>
2019-09-19 16:11:20 +0200

    fix(migration): update renamed call

--- db/migrate/20190919131236_dedup_qpv_proche_with_qpv.rb
+++ db/migrate/20190919131236_dedup_qpv_proche_with_qpv.rb
@@ -2,7 +2,7 @@ class DedupQpvProcheWithQpv < ActiveRecord::Migration[6.0]
   def up
     School.where(kind: :qpv_proche).each do |possibly_duplicate|
       deduper = Dto::SchoolDedup.new(school: possibly_duplicate)
-      deduper.migrate if deduper.dup?
+      deduper.migrate_all if deduper.dup?
     end
 
     School.where(kind: :qpv_proche).update_all(visible: true)
