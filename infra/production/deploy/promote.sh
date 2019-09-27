b42d8e3094ba969245aacd5366f6489c32ac8bab
Martin <martin@sharypic.com>
2019-09-27 14:13:33 +0200

    instrument(sentry.releases): also tag release automatically based on commit hash for easier debugging

--- infra/production/deploy/promote.sh
+++ infra/production/deploy/promote.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
+command -v sentry-cli >/dev/null 2>&1 || { echo "Deploy must ping sentry-cli to track bug from commits, install it with brew install getsentry/tools/sentry-cli. then login with sentry-cli login [credentials for sentri.io are in monstage.kdbx], also"; exit 1; }
 
+# Deploy heroku
 heroku pipelines:promote -a betagouv-monstage-staging
 heroku run rails db:migrate -a betagouv-monstage-prod
 heroku restart -a betagouv-monstage-prod
+
+# Create sentry release
+sentry-cli releases set-commits "$VERSION" --auto
--- infra/production/deploy/push.sh
+++ infra/production/deploy/push.sh
@@ -5,7 +5,9 @@ target='production'
 git remote -vvv | grep $target
 
 if [ $? -eq 0 ]; then
+  command -v sentry-cli >/dev/null 2>&1 || { echo "Deploy must ping sentry-cli to track bug from commits, install it with brew install getsentry/tools/sentry-cli. then login with sentry-cli login [credentials for sentri.io are in monstage.kdbx], also"; exit 1; }
   git push $target master:master
+  sentry-cli releases set-commits "$VERSION" --auto
   exit $?
 else
   echo "missing git remote $target"
