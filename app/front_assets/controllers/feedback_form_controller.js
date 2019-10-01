520362ba840cf8e91e00734ac1da29a38a6d399a
Brice <brice.durand2@gmail.com>
2019-10-01 15:13:16 +0200

    Refactor feedback system

--- app/front_assets/controllers/feedback_form_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "stimulus"
-import { hideElement, showElement } from "../utils/dom";
-export default class extends Controller {
-  static targets = [ 'container' ];
-
-  toggle() {
-    $(this.containerTarget).toggleClass('feedback-form-open')
-  }
-}
