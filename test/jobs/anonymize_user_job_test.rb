023bf651ad5227b9675007dc2b9ff667af488017
Martin <martin@sharypic.com>
2019-11-06 13:15:50 +0100

    feat(test): try to avoid git

--- test/jobs/anonymize_user_job_test.rb
+++ test/jobs/anonymize_user_job_test.rb
@@ -1,6 +1,11 @@
 class AnonymizeUserJobTest < ActiveJob::TestCase
   include ActionMailer::TestHelper
 
+  # @warning: sometimes it fails ; surprising,
+  # try to empty deliveries before running the spec
+  setup { ActionMailer::Base.deliveries = [] }
+  teardown { ActionMailer::Base.deliveries = [] }
+
   test 'send email' do
     AnonymizeUserJob.perform_now(recipient_email: 'fourcade.m@gmail.com')
     assert_emails 1
