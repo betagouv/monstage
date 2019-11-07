fac430219be74d61d84d9c86281be37079bf3b77
Brice <brice.durand2@gmail.com>
2019-11-07 13:46:16 +0100

    Stats feature flipping

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+
+  config.show_stats = true
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -136,4 +136,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.show_stats = true
 end
