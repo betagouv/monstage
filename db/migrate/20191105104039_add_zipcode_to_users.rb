af475b3dae2841cfcad4d193e56137f98ae908bd
Martin <martin@sharypic.com>
2019-11-12 20:43:46 +0100

    fix(migration): update file name

--- db/migrate/20191105104039_add_zipcode_to_users.rb
+++ db/migrate/20191105104039_add_zipcode_to_users.rb
@@ -1,4 +1,4 @@
-class PrefillInternshipOfferWeeksCount < ActiveRecord::Migration[6.0]
+class AddZipcodeToUsers < ActiveRecord::Migration[6.0]
   def up
     add_column :users, :zipcode, :string
   end
