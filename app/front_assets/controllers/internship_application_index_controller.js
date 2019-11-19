9e60e6549104205d25812cf803e63b609e140b4d
Martin <martin@sharypic.com>
2019-11-19 18:18:10 +0100

    feat(internship_application#index): enhance collapsible behaviour by increasing size of icons and adding a dedicated link to show more info about the internship application

--- app/front_assets/controllers/internship_application_index_controller.js
+++ app/front_assets/controllers/internship_application_index_controller.js
@@ -1,16 +1,18 @@
 import { Controller } from "stimulus"
-import { toggleElement, isVisible } from "../utils/dom";
+import { toggleElement, hideElement, isVisible } from "../utils/dom";
 
 export default class extends Controller {
   static targets = [ "collapsible",
                      "linkIconContainer",
-                     "motivation" ]
+                     "motivation",
+                     "linkTextShowMore" ]
 
   toggle() {
     const $collapsibleTarget = $(this.collapsibleTarget);
     const $linkIconContainer = $(this.linkIconContainerTarget);
-
+    const $linkTextShowMore = $(this.linkTextShowMoreTarget);
     toggleElement($collapsibleTarget);
+    toggleElement($linkTextShowMore);
     $(this.motivationTarget).toggleClass('text-truncate-max-height-50')
     if (isVisible($collapsibleTarget)) {
       $linkIconContainer.html(`<i class="fas fa-2x fa-chevron-down"></i>`)
