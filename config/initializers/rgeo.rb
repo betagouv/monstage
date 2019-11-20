f2c5b72f15ba4770ac2c7b4045e49fdd2619873f
Martin <martin@sharypic.com>
2019-11-20 16:28:49 +0100

    feat(rgeo): try with st_point  factory

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -6,11 +6,11 @@ RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
                                                      has_z_coordinate: false)
   config.default = POINT_FACTORY
 
-  config.register(POINT_FACTORY, geo_type: 'point')
+  config.register(POINT_FACTORY, geo_type: 'st_point')
 end
 
 def geo_point_factory(latitude:, longitude:)
-  type = { geo_type: 'point' }
+  type = { geo_type: 'st_point' }
   factory = RGeo::ActiveRecord::SpatialFactoryStore.instance
                                                    .factory(type)
   factory.point(longitude, latitude)
