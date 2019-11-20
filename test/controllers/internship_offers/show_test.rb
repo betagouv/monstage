25e898aced40990c68c10d26a5a671b5ddf3b877
Martin <martin@sharypic.com>
2019-11-20 17:42:52 +0100

    fix(test): move last spec on show

--- test/controllers/internship_offers/show_test.rb
+++ test/controllers/internship_offers/show_test.rb
@@ -335,7 +335,19 @@ module InternshipOffers
                                               longitude: 2)
         end
       end
+    end
 
+    test 'GET #show as Employer displays internship_applications link' do
+      internship_offer = create(:internship_offer)
+      sign_in(internship_offer.employer)
+      get internship_offer_path(internship_offer)
+      assert_response :success
+      assert_template 'dashboard/internship_offers/_navigation'
+      assert_select 'a[href=?]', edit_dashboard_internship_offer_path(internship_offer),
+                    count: 1
+      assert_select 'a[href=?]', dashboard_internship_offer_internship_applications_path(internship_offer),
+                    text: '0 candidatures',
+                    count: 1
     end
   end
 end
