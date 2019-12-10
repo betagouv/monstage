8f929380c9dbcb3aa2d0c8fe67a5f50e09cf999c
Brice <brice.durand2@gmail.com>
2019-12-10 15:42:54 +0100

    Change file name for parental consent

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -2,7 +2,7 @@ class CustomDeviseMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts={})
     if record.is_a?(Users::MainTeacher) || record.is_a?(Users::Student)
       file_path = Rails.root.join('public', 'autorisation_parentale.pdf')
-       attachments['authorisation-parentale.pdf'] = File.read(file_path)
+       attachments['autorisation-parentale.pdf'] = File.read(file_path)
     end
     super(record, token, opts={})
   end
