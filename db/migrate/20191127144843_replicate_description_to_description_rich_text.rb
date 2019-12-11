d2ede8b547cb9207fec47ebc3a9d0d273f1ceb17
Martin <martin@sharypic.com>
2019-12-11 17:05:03 +0100

    fix(migration): forgotten io

--- db/migrate/20191127144843_replicate_description_to_description_rich_text.rb
+++ db/migrate/20191127144843_replicate_description_to_description_rich_text.rb
@@ -1,7 +1,7 @@
 class ReplicateDescriptionToDescriptionRichText < ActiveRecord::Migration[6.0]
   def change
     InternshipOffer.all.in_batches(of: 10) do |batch|
-      batch.each do
+      batch.each do |io|
         io.description_rich_text = io.description
         io.employer_description_rich_text = io.employer_description
         io.save
