ec18a988ccd0ca95b4ec33f523ebb8dbaad94fdf
Martin <martin@sharypic.com>
2019-12-11 16:57:14 +0100

    fix(migration): was stuck

--- db/migrate/20191127144843_replicate_description_to_description_rich_text.rb
+++ db/migrate/20191127144843_replicate_description_to_description_rich_text.rb
@@ -1,9 +1,11 @@
 class ReplicateDescriptionToDescriptionRichText < ActiveRecord::Migration[6.0]
   def change
-    InternshipOffer.all.in_batches(of: 10) do |io|
-      io.description_rich_text = io.description
-      io.employer_description_rich_text = io.employer_description
-      io.save
+    InternshipOffer.all.in_batches(of: 10) do |batch|
+      batch.each do
+        io.description_rich_text = io.description
+        io.employer_description_rich_text = io.employer_description
+        io.save
+      end
     end
   end
 end
