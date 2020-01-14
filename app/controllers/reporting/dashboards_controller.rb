ac398b9936fd1f43f93cd802804cf7de9d3677bb
Martin <martin@sharypic.com>
2020-01-14 14:36:06 +0100

    feat(reporting/dashboard#index): add counters on internship offers

--- app/controllers/reporting/dashboards_controller.rb
+++ app/controllers/reporting/dashboards_controller.rb
@@ -4,23 +4,30 @@ module Reporting
       render locals: {
         total_schools_with_manager: total_schools_with_manager,
         total_schools_ratio: total_schools_ratio,
-        schools_without_manager: finder.fetch_all_without_manager.limit(10),
-        schools_with_weeks_and_internship: finder.fetch_with_weeks_and_internships
+        schools_without_manager: school_finder.fetch_all_without_manager.limit(10),
+        schools_with_weeks_and_internship: school_finder.fetch_with_weeks_and_internships,
+
+        total_internship_offers: internship_offers_finder.total,
+        total_approved_applications_count: internship_offers_finder.total_approved_applications_count,
       }
     end
 
     private
 
     def total_schools_with_manager
-      @total_schools_with_manager ||= finder.total_with_manager
+      @total_schools_with_manager ||= school_finder.total_with_manager
     end
 
     def total_schools_ratio
-      (total_schools_with_manager.to_f * 100 / finder.total).round(2)
+      (total_schools_with_manager.to_f * 100 / school_finder.total).round(2)
     end
 
-    def finder
-      @finder ||= Finders::ReportingSchool.new(params: params.permit(:department))
+    def school_finder
+      @school_finder ||= Finders::ReportingSchool.new(params: params.permit(:department))
+    end
+
+    def internship_offers_finder
+      @internship_offers_finder ||= Finders::ReportingInternshipOffer.new(params: params.permit(:department))
     end
   end
 end
--- app/libs/finders/reporting_internship_offer.rb
+++ app/libs/finders/reporting_internship_offer.rb
@@ -1,6 +1,14 @@
 module Finders
   class ReportingInternshipOffer
 
+    def total
+      base_query.sum('max_candidates')
+    end
+
+    def total_approved_applications_count
+      base_query.sum('approved_applications_count')
+    end
+
     def grouped_by_publicy
       base_query.grouped_by_publicy
     end
