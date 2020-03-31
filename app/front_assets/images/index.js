93498861149c7da38ec7badcee4daaa0b68bae84
Martin <martin@sharypic.com>
2020-03-31 17:18:16 +0200

    feat(homepage): new with covid updates

--- app/front_assets/images/index.js
+++ app/front_assets/images/index.js
@@ -9,25 +9,36 @@ import './onboarding/confirm-mobile.svg';
 import './pages/logo_onisep.jpg';
 import './pages/logo_cget_texte.png';
 
-import './homepage/icon_documents_utiles.svg';
-import './homepage/benefices/icon-accompagner-les-eleves.png';
-import './homepage/benefices/icon-offrir-un-stage.png';
-import './homepage/benefices/icon-trouver-un-stage.png';
+import './homepage/explications.svg';
+import './homepage/illustration-documents-utiles.svg';
+import './homepage/scroll_down.svg';
 
-import './homepage/partenaires/Logo-onisep.jpg';
+import './homepage/covid-bonne-pratique/mouchoir-a-usage-unique.svg';
+import './homepage/covid-bonne-pratique/se-laver-les-mains.svg';
+import './homepage/covid-bonne-pratique/se-saluer-a-distance.svg';
+import './homepage/covid-bonne-pratique/tousser-dans-son-coude.svg';
+
+import './homepage/covid-alternatives/telemaque.jpg';
+import './homepage/covid-alternatives/jobirl-tutorat.jpg';
+
+import './homepage/benefices/icon-accompagner-les-eleves.svg';
+import './homepage/benefices/icon-offrir-un-stage.svg';
+import './homepage/benefices/icon-trouver-un-stage.svg';
+
 import './homepage/partenaires/Logo-face.jpg';
 import './homepage/partenaires/Logo-degunsanstage.jpg';
 import './homepage/partenaires/Logo-jobirl.jpg';
 import './homepage/partenaires/Logo-le-reseau.jpg';
 import './homepage/partenaires/Logo-les-entreprises-pour-la-cite.jpg';
-import './homepage/partenaires/Logo-myfuture.png';
+import './homepage/partenaires/Logo-myfuture.jpg';
 import './homepage/partenaires/Logo-tous-en-stage.jpg';
 import './homepage/partenaires/Logo-un-stage-et-apres.jpg';
 import './homepage/partenaires/Logo-viens-voir-mon-taf.jpg';
-import './homepage/partenaires/Logo-jndj.png';
-import './homepage/partenaires/Logo-united-way.png';
+import './homepage/partenaires/Logo-jndj.jpg';
+import './homepage/partenaires/Logo-united-way.jpg';
 
-import './homepage/sponsors/Logo-MCTRCT-CGET.png';
+import './homepage/sponsors/Logo-ministere-de-la-cohesion-des-territoire.png';
+import './homepage/sponsors/Logo-anct.png';
 import './homepage/sponsors/Logo-ministere-education-nationale.png';
 
 import './documents_utiles/CarnetStagePro_USEA_WEB-2.jpg';
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def main_partners_map_of_logo_url
-    {
-      "Logo-onisep.jpg" => "https://www.onisep.fr/"
-    }.sort.to_h
-  end
-
   def partners_map_of_logo_url
     {
       "Logo-face.jpg" => "https://www.fondationface.org",
@@ -14,12 +8,12 @@ module UsersHelper
       "Logo-jobirl.jpg" => "https://www.jobirl.com",
       "Logo-le-reseau.jpg" => "http://www.lereseau.asso.fr",
       "Logo-les-entreprises-pour-la-cite.jpg" => "http://www.reseau-lepc.fr",
-      "Logo-myfuture.png" => "https://www.myfutu.re",
+      "Logo-myfuture.jpg" => "https://www.myfutu.re",
       "Logo-tous-en-stage.jpg" => "https://tousenstage.com",
       "Logo-un-stage-et-apres.jpg" => "https://www.unstageetapres.fr",
       "Logo-viens-voir-mon-taf.jpg" => "https://www.viensvoirmontaf.fr",
-      "Logo-jndj.png" => "https://jndj.org",
-      "Logo-united-way.png" => "https://uwafrance.org",
+      "Logo-jndj.jpg" => "https://jndj.org",
+      "Logo-united-way.jpg" => "https://uwafrance.org",
     }.sort.to_h
   end
 end
