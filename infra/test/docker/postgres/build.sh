b59f1787d2b55b576fde1dafeef4ffddc119298c
Martin <martin@sharypic.com>
2020-04-07 06:56:42 +0200

    tech(docker): switch to team account

--- infra/test/docker/postgres/build.sh
+++ infra/test/docker/postgres/build.sh
@@ -2,4 +2,4 @@
 set -x
 set -o nounset
 
-docker build -t mfourcade/postgres-12-alpine-postgis-synonym:$1 .circleci/docker/postgres
+docker build -t monstage/postgres-12-alpine-postgis-synonym:$1 .circleci/docker/postgres
--- infra/test/docker/postgres/publish.sh
+++ infra/test/docker/postgres/publish.sh
@@ -2,4 +2,4 @@
 set -x
 set -o nounset
 
-docker push mfourcade/postgres-12-alpine-postgis-synonym:$1
+docker push monstage/postgres-12-alpine-postgis-synonym:$1
