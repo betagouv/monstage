8cc2df2f7b0c92f4425ee87e83c25ee872fab778
Martin <martin@sharypic.com>
2020-04-10 15:57:53 +0200

    tech(deploy): ensure migrate/restart otherwise can have unsynced data

--- infra/production/deploy/promote.sh
+++ infra/production/deploy/promote.sh
@@ -3,7 +3,12 @@ set -x
 command -v sentry-cli >/dev/null 2>&1 || { echo "Deploy must ping sentry-cli to track bug from commits, install it with brew install getsentry/tools/sentry-cli. then login by creating a ~/.sentryclirc [credentials for sentri.io are in monstage.kdbx, file format can be seen: https://docs.sentry.io/cli/configuration/ and $repo/ .sentryclirc.sample]."; exit 1; }
 
 # Deploy heroku
-heroku pipelines:promote -a betagouv-monstage-staging
+HEROKU_STAGING_APP="betagouv-monstage-staging"
+HEROKU_PRODUCTION_APP="betagouv-monstage-prod"
+
+heroku pipelines:promote -a $HEROKU_STAGING_APP
+heroku rails db:migrate -a $HEROKU_PRODUCTION_APP
+heroku restart -a $HEROKU_PRODUCTION_APP
 
 # Create sentry release
 VERSION=$(sentry-cli releases propose-version)
--- infra/production/deploy/push.sh
+++ infra/production/deploy/push.sh
@@ -3,10 +3,13 @@ set -x
 
 target='production'
 git remote -vvv | grep $target
+HEROKU_APP="betagouv-monstage-staging"
 
 if [ $? -eq 0 ]; then
   command -v sentry-cli >/dev/null 2>&1 || { echo "Deploy must ping sentry-cli to track bug from commits, install it with brew install getsentry/tools/sentry-cli. then login with sentry-cli login [credentials for sentri.io are in monstage.kdbx], also"; exit 1; }
   git push $target master:master
+  heroku rails db:migrate -a $HEROKU_APP
+  heroku restart -a $HEROKU_APP
   # Create sentry release
   VERSION=$(sentry-cli releases propose-version)
   sentry-cli releases new $VERSION
