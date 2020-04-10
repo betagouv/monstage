91ee135b3e5def6c4fe6459e9d88975212422a19
Martin <martin@sharypic.com>
2020-04-10 16:56:30 +0200

    test(internship_offer_keywords#search): add missing test

--- app/controllers/internship_offer_keywords_controller.rb
+++ app/controllers/internship_offer_keywords_controller.rb
@@ -1,7 +1,7 @@
 class InternshipOfferKeywordsController < ApplicationController
   protect_from_forgery with: :null_session
   def search
-    render json: InternshipOfferKeyword.search(params[:keyword]),
+    render json: InternshipOfferKeyword.search(params.require(:keyword)),
            status: 200
   end
 end
---
+++ test/controllers/internship_offer_keywords_controller_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class InternshipOfferKeywordsControllerTest < ActionDispatch::IntegrationTest
+  include ::ApiTestHelpers
+
+  test '#post search with nothing work' do
+    post search_internship_offer_keywords_path, params:{ keyword: 'bim' }
+    assert_response :success
+  end
+
+  test '#post search with existing find it' do
+    create(:internship_offer, title: 'Horticuleur',
+                              description: 'Des plantes, des fleurs, des légumes',
+                              employer_description: 'De la nature, du bien être')
+    SyncInternshipOfferKeywordsJob.perform_now
+
+    post(search_internship_offer_keywords_path, params:{ keyword: 'Hortic' })
+
+    assert_response :success
+    expected_keyword = InternshipOfferKeyword.where(word: 'horticuleur').first
+    assert_equal expected_keyword.as_json, json_response[0]
+  end
+end
