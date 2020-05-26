b36d423c6da91f80fae02c152498d64bdc8e8644
Martin <martin@sharypic.com>
2020-05-26 07:51:11 +0200

    fix(test/system/internship_application): rich_textify filling of student motivation

--- app/front_assets/controllers/footer_controller.js
+++ app/front_assets/controllers/footer_controller.js
@@ -1,8 +1,12 @@
 import $ from 'jquery';
 import { Controller } from 'stimulus';
-
+import { hideElement } from '../utils/dom';
 export default class extends Controller {
   static targets = ['placeholder', 'fixedContent'];
+
+  close() {
+    hideElement($(this.element));
+  }
 
   resize() {
     $(this.placeholderTarget).height($(this.fixedContentTarget).height());
