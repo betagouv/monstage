f648e7445f2d3bb3a46f192504dd1508d5384f0f
Martin <martin@sharypic.com>
2020-05-28 15:55:24 +0200

    doc(enum): explain roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
 
   include DelayedDeviseEmailSender
 
+  # school_managements includes different roles
+  # 1. school_manager should register with ac-xxx.fr email
+  # 2.3.4. can register
   enum role: {
     school_manager: 'school_manager',
     teacher: 'teacher',
