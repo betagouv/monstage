e9331ac9da9caccdbee928bc0bf9182565c774a0
Martin <martin@sharypic.com>
2020-06-18 07:35:43 +0200

    tech(test): move w3c to system test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,8 @@ Minitest::Retry.use!(
     ActionView::Template::Error, # during test, sometimes fails on "unexpected token at ''", not fixable
     PG::InternalError,           # sometimes postgis ref system is not yet ready
     DRb::DRbRemoteError,         # sometimes capybara is too quick to run a test
-    Selenium::WebDriver::Error::UnknownError # sometimes Selenium is not yet ready
+    Selenium::WebDriver::Error::UnknownError, # sometimes Selenium is not yet ready
+    Selenium::WebDriver::Error::StaleElementReferenceError # again selenium...
   ]
 )
 
