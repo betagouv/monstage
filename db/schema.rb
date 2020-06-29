38bdb92935925cba4ad101f7d76d2c6e222acc3d
Martin <martin@sharypic.com>
2020-06-29 17:37:16 +0200

    test(devise#signin): start cleanup of devise test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_080019) do
+ActiveRecord::Schema.define(version: 2020_06_29_133744) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "autoinc"
