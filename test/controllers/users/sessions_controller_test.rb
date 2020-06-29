8d7691b250058db04409e480fdf440604eead794
Martin <martin@sharypic.com>
2020-06-29 17:24:29 +0200

    feat(user#signin): remove not null index on email, fix sign in with channel nested within user param, add tests

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -24,9 +24,35 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   test 'POST session not confirmed render warning with icon' do
     pwd = 'okokok'
     student = create(:student, password: pwd, confirmed_at: nil)
-    post user_session_path(params: { user: { email: student.email, password: pwd } })
+    post user_session_path(params: { user: { channel: 'email',
+                                             email: student.email,
+                                             password: pwd } })
     assert_response :found
     follow_redirect!
-    assert_select "#alert-warning #alert-text", text: 'Un message d’activation vous a été envoyé par courrier électronique. Veuillez suivre les instructions qu’il contient.'
+    assert_select "#alert-warning #alert-text", text: 'Un message d’activation vous a été envoyé par courrier électronique. Veuillez suivre les instructions qu’il contient.', count: 1
+  end
+
+  test 'POST session with phone' do
+    pwd = 'okokok'
+    phone = "+33637607756"
+    student = create(:student, email:nil, phone: phone, password: pwd, confirmed_at: 2.days.ago)
+    post user_session_path(params: { user: { channel: 'phone',
+                                             phone: student.phone,
+                                             password: pwd } })
+    assert_response :found
+    follow_redirect!
+    assert_select "a[href=?]", account_path
+  end
+
+  test 'POST session with email' do
+    pwd = 'okokok'
+    email = 'fourcade.m@gmail.com'
+    student = create(:student, email: email, phone: nil, password: pwd, confirmed_at: 2.days.ago)
+    post user_session_path(params: { user: { channel: 'email',
+                                             phone: student.email,
+                                             password: pwd } })
+    assert_response :found
+    follow_redirect!
+    assert_select "a[href=?]", account_path
   end
 end
