bb2459c582346a8ed3203ea6c08ac45210d45760
Martin <martin@sharypic.com>
2020-06-30 10:47:01 +0200

    test(users#confirmation): add test on user confirmation

--- test/controllers/users/confirmations_controller_test.rb
+++ test/controllers/users/confirmations_controller_test.rb
@@ -3,11 +3,37 @@
 require 'test_helper'
 
 class ConfirmationsControllerTest < ActionDispatch::IntegrationTest
-  test 'GET with valid token and not confirmed user' do
+  test 'GET confirmation by email with with valid token and not confirmed user' do
     student = create(:student, confirmed_at: nil, confirmation_token: 'abc')
     get user_confirmation_path(confirmation_token: student.confirmation_token)
     assert_redirected_to new_user_session_path(email: student.email)
     follow_redirect!
     assert_select("#alert-success #alert-text", text: "Votre compte est bien confirmé. Vous pouvez vous connecter.")
+  end
+
+  test 'GET#new_user_confirmation' do
+    student = create(:student, confirmed_at: nil)
+    get new_user_confirmation_path()
+    assert_response :success
+  end
+
+  test 'CREATE#user_confirmation by phone' do
+    student = create(:student, phone: '+33637607756',
+                               email: nil,
+                               confirmed_at: nil)
+    assert_enqueued_jobs 1, only: SendSmsJob do
+      post user_confirmation_path(user: {channel: :phone, phone: student.phone})
+    end
+    assert_redirected_to users_registrations_phone_standby_path(phone: student.phone)
+  end
+
+  test 'CREATE#user_confirmation by email' do
+    student = create(:student, phone: nil,
+                               email: 'fourcade.m@gmail.com',
+                               confirmed_at: nil)
+    assert_enqueued_emails 1 do
+      post user_confirmation_path(user: {channel: :email, email: student.email})
+    end
+    assert_redirected_to new_user_session_path
   end
 end
