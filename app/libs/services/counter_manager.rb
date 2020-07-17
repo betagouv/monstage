2cf1d2c962d0424d07e39b1f2c75d74b73a8e95b
Martin <martin@sharypic.com>
2020-07-17 07:56:59 +0200

    clean(counter_manager): sync fields with managed counters on internship_offers

--- app/libs/services/counter_manager.rb
+++ app/libs/services/counter_manager.rb
@@ -6,13 +6,14 @@ module Services
   class CounterManager
     def self.reset_internship_offer_counters
       InternshipOffer.update_all(
-        blocked_weeks_count: 0,
         total_applications_count: 0,
-        convention_signed_applications_count: 0,
-        approved_applications_count: 0,
-        submitted_applications_count: 0,
-        rejected_applications_count: 0,
         total_male_applications_count: 0,
+        submitted_applications_count: 0,
+        approved_applications_count: 0,
+        total_male_approved_applications_count: 0,
+        total_custom_track_approved_applications_count: 0,
+        rejected_applications_count: 0,
+        convention_signed_applications_count: 0,
         total_male_convention_signed_applications_count: 0,
         total_custom_track_convention_signed_applications_count: 0
       )
