6f2f63ca24fb1a5f93a47666094bb42cd1a23841
Martin <martin@sharypic.com>
2020-07-20 16:33:05 +0200

    feat(student.signup): group class rooms

--- app/front_assets/components/search_school/ClassRoomInput.jsx
+++ app/front_assets/components/search_school/ClassRoomInput.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-
+const TRADS = {
+  troisieme_generale: '3e',
+  troisieme_prepa_metier: '3e prépa-métiers',
+  troisieme_segpa: 'Segpa',
+  bac_pro: 'Bac pro',
+}
 function RenderClassRoomsInput({selectedClassRoom, classRoomsSuggestions, resourceName, existingClassRoom, classes}) {
     const isWaitingSchoolSelection =
       classRoomsSuggestions === null && !selectedClassRoom && !existingClassRoom;
     const isAlreadySelected = classRoomsSuggestions === null && existingClassRoom;
     const hasPendingSuggestion = classRoomsSuggestions && classRoomsSuggestions.length > 0;
+
+    const renderClassRoomOption = classRoom => (
+      <option
+        key={`class-room-${classRoom.id}`}
+        value={classRoom.id}
+        selected={selectedClassRoom && selectedClassRoom.id === classRoom.id}
+      >
+        {classRoom.name}
+      </option>
+    )
+
+    const classRoomsSuggestionsByType = (classRoomsSuggestions || []).reduce((accu, item) => {
+      if (!accu[item.school_track]) {
+        accu[item.school_track] = []
+      }
+      accu[item.school_track].push(item)
+      accu[item.school_track].sort((a,b) => {
+        if(a.name < b.name) { return -1; }
+        if(a.name > b.name) { return 1; }
+        return 0;
+      })
+      return accu
+    }, {})
 
     return (
       <div className={`form-group custom-label-container ${isWaitingSchoolSelection ? 'opacity-05' : ''}`}>
@@ -52,15 +80,13 @@ function RenderClassRoomsInput({selectedClassRoom, classRoomsSuggestions, resour
                   -- Veuillez choisir une classe --
                 </option>
               )}
-              {(classRoomsSuggestions || []).map(classRoom => (
-                <option
-                  key={`class-room-${classRoom.id}`}
-                  value={classRoom.id}
-                  selected={selectedClassRoom && selectedClassRoom.id === classRoom.id}
-                >
-                  {classRoom.name}
-                </option>
+              {Object.keys(classRoomsSuggestionsByType).length > 1 && Object.keys(classRoomsSuggestionsByType).map(schoolTrack => (
+                <optgroup label={TRADS[schoolTrack]}>
+                  {classRoomsSuggestionsByType[schoolTrack].map(renderClassRoomOption)}
+                </optgroup>
               ))}
+
+              {Object.keys(classRoomsSuggestionsByType).length <= 1 && (classRoomsSuggestions || []).map(renderClassRoomOption)}
             </select>
             <label htmlFor={`${resourceName}_class_room_id`}>Classe</label>
           </>
