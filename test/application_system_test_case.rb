45871b4b2fbedc87a9df9abda0d6d85fce9d5860
Martin <martin@sharypic.com>
2020-07-20 08:51:19 +0200

    feat(test/system): add firefox

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,6 +3,5 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # driven_by :selenium, using: ENV['CHROME'] ? :chrome : :headless_chrome
-  driven_by :selenium, using: :firefox
+    driven_by :selenium, using: ENV.fetch('CHROME') { 'headless_chrome' }
 end
