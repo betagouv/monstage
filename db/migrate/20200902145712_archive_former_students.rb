0f3ab52bfc2303a4aaed4191c37e0cd0e629eee7
Martin <martin@sharypic.com>
2020-09-02 17:17:34 +0200

    rgpd(archive): archive students via migration

---
+++ db/migrate/20200902145712_archive_former_students.rb
@@ -0,0 +1,6 @@
+class ArchiveFormerStudents < ActiveRecord::Migration[6.0]
+  def change
+    Services::StudentArchiver.new(begins_at: Date.new(2019, 9, 1), ends_at: Date.new(2020, 8, 31))
+                             .run
+  end
+end
