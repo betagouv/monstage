253891386a189e37cdb0c1da30d2035b44666f45
Martin <martin@sharypic.com>
2020-09-08 18:20:21 +0200

    fix(sidekiq): queue_name_prefix is not namespace, avoid using it

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Monstage
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.active_job.queue_adapter = :sidekiq
-    config.active_job.queue_name_prefix = "monstage_#{Rails.env}"
 
     config.public_file_server.enabled = true
 
