d37a18d205351aad44b903acfbec65e59c812828
Martin <martin@sharypic.com>
2020-09-08 16:51:24 +0200

    fix(raven): try to disable sentry raven which fails to forward an error

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
-  config.dsn = Rails.application.credentials.sentry_dns
-  config.environments = %w[review staging production]
+# Raven.configure do |config|
+#   config.dsn = Rails.application.credentials.sentry_dns
+#   config.environments = %w[review staging production]
 
-  # works async, if it fails, goes in queue
-  config.async = lambda { |event| SentryJob.perform_later(event) }
+#   # works async, if it fails, goes in queue
+#   config.async = lambda { |event| SentryJob.perform_later(event) }
 
-  # record post data, helps with debug
-  config.processors -= [Raven::Processor::PostData]
+#   # record post data, helps with debug
+#   config.processors -= [Raven::Processor::PostData]
 
-  # by recording posts data, ensure we do not track password fields
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-end
+#   # by recording posts data, ensure we do not track password fields
+#   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+# end
