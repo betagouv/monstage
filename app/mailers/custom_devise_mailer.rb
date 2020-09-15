9dacbfa598d6a3110d6356221019414e1d622313
Martin <martin@sharypic.com>
2020-09-15 14:53:29 +0200

    bootstrap(fix): together

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -5,6 +5,26 @@ class CustomDeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
 
   def confirmation_instructions(record, token, _opts = {})
-    super(record, token, opts = {})
+    if creating_account?
+      super(record, token, opts = {})
+    elsif updating_email?
+
+    elsif adding_email?
+    else
+      raise 'wtf?'
+    end
+  end
+
+  private
+  # user.attributes['unconfirmed_email'] : next email
+  # user.attributes['email'] : current email (may be nil for user registred by sms)
+  def creating_account?
+  end
+
+  def updating_email?
+
+  end
+
+  def adding_email?
   end
 end
