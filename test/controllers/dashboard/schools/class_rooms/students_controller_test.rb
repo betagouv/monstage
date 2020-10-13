30bf67a763a4a52bc7f0cf9a7ee571129e9c1ec0
Etienne Weil <weil.etienne@hotmail.fr>
2020-10-13 21:43:59 +0200

    fix: anonymized students not to be shown in SchoolManagement lists

--- test/controllers/dashboard/schools/class_rooms/students_controller_test.rb
+++ test/controllers/dashboard/schools/class_rooms/students_controller_test.rb
@@ -29,6 +29,26 @@ module Dashboard
         get dashboard_school_class_room_student_path(school, class_room, student)
         assert_response :success
       end
+      test 'GET index as SchoolManagement shows active students and not anonymized' do
+        school = create(:school)
+        class_room = create(:class_room, school: school)
+        student = create(:student, school: school, class_room: class_room, last_name: 'Poireau')
+        student_2 = create(:student, school: school, class_room: class_room)
+
+        sign_in(create(:school_manager, school: school))
+        get dashboard_school_class_room_path(school, class_room)
+
+        assert response.body.include?("test-student-#{student.id}")
+        assert response.body.include?(student.last_name)
+        assert response.body.include?("test-student-#{student_2.id}")
+        assert response.body.include?(student_2.last_name)
+
+        student_2.anonymize(send_email: false)
+        get dashboard_school_class_room_path(school, class_room)
+
+        refute response.body.include?("test-student-#{student_2.id}")
+        refute response.body.include?(student_2.last_name)
+      end
     end
   end
 end
