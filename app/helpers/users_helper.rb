d612d3124bd49e7abbe812e8569383404df9e4ee
Etienne Weil <weil.etienne@hotmail.fr>
2020-10-19 10:21:49 +0200

    add telemaq, moidans10 remove face degun jndj united_w

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,17 +3,15 @@
 module UsersHelper
   def partners_map_of_logo_url
     {
-      'Logo-face.jpg' => 'https://www.fondationface.org',
-      'Logo-degunsanstage.jpg' => 'http://www.degunsansstage.fr',
       'Logo-jobirl.jpg' => 'https://www.jobirl.com',
       'Logo-le-reseau.jpg' => 'http://www.lereseau.asso.fr',
+      'Logo-moidans10ans.png' => 'https://moidans10ans.fr/',
       'Logo-les-entreprises-pour-la-cite.jpg' => 'http://www.reseau-lepc.fr',
       'Logo-myfuture.jpg' => 'https://www.myfutu.re',
       'Logo-tous-en-stage.jpg' => 'https://tousenstage.com',
       'Logo-un-stage-et-apres.jpg' => 'https://www.unstageetapres.fr',
       'Logo-viens-voir-mon-taf.jpg' => 'https://www.viensvoirmontaf.fr',
-      'Logo-jndj.jpg' => 'https://jndj.org',
-      'Logo-united-way.jpg' => 'https://uwafrance.org'
+      'Logo-telemaque.jpg' => 'https://www.institut-telemaque.org/',
     }.sort.to_h
   end
 
