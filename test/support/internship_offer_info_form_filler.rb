495439a433c548336e3758ff5ac034b8b61211d8
Martin <martin@sharypic.com>
2020-10-28 04:56:00 +0100

    refactor(test/system/stepper): extract for w3c/a11y pass

---
+++ test/support/internship_offer_info_form_filler.rb
@@ -0,0 +1,13 @@
+module InternshipOfferInfoFormFiller
+  def fill_in_internship_offer_info_form(sector:, weeks:, school_track:)
+    if school_track
+      select I18n.t("enum.school_tracks.#{school_track}"),
+             from: 'internship_offer_info_school_track'
+    end
+    fill_in 'internship_offer_info_title', with: 'Stage de dev @betagouv.fr ac Brice & Martin'
+    select sector.name, from: 'internship_offer_info_sector_id' if sector
+    find('#internship_offer_info_description_rich_text', visible: false).set("Le dev plus qu'une activité, un lifestyle.\n Venez découvrir comment creer les outils qui feront le monde de demain")
+    find('label', text: 'Individuel').click
+  end
+
+end
