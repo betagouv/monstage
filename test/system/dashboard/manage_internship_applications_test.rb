23f9c21075007350cf90a0a82277d8b663bf67cb
Martin <martin@sharypic.com>
2020-10-29 17:11:37 +0100

    fix(test): broken due to change in html for aria

--- test/system/dashboard/manage_internship_applications_test.rb
+++ test/system/dashboard/manage_internship_applications_test.rb
@@ -116,7 +116,7 @@ module Dashboard
       find "div[data-test-id=\"internship-application-#{early_application_for_week_2.id}\"]", count: 1
 
       click_link(internship_offer.title)
-      find('p.h3', text: internship_offer.title, exact_text: true)
+      find('div.h3', text: internship_offer.title, exact_text: true)
     end
 
     test 'weekly_internship_applications show student details anyway' do
