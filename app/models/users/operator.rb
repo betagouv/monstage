5efe0054405cabdf1d51fa8baea76e2b26aa313a
Etienne Weil <weil.etienne@hotmail.fr>
2020-11-02 13:44:35 +0100

    fix: relationship does not keep the internship_offers kept constraint

--- app/models/users/operator.rb
+++ app/models/users/operator.rb
@@ -8,7 +8,7 @@ module Users
     has_many :internship_offers, as: :employer,
                                  dependent: :destroy
 
-    has_many :internship_applications, through: :kept_internship_offers
+    has_many :internship_applications, through: :internship_offers
 
     before_create :set_api_token
 
