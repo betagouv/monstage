b0ce28190b3bc54fe4fc6991f01d2749222c01d1
Etienne Weil <weil.etienne@hotmail.fr>
2020-11-09 15:12:58 +0100

    feat: trix content focus , disabling and abilities for employer and school_manager

--- app/front_assets/controllers/rich_text_label_controller.js
+++ app/front_assets/controllers/rich_text_label_controller.js
@@ -1,17 +1,25 @@
-import $ from 'jquery';
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
   static targets = ['label'];
 
+  forElement(){
+    return document.getElementById(this.data.get('for'))
+  }
   focusTrixInput(event) {
-    document.getElementById(this.data.get('for'))
-            .focus();
+    this.forElement().focus();
 
     return event;
   }
+  enableTrixInput(bool){
+    this.forElement().contentEditable = bool
+  }
+
   connect(){
     this.element.addEventListener('click', this.focusTrixInput.bind(this))
+  }
+  initialize(){
+   this.enableTrixInput(this.data.get('enable'))
   }
   disconnect(){
     this.element.removeEventListener('click', this.focusTrixInput.bind(this))
