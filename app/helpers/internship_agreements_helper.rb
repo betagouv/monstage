867405f66d96fe27ed8143bde517adfe13bce57d
Etienne Weil <weil.etienne@hotmail.fr>
2020-11-18 18:14:55 +0100

    fix : after review

--- app/helpers/internship_agreements_helper.rb
+++ app/helpers/internship_agreements_helper.rb
@@ -1,8 +1,8 @@
 module InternshipAgreementsHelper
   def current_user_agreement_terms(user)
     return nil unless user
-    return :school_manager_accept_terms if user.is_a?(Users::SchoolManagement) && user.role == 'school_manager'
-    return :main_teacher_accept_terms if user.is_a?(Users::SchoolManagement) && user.role == 'main_teacher'
+    return :school_manager_accept_terms if user.school_manager?
+    return :main_teacher_accept_terms if user.main_teacher?
     return :employer_accept_terms if user.is_a?(Users::Employer)
     raise  ArgumentError, "#{user.type} does not support accept terms yet "
   end
