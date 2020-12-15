bee9ec1157467ab6027a1b1f788b859b6693443c
Martin <martin@sharypic.com>
2020-12-15 14:15:22 +0100

    fix(system.tests): with js validation on select week, checking backend error was no more possible

--- test/system/dashboard/support_ticket_test.rb
+++ test/system/dashboard/support_ticket_test.rb
@@ -12,17 +12,19 @@ class SupportTicketTest < ApplicationSystemTestCase
     visit school_manager.custom_dashboard_path
     click_link('Contactez-nous !')
     find('.h4.text-body', text: "Contactez-nous, nous vous mettrons en lien avec nos associations partenaires.")
+    click_on "Envoyer la demande"
 
+    # js validation
+    find(".form-text.text-danger", text: 'Veuillez saisir au moins une semaine de stage')
+    all(".custom-control-checkbox-list label").first.click
     click_on "Envoyer la demande"
 
     find('label', text: "L'un des trois modes 'Semaine digitale', 'Webinaire' ou 'En présentiel' doivent être sélectionnés")
-    find('label', text: "Il faudrait sélectionner au moins une semaine cible pour vos étudiants")
     find('label', text: "Il manque à cette demande le nombre d'étudiants concernés")
     find('label', text: "Le nombre d'étudiants devrait être chiffré")
     assert_enqueued_with(job: SupportTicketJobs::SchoolManager) do
+      all(".custom-control-checkbox-list label").first.click
       check 'support_ticket[webinar]'
-      selectable_week = Week.selectable_from_now_until_end_of_school_year.first
-      find("label[for=\"support_ticket_week_ids_#{selectable_week.id}_checkbox\"]").click
       select "2 classes"
       fill_in("support_ticket_students_quantity", with: 5)
       click_on "Envoyer la demande"
@@ -38,14 +40,18 @@ class SupportTicketTest < ApplicationSystemTestCase
     find('.h4.text-body', text: "Vous souhaitez participer à la mise en place de stages à distance ?")
 
     click_on "Envoyer la demande"
+
+    # js validation
+    find(".form-text.text-danger", text: 'Veuillez saisir au moins une semaine de stage')
+    all(".custom-control-checkbox-list label").first.click
+
+    click_on "Envoyer la demande"
     find('label', text: "L'un des trois modes 'Semaine digitale', 'Webinaire' ou 'En présentiel' doivent être sélectionnés")
-    find('label', text: "Il faudrait sélectionner au moins une semaine cible pour vos étudiants")
     find('label', text: "Il manque à cette demande le nombre d'intervenants")
     find('label', text: "Il manque à cette demande le nombre de métiers abordés")
     assert_enqueued_with(job: SupportTicketJobs::Employer) do
+      all(".custom-control-checkbox-list label").first.click
       check 'support_ticket[webinar]'
-      selectable_week = Week.selectable_from_now_until_end_of_school_year.first
-      find("label[for=\"support_ticket_week_ids_#{selectable_week.id}_checkbox\"]").click
       select "1 intervenant"
       select "1 métier"
       click_on "Envoyer la demande"
