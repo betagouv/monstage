54bd2ac020ef76531676636f02c954de3bceb4a9
Martin <martin@sharypic.com>
2020-12-16 15:17:01 +0100

    fix(sentry#2078714993): private method called

--- app/jobs/add_contact_to_sync_email_delivery_job.rb
+++ app/jobs/add_contact_to_sync_email_delivery_job.rb
@@ -8,6 +8,6 @@ class AddContactToSyncEmailDeliveryJob < ActiveJob::Base
   def perform(user:)
     sync_delivery_service = Services::SyncEmailDelivery.new
     sync_delivery_service.create_contact(user: user) unless sync_delivery_service.contact_exists?(email: user.email)
-    sync_delivery_service.send_update_contact_metadata(user: user)
+    sync_delivery_service.update_contact_metadata(user: user)
   end
 end
