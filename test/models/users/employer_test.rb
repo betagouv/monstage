952e74473d131fc15a4a57e442b9a68267f09803
Martin <martin@sharypic.com>
2020-12-17 09:39:24 +0100

    refactor(employers.kept_internship_offer): remove source from association definition and extend test

--- test/models/users/employer_test.rb
+++ test/models/users/employer_test.rb
@@ -8,5 +8,31 @@ module Users
       assert_equal(employer.after_sign_in_path,
                    Rails.application.routes.url_helpers.dashboard_internship_offers_path)
     end
+
+    test '(rails6.1 upgrade) employer.kept_internship_offers' do
+      employer = create(:employer)
+      kept_internship_offer = create(:weekly_internship_offer, employer: employer)
+      discarded_internship_offer = create(:weekly_internship_offer, employer: employer)
+      discarded_internship_offer.discard
+
+      assert_equal 1, employer.kept_internship_offers.count
+      assert_includes employer.kept_internship_offers, kept_internship_offer
+      refute_includes employer.kept_internship_offers, discarded_internship_offer
+    end
+
+
+    test '(rails6.1 upgrade) employer.internship_applications' do
+      employer = create(:employer)
+      kept_internship_offer = create(:free_date_internship_offer, employer: employer)
+      discarded_internship_offer = create(:free_date_internship_offer, employer: employer)
+      kept_internship_application = create(:free_date_internship_application, internship_offer: kept_internship_offer)
+      discarded_internship_application = create(:free_date_internship_application, internship_offer: discarded_internship_offer)
+
+      discarded_internship_offer.discard
+
+      assert_equal 1, employer.internship_applications.count
+      assert_includes employer.internship_applications, kept_internship_application
+      refute_includes employer.internship_applications, discarded_internship_application
+    end
   end
 end
