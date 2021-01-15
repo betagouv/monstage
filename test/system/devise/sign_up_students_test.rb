a6da52b771b080367a13bd0e1633ae44e2e6a7b7
Martin <martin@sharypic.com>
2021-01-15 10:39:37 +0100

    fix(test/system): again we are in a loop regarding this CI test...

--- test/system/devise/sign_up_students_test.rb
+++ test/system/devise/sign_up_students_test.rb
@@ -94,8 +94,8 @@ class SignUpStudentsTest < ApplicationSystemTestCase
       execute_script("document.getElementById('phone-input').value = '#{existing_phone}';")
       fill_in 'Créer un mot de passe', with: 'kikoololletest'
       fill_in 'Ressaisir le mot de passe', with: 'kikoololletest'
-      # execute_script("document.getElementById('user_accept_terms').checked = true")
-      find('label[for="user_accept_terms"]').click
+      execute_script("document.getElementById('user_accept_terms').checked = true;")
+      # find('label[for="user_accept_terms"]').click
       safe_submit
     end
 
