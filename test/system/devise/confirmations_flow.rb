b3a777ec052e516eb1412d1240faa98779569c64
Etienne Weil <weil.etienne@hotmail.fr>
2021-01-26 17:37:15 +0100

    feat(test): adding tests for phone number and confirmations requests

--- test/system/devise/confirmations_flow.rb
+++ test/system/devise/confirmations_flow.rb
@@ -24,11 +24,9 @@ class ConfirmationFlowTest < ApplicationSystemTestCase
   end
 
   test 'ask notification confirmed with phone' do
-    password = 'kikoolol'
     phone = '+330637607756'
     user = create(:student, email: nil,
                             phone: phone,
-                            password: password,
                             confirmed_at: nil)
 
     visit new_user_confirmation_path
@@ -42,5 +40,33 @@ class ConfirmationFlowTest < ApplicationSystemTestCase
       fill_in 'Code de confirmation', with: user.phone_token
       click_on 'Valider'
     end
+  end
+
+  test 'register confirmation done with wrong phone number' do
+    phone       = '+330637607756'
+    wrong_phone = '+330637607750'
+    user = create(:student, email: nil,
+                            phone: phone,
+                            confirmed_at: nil)
+
+    visit new_user_confirmation_path
+    find('label', text: 'SMS').click
+    execute_script("document.getElementById('phone-input').value = '#{wrong_phone}';")
+    click_on 'Renvoyer'
+    find('#error_explanation label', text:'Veuillez saisir un email ou un numéro de téléphone')
+  end
+
+  test 'register confirmation confirmed with phone number while subscribed with email' do
+    phone = '+330637607756'
+    user  = create(:student, email: 'test@test.fr',
+                             phone: nil,
+                             confirmed_at: nil)
+
+    visit new_user_confirmation_path
+    find('label', text: 'SMS').click
+    execute_script("document.getElementById('phone-input').value = '#{phone}';")
+    click_on 'Renvoyer'
+    find '#error_explanation label',
+         text: 'Veuillez saisir un email ou un numéro de téléphone'
   end
 end
