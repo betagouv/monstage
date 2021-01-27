76d0c928147bdfa1e8c1c69f5ca65abe3e842e27
Maxime Pierrot <maxime.pierrot@gmail.com>
2021-01-27 11:04:38 +0100

    Fix tests

--- test/controllers/footer_test.rb
+++ test/controllers/footer_test.rb
@@ -4,17 +4,20 @@ require 'test_helper'
 
 class FooterTest < ActionDispatch::IntegrationTest
   test 'presence of footer links within valid rg2a footer' do
-    get root_path
+    mock_prismic = Marshal.load(File.read(Rails.root.join('test', 'fixtures', 'files', 'prismic-homepage-response.dump')))
+    PrismicFinder.stub(:homepage, mock_prismic) do
+      get root_path
 
-    assert_select('a[href=?]',
-                  'https://beta.gouv.fr/startups/monstage.html')
-    assert_select('a[href=?]', mentions_legales_path)
-    assert_select('a[href=?]', conditions_d_utilisation_path)
-    assert_select('a[href=?]', accessibilite_path)
-    assert_select('a[href=?]', accessibilite_path)
-    assert_select(
-      'a[href=?]',
-      "#{root_url}/modes_d_emploi/MS3_Guide-d-utilisation-global-2020.pdf"
-    )
+      assert_select('a[href=?]',
+                    'https://beta.gouv.fr/startups/monstage.html')
+      assert_select('a[href=?]', mentions_legales_path)
+      assert_select('a[href=?]', conditions_d_utilisation_path)
+      assert_select('a[href=?]', accessibilite_path)
+      assert_select('a[href=?]', accessibilite_path)
+      assert_select(
+        'a[href=?]',
+        "#{root_url}/modes_d_emploi/MS3_Guide-d-utilisation-global-2020.pdf"
+      )
+    end
   end
 end
