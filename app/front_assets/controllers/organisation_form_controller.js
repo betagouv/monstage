c0d34198eb735d1aa447ef7e5645883f20046178
Maxime Pierrot <maxime.pierrot@gmail.com>
2021-02-09 18:08:37 +0100

    API sirene

--- app/front_assets/controllers/organisation_form_controller.js
+++ app/front_assets/controllers/organisation_form_controller.js
@@ -4,6 +4,9 @@ import { showElement, hideElement } from '../utils/dom';
 
 export default class extends Controller {
   static targets = [
+    'organisationName',
+    'invalidFeedback',
+    'validFeedback',
     'groupBlock',
     'groupLabel',
     'groupNamePublic',
@@ -78,6 +81,54 @@ export default class extends Controller {
       $(this.groupNamePublicTargets).hide();
       $(this.groupNamePrivateTargets).show();
     }
+  }
+
+  checkSiren(event) {
+    this.hideFeedbackMessages();
+    const siren = event.target.value.replace(/\s/g, '');
+    const sirenToken = `${Rails.application.credentials.api_sirene.token}`;
+    //const sirenToken = 'xxx';
+    if (siren.length === 9) {
+      const Params = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${sirenToken}`,
+        },
+        method: "GET"
+      }
+      const SirenUrl = `https://api.insee.fr/entreprises/sirene/V3/siret?q=siren:${siren}`;
+      fetch(SirenUrl, Params)
+        .then(data=> {
+          if (data.status === 200) {
+            $(this.validFeedbackTarget).show();
+            return data.json();
+          } else {
+            this.invalidSiren();
+            return
+          }
+        })
+        .then(res=> {
+          $(this.organisationNameTarget).val(res.etablissements[0].uniteLegale.denominationUniteLegale);
+        })
+        .catch(error=> {
+          this.invalidSiren();
+        })
+    } else {
+      if (siren.length > 0) {
+        this.invalidSiren();
+      }
+    }
+    return
+  }
+
+  invalidSiren() {
+    $(this.invalidFeedbackTarget).show();
+    $(this.organisationNameTarget).val('');
+  }
+
+  hideFeedbackMessages() {
+    $(this.invalidFeedbackTarget).hide();
+    $(this.validFeedbackTarget).hide();
   }
 
   connect() {
