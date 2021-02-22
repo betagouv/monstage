8c0951dbbde4c8295771952bc9d30446a97e78d3
Etienne Weil <weil.etienne@hotmail.fr>
2021-02-22 17:01:06 +0100

    fix(after review): using floating labels

--- app/front_assets/components/FilterInternshipOffer.jsx
+++ app/front_assets/components/FilterInternshipOffer.jsx
@@ -21,14 +21,14 @@ function FilterInternshipOffer({ filterOptions }) {
     changeURLFromEvent(event,'school_track')
   }
   return (
-    <div className="form-group form-inline justify-content-center justify-content-sm-start justify-content-md-center p-0 m-0 custom-radio-boxes">
+    <div className="form-group form-inline justify-content-center p-0 m-0 custom-radio-boxes">
       <span className="font-weight-normal justify-content-sm-center mr-1">{filterOptions.component_label} : </span>
       {filterOptions.options.map(
         function (buttonLabel, index) {
           let extraClass = (index == 0) ? "custom-radio-box-control-prepend" : ""
           extraClass = (index == (filterOptions.options.length - 1)) ? "custom-radio-box-control-append" : extraClass
           return (
-            <div className={`custom-radio-box-control d-none d-md-block ${extraClass}`} key={index}>
+            <div className={`custom-radio-box-control d-none d-sm-block ${extraClass}`} key={index}>
               <input
                 type="radio"
                 name="school_track"
