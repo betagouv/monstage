cb8f153e5c9678080ffb9a0b5d1da146a026fe70
mfo <mfo@users.noreply.github.com>
2021-03-11 17:14:41 +0100

    Update app/components/internship_agreements/progress_component.rb

--- app/components/internship_agreements/progress_component.rb
+++ app/components/internship_agreements/progress_component.rb
@@ -3,6 +3,7 @@ module InternshipAgreements
    def curret_role(role_attribute_name:)
       matches = role_attribute_name.to_s.match(/(?<role>.*)_accept_terms/)
       raise ArgumentError, "#{user.type} does not support accept terms yet " if matches[:role].empty?
+
       matches[:role]
     end
 
