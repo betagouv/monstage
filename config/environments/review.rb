9d44892bf79982705cc3b5a1d956f5eac7ec2892
Martin <martin@sharypic.com>
2021-03-12 10:35:03 +0100

    feat(mailer#previews): allow preview of email in review app

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "monstage_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  # config.action_mailer.preview_path  = "#{Rails.root}/whatever"
+  config.action_mailer.show_previews = true
 
   config.action_mailer.default_url_options = { host: HOST }
 
