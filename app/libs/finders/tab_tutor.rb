ee943c9c022562b069f7125cf48fe4e62543b018
Etienne Weil <weil.etienne@hotmail.fr>
2021-04-12 17:31:08 +0200

    feat(tab_tutor): wip

---
+++ app/libs/finders/tab_tutor.rb
@@ -0,0 +1,33 @@
+module Finders
+  class TabTutor
+    def pending_agreements_count
+      # internship_agreement approved with internship_agreement without terms_accepted
+      @pending_internship_agreement_count ||= user.internship_applications
+                                                  .approved
+                                                  .voie_pro
+                                                  .joins(:internship_agreement)
+                                                  .where(internship_agreement: {employer_accept_terms: false})
+                                                  .count
+
+      # internship_applications approved without internship_agreement
+      @to_be_created_internship_agreement ||= user.internship_applications
+                                                  .approved
+                                                  .voie_pro
+                                                  .left_outer_joins(:internship_agreement)
+                                                  .where(internship_agreement: {internship_application_id: nil})
+                                                  .count
+      [
+        @pending_internship_agreement_count,
+        @to_be_created_internship_agreement
+      ].sum
+    end
+
+    private
+
+    attr_reader :user
+
+    def initialize(user:)
+      @user = user
+    end
+  end
+end
