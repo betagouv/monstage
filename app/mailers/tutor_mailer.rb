95edf889687ebde80037d25657c30a2f50da260a
Etienne Weil <weil.etienne@hotmail.fr>
2021-04-23 17:01:21 +0200

    fix(mail): tutor invitation with/wihout link

--- app/mailers/tutor_mailer.rb
+++ app/mailers/tutor_mailer.rb
@@ -2,11 +2,17 @@
 
 class TutorMailer < ApplicationMailer
   def new_tutor(tutor_id, internship_offer_id)
-    @tutor = Users::Tutor.find(tutor_id)
     @internship_offer = InternshipOffer.find(internship_offer_id)
-    @token = @tutor.store_reset_password_token
+    tutor = Users::Tutor.find(tutor_id)
+    @registered_tutor = tutor.encrypted_password.present?
+    token = tutor.store_reset_password_token
+    @link_url = if @registered_tutor
+      dashboard_internship_offers_url
+    else
+      edit_user_password_url(reset_password_token: token)
+    end
 
-    mail(to: @tutor.email,
+    mail(to: tutor.email,
          subject: 'Vous avez été désigné tuteur de stage')
   end
 end
