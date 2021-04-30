a8f811d587a593732e7331fda74ef7b41258c724
Etienne Weil <weil.etienne@hotmail.fr>
2021-04-30 09:14:59 +0200

    fix loose condition on sign_in

--- app/controllers/users/invited_passwords_controller.rb
+++ app/controllers/users/invited_passwords_controller.rb
@@ -2,15 +2,12 @@
 
 module Users
   class InvitedPasswordsController < Devise::PasswordsController
-
     def edit
       @minimum_password_length = User.password_length.min
       @user ||= User.fetch_user_by_reset_token(params[:reset_password_token])
-      @user ||= User.find_by_reset_password_token(params[:reset_passord_token])
-      sign_in @user
+      sign_in @user if role_invited?(@user)
       authorize! :invited_set_password, @user
       render 'devise/invited_passwords/edit'
-      # render :edit
     end
 
     def update
@@ -19,9 +16,14 @@ module Users
       bypass_sign_in(current_user)
       redirect_to dashboard_internship_offers_path
     rescue ActiveRecord::RecordInvalid
-      # render 'devise/invited_passwords/edit'
       @user = current_user
       render 'devise/invited_passwords/edit'
+    end
+
+    def role_invited?(user)
+      return true if user.tutor?
+
+      false
     end
   end
 end
