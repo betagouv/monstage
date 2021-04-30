4fa26800a423c4a5581221b18487cd7efaec9182
Etienne Weil <weil.etienne@hotmail.fr>
2021-04-30 15:34:12 +0200

    fix(tab_employer): it shall count all agreements wether voie pro or 3e generale

--- app/libs/finders/tab_employer.rb
+++ app/libs/finders/tab_employer.rb
@@ -4,7 +4,6 @@ module Finders
       # internship_agreement approved with internship_agreement without terms_accepted
       @pending_internship_agreement_count ||= user.internship_applications
                                                   .approved
-                                                  .troisieme_generale
                                                   .joins(:internship_agreement)
                                                   .where(internship_agreement: {employer_accept_terms: false})
                                                   .count
@@ -12,7 +11,6 @@ module Finders
       # internship_applications approved without internship_agreement
       @to_be_created_internship_agreement ||= user.internship_applications
                                                   .approved
-                                                  .troisieme_generale
                                                   .left_outer_joins(:internship_agreement)
                                                   .where(internship_agreement: {internship_application_id: nil})
                                                   .count
