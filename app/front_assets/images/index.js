3b747f0895f29c31f103114d7de4933ef41c52f5
Etienne Weil <weil.etienne@hotmail.fr>
2021-05-18 19:19:17 +0200

    feat(reporting school view) : new looks for table

--- app/front_assets/images/index.js
+++ app/front_assets/images/index.js
@@ -19,6 +19,7 @@ import './pages/statistics/hand_left.svg';
 import './pages/statistics/hand_right.svg';
 import './pages/statistics/handshake.svg';
 import './pages/statistics/ok.svg';
+import './pages/statistics/green_check.svg';
 import './pages/statistics/school.svg';
 import './pages/statistics/reseau.png';
 import './pages/statistics/la_poste.png';
