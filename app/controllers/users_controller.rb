13284100640b49c5ee91cea51361c37d96a15d56
Etienne Weil <weil.etienne@hotmail.fr>
2021-05-28 15:43:11 +0200

    fix(deprecated) : by_pass_sign_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     authorize! :update, current_user
     if password_change_allowed?
       current_user.update!(user_params)
-      sign_in :user, current_user, bypass: true
+      bypass_sign_in(current_user)
       redirect_to  account_path(section: :password), flash: { success: current_flash_message }
     else
       redirect_to account_path(section: :password), flash: { warning: 'impossible de mettre à jour le mot de passe.' }
