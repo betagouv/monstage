551313daee140f0b252bb365d6eaf6f2944cd26d
Maxime Pierrot <maxime.pierrot@gmail.com>
2021-06-14 11:42:26 +0200

    Send report offers by email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,11 @@ class UserMailer < ApplicationMailer
   def anonymize_user(recipient_email:)
     mail(to: recipient_email, subject: 'Confirmation - suppression de votre compte')
   end
+
+  def export_offers(recipient_email, params)
+    offers = Finders::ReportingInternshipOffer.new(params: params).dimension_offer
+    xlsx = render_to_string layout: false, handlers: [:axlsx], formats: [:xlsx], template: "reporting/internship_offers/index_offers", locals: { offers: offers, presenter_for_dimension: Presenters::Reporting::DimensionByOffer }
+    attachments["offers.xlsx"] = {mime_type: Mime[:xlsx], content: xlsx}
+    mail(to: "maxime.pierrot@gmail.com", subject: 'Export Offres')
+  end
 end
