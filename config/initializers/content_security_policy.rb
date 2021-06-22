065e19f5f0d12a978f7dbca003406e528566676d
Martin <martin@sharypic.com>
2021-06-22 16:24:29 +0200

    fix(csp): try with unsafe inline for self scripts. also allow prismic / asset_host

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,14 +5,13 @@
 # Define an application-wide content security policy
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
   policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
+  policy.img_src     :self, 'https://images.prismic.io'
   policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https
+  policy.script_src  :self, "'unsafe-inline'", ENV['ASSET_HOST']
+  policy.style_src   :self, "'unsafe-inline'", ENV['ASSET_HOST']
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
 
