b52c587303c06334478c285ea637afd421cc578f
Etienne Weil <weil.etienne@hotmail.fr>
2021-07-02 16:28:02 +0200

    fix(hammer use) : on mobile only

--- app/front_assets/controllers/list_navigation_controller.js
+++ app/front_assets/controllers/list_navigation_controller.js
@@ -1,4 +1,6 @@
-import { Controller } from 'stimulus';
+import {
+  Controller
+} from 'stimulus';
 import Hammer from 'hammerjs';
 import Turbolinks from 'turbolinks';
 
@@ -11,13 +13,14 @@ export default class extends Controller {
   connect() {
     // see: http://hammerjs.github.io/tips/#i-cant-select-my-text-anymore
     delete Hammer.defaults.cssProps.userSelect;
-    this.hammer = new Hammer(document.body, { });
-
-    if (this.previousUrlValue.length > 0) {
-      this.hammer.on('swipeleft', this.previous.bind(this));
-    }
-    if (this.nextUrlValue.length > 0) {
-      this.hammer.on('swiperight', this.next.bind(this));
+    this.hammer = new Hammer(document.body, {});
+    if (navigator.userAgent.toLowerCase().match(/mobile/i)) {
+      if (this.previousUrlValue.length > 0) {
+        this.hammer.on('swipeleft', this.previous.bind(this));
+      }
+      if (this.nextUrlValue.length > 0) {
+        this.hammer.on('swiperight', this.next.bind(this));
+      }
     }
   }
 
