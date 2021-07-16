94f9a9576b0b1a0d304c37b5ddd777ba71545782
Martin <martin@sharypic.com>
2021-07-16 13:40:42 +0200

    feat(search): begin with some testing, and search by school_track

---
+++ test/controllers/internship_offers/search_test.rb
@@ -0,0 +1,16 @@
+
+class SearchTest < ActionDispatch::IntegrationTest
+  test 'GET #search render default form' do
+    get search_internship_offers_path
+
+    assert_response :success
+    assert_select "label[for=input-search-by-keyword]"
+    assert_select "label[for=input-search-by-city]"
+    assert_select ".label", text:  "Filtrer par filière"
+
+    assert_select ".label", text:  "Filtrer par filière"
+    Presenters::ClassRoom.school_tracks_list.each do |(track, enum)|
+      assert_select "input#school_track_#{enum}"
+    end
+  end
+end
