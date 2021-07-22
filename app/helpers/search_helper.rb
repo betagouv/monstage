e3a0e01466cf156c5884a08f73478b650e40e74a
Martin <martin@sharypic.com>
2021-07-22 15:01:52 +0200

    feat(search): plug calendar interactions

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,14 +1,40 @@
 module SearchHelper
+  def list_months_range
+     year_start = list_months_for_search.values
+                                        .flatten
+                                        .first
+                                        .year
+    "#{year_start} - #{year_start + 1}"
+  end
 
-  def month_in_text(month_i)
-    month_date = Date.new(Date.today.year, month_i.to_i, 1)
-    I18n.localize(month_date, format: :month_with_year).capitalize
+  def search_first_month?(month_i)
+    month_i == 9
+  end
+
+  def search_last_month?(month_i)
+    month_i == 5
+  end
+
+  def next_month(month_i)
+    month_list = list_months_for_search.keys
+    month_index = month_list.find_index(month_i)
+    month_list[month_index + 1]
+  end
+
+  def previous_month(month_i)
+    month_list = list_months_for_search.keys
+    month_index = month_list.find_index(month_i)
+    month_list[month_index - 1]
+  end
+
+  def month_in_text(month_i, opts)
+    month_date = Date.new(Date.today.year, month_i, 1)
+    I18n.localize(month_date, opts).capitalize
   end
 
   def current_month_for_search
-    default_month = list_months_for_search.keys.first.to_i
+    default_month = list_months_for_search.keys.first
     list_months_include_current_month = list_months_for_search.keys
-                                                      .map(&:to_i)
                                                       .include?(Date.today.month)
     list_months_include_current_month ?
       Date.today.month :
@@ -17,16 +43,15 @@ module SearchHelper
 
   def list_months_for_search
     months_map = {
-      '9' => [],
-      '10' => [],
-      '11' => [],
-      '12' => [],
-      '1' => [],
-      '2' => [],
-      '3' => [],
-      '4' => [],
-      '5' => [],
-      '6' => [],
+      9 => [],
+      10 => [],
+      11 => [],
+      12 => [],
+      1 => [],
+      2 => [],
+      3 => [],
+      4 => [],
+      5 => [],
     }
 
     @_list_months_for_search ||= Week.selectable_from_now_until_end_of_school_year
@@ -35,7 +60,7 @@ module SearchHelper
         beginning_of_week = week_date.beginning_of_week
         # end_of_week = week_date.end_of_week
 
-        months[beginning_of_week.month.to_s].push(week)
+        months[beginning_of_week.month].push(week)
         # months[end_of_week.month.to_s].push(week) if beginning_of_week.month != end_of_week.month
         months
       end
