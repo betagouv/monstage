87f3ac346b3173ab5031fb79a5ccbf05472d2f01
Martin <martin@sharypic.com>
2021-08-09 15:56:40 +0200

    feat(internship_offers#search): enhance calendar component

--- app/front_assets/controllers/checkbox_svg_icon_controller.js
+++ app/front_assets/controllers/checkbox_svg_icon_controller.js
@@ -5,12 +5,10 @@ export default class extends Controller {
   static targets = [ 'border', 'option' ]
 
   connect(){
-    console.log('hello')
     this.optionTarget.checked = false
   }
 
   onCheck(e){
-    debugger
     e.preventDefault();
     this.borderTarget.classList.toggle('active')
   }
