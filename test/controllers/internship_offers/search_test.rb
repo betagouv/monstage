e62e6c6a4b7a560d9a1e3b76e712dd6881cbf9b7
Martin <martin@sharypic.com>
2021-09-06 07:35:46 +0200

    fix(test.search): update test, using select

--- test/controllers/internship_offers/search_test.rb
+++ test/controllers/internship_offers/search_test.rb
@@ -9,11 +9,10 @@ class SearchTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "label[for=input-search-by-keyword]"
     assert_select "label[for=input-search-by-city]"
-    assert_select ".label", text:  "Filtrer par filière"
-
-    assert_select ".label", text:  "Filtrer par filière"
+    assert_select "label", text:  "Filtrer par filière"
+    assert_select ".label", text:  "Quand se déroule le stage ?"
     Presenters::ClassRoom.school_tracks_list.each do |(track, enum)|
-      assert_select "input#school_track_#{enum}"
+      assert_select "select option[value=\"#{enum}\"]"
     end
   end
 end
