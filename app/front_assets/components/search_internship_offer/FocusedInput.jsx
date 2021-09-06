40ea8497667c573aae09f5a8f9048efb1e7b8727
Martin <martin@sharypic.com>
2021-09-06 08:10:45 +0200

    clean(deadcode): remove another unused component

--- app/front_assets/components/search_internship_offer/FocusedInput.jsx
+++
@@ -1,9 +0,0 @@
-export default function focusedInput({ check, focus }) {
-  if (focus == null) {
-    return 'no-focus';
-  }
-  if (focus == check) {
-    return 'focus-expanded';
-  }
-  return 'focus-collapsed';
-}
