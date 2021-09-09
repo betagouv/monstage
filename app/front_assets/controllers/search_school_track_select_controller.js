2835b844976e1e2bc31ac6fe581123e7c767a4c7
Martin <martin@sharypic.com>
2021-09-09 11:55:50 +0200

    feat(ux): hide select week on mobile

--- app/front_assets/controllers/search_school_track_select_controller.js
+++ app/front_assets/controllers/search_school_track_select_controller.js
@@ -5,8 +5,9 @@ export default class extends Controller {
 
   static targets = [
     'schoolTrackInput', // select
-    'inputPlaceholder'  // used as 'label/placeholder'
-    ]
+    'inputPlaceholder',  // used as 'label/placeholder'
+    'searchByDateContainer'
+  ]
 
   connect(){
     this.updateVisibleForm(this.schoolTrackInputTarget.value);
@@ -18,10 +19,15 @@ export default class extends Controller {
 
   updateVisibleForm(value) {
     const $inputPlaceholder = $(this.inputPlaceholderTarget);
+    const searchByDateContainer = this.searchByDateContainerTarget
     if (value == 'troisieme_generale') {
       enableInput($inputPlaceholder)
+      this.searchByDateContainerTarget.classList.remove('d-none')
+      this.searchByDateContainerTarget.classList.remove('d-sm-block')
     } else {
       disableInput($inputPlaceholder)
+      this.searchByDateContainerTarget.classList.add('d-none')
+      this.searchByDateContainerTarget.classList.add('d-sm-block')
     }
   }
 }
