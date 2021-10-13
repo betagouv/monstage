7556ea4b10902214c508110c5e04317e7533337d
Maxime Pierrot <maxime.pierrot@gmail.com>
2021-10-13 16:36:24 +0200

    Fix test agreements

--- test/models/internship_agreement_test.rb
+++ test/models/internship_agreement_test.rb
@@ -11,31 +11,4 @@ class InternshipAgreementTest < ActiveSupport::TestCase
     internship_agreement = InternshipAgreement.new(school_track: :troisieme_generale)
     internship_agreement.valid?
   end
-
-  test 'ensure presence fields when school_manager and not 3e' do
-    internship_agreement = InternshipAgreement.new(school_track: :troisieme_prepa_metiers,
-                                                   enforce_school_manager_validations: true)
-    internship_agreement.valid?
-
-    assert internship_agreement.errors.include?(:activity_rating_rich_text)
-    assert internship_agreement.errors.include?(:complementary_terms_rich_text)
-  end
-
-  test 'ensure presence of fields when employer and not 3e' do
-    internship_agreement = InternshipAgreement.new(school_track: :troisieme_prepa_metiers,
-                                                   enforce_employer_validations: true)
-    internship_agreement.valid?
-
-    assert internship_agreement.errors.include?(:activity_learnings_rich_text)
-    assert internship_agreement.errors.include?(:activity_scope_rich_text)
-    assert internship_agreement.errors.include?(:complementary_terms_rich_text)
-  end
-
-  test 'ensure presence of fields when main_teacher and not 3e' do
-    internship_agreement = InternshipAgreement.new(school_track: :troisieme_prepa_metiers,
-                                                   enforce_main_teacher_validations: true)
-    internship_agreement.valid?
-
-    assert internship_agreement.errors.include?(:activity_preparation_rich_text)
-  end
 end
