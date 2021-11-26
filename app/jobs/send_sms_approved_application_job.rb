00f2b2a203fe55b1ef9956ed4ef85699f40f5146
Maxime Pierrot <maxime.pierrot@gmail.com>
2021-11-26 10:55:48 +0100

    Approved SMS application

---
+++ app/jobs/send_sms_approved_application_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class SendSmsApprovedApplicationJob < ApplicationJob
+  queue_as :default
+
+  def perform(phone)
+
+    client = OVH::REST.new(
+      ENV['OVH_APPLICATION_KEY'],
+      ENV['OVH_APPLICATION_SECRET'],
+      ENV['OVH_CONSUMMER_KEY']
+    )
+    response = client.post("/sms/#{ENV['OVH_SMS_APPLICATION']}/jobs",
+                {
+                  'sender': ENV['OVH_SENDER'],
+                  'message': "Votre candidature à un stage a été acceptée. Connectez-vous à monstagedetroisieme.fr et contactez l'employeur pour signer votre convention de stage.",
+                  'receivers': [phone]
+                })
+  end
+end
