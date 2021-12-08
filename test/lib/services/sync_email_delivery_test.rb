573ca10fb286cdfba0636238986e67e6318e0440
Etienne Weil <weil.etienne@hotmail.fr>
2021-12-08 13:15:49 +0100

    fix(test): since syncEmailDeliveryTest implementation change

--- test/lib/services/sync_email_delivery_test.rb
+++ test/lib/services/sync_email_delivery_test.rb
@@ -5,18 +5,18 @@ module Services
     test 'destroy_contact should destroy contact when contact exists' do
       sync = Services::SyncEmailDelivery.new
       expected_id = 'user_id'
+      email_parameter = { email: 'test@test.fr' }
 
       fake_user_id = Minitest::Mock.new
-      fake_user_id.expect :call,
-                          { Data: [ID: expected_id], Count: 1 }.with_indifferent_access,
-                          [ { email: 'test@test.fr' } ]
+      fake_user_response = OpenStruct.new(body: JSON.generate({ Data: [ID: expected_id], Count: 1 }.with_indifferent_access ))
+      fake_user_id.expect :call, fake_user_response, [ email_parameter ]
       fake_response = Minitest::Mock.new
       fake_response.expect :call,
                            OpenStruct.new(code: 200),
                            [ { mailjet_user_id: expected_id } ]
       sync.stub :send_read_contact, fake_user_id do
         sync.stub :send_destroy_contact, fake_response do
-          assert_equal true, sync.destroy_contact(email: 'test@test.fr')
+          assert_equal true, sync.destroy_contact(email_parameter)
         end
       end
     end
@@ -25,11 +25,12 @@ module Services
       sync = Services::SyncEmailDelivery.new
       wrong_return_code = 404
       expected_id       = 'user_id'
+      email_parameter = { email: 'test@test.fr' }
 
       fake_user_id = Minitest::Mock.new
       fake_user_id.expect :call,
                           { "ErrorInfo": "", "ErrorMessage": "Object not found", "StatusCode": 404 }.with_indifferent_access,
-                          [ { email: 'test@test.fr' } ]
+                          [ email_parameter ]
 
       fake_response = Minitest::Mock.new
       fake_response.expect :call,
@@ -38,7 +39,7 @@ module Services
 
       sync.stub :send_read_contact, fake_user_id do
         assert_raises(StandardError, "unknown email : test@test.fr - contact destroy impossible") do
-          sync.destroy_contact(email: 'test@test.fr')
+          sync.destroy_contact(email_parameter)
         end
       end
     end
