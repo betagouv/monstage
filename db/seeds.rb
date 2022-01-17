ede0ccb439e628e64ba07bdc2feec3313f6eb9bd
Etienne Weil <weil.etienne@hotmail.fr>
2022-01-17 19:06:47 +0100

    feat(seeds) : some application with :draft state

--- db/seeds.rb
+++ db/seeds.rb
@@ -488,14 +488,25 @@ def populate_applications
   troisieme_generale_offers = InternshipOffers::WeeklyFramed.where(school_track: :troisieme_generale)
   puts "every 3e generale offers receives an application first 3e generale stud"
   troisieme_generale_offers.each do |io_trois_gene|
-    InternshipApplications::WeeklyFramed.create!(
-      aasm_state: :submitted,
-      submitted_at: 10.days.ago,
-      student: trois_gene_studs.first,
-      motivation: 'Au taquet',
-      internship_offer: io_trois_gene,
-      week: io_trois_gene.internship_offer_weeks.sample.week
-    )
+    if io_trois_gene.id.to_i.even?
+      InternshipApplications::WeeklyFramed.create!(
+        aasm_state: :submitted,
+        submitted_at: 10.days.ago,
+        student: trois_gene_studs.first,
+        motivation: 'Au taquet',
+        internship_offer: io_trois_gene,
+        week: io_trois_gene.internship_offer_weeks.sample.week
+      )
+    else
+      InternshipApplications::WeeklyFramed.create!(
+        aasm_state: :drafted,
+        submitted_at: 10.days.ago,
+        student: trois_gene_studs.first,
+        motivation: 'Au taquet',
+        internship_offer: io_trois_gene,
+        week: io_trois_gene.internship_offer_weeks.sample.week
+      )
+    end
   end
 
   puts "second 3e generale offer receive an approval --> second 3e generale stud"
