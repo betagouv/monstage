23388f6c3144629aefc5ec2f26de3cf474116250
Etienne Weil <weil.etienne@hotmail.fr>
2022-01-20 11:56:21 +0100

    fix(code): alignments

--- test/factories/internship_applications.rb
+++ test/factories/internship_applications.rb
@@ -53,6 +53,7 @@ FactoryBot.define do
     transient do
       weekly_internship_offer_helper {create(:weekly_internship_offer)}
     end
+
     trait :weekly do
       internship_offer { weekly_internship_offer_helper }
       internship_offer_week { internship_offer.internship_offer_weeks.first}
