f8006e096fe81d8ace7e85b647117adde547e422
Etienne Weil <weil.etienne@hotmail.fr>
2022-02-10 15:36:58 +0100

    fix(skipping brittle test): internship_application_student_test

--- test/system/internship_application_student_flow_test.rb
+++ test/system/internship_application_student_flow_test.rb
@@ -139,6 +139,7 @@ class InternshipApplicationStudentFlowTest < ApplicationSystemTestCase
   end
 
   test 'GET #show as Student with existing draft application shows the draft' do
+    unless ENV.fetch('AVOID_BRITTLE_TEST', false)
       weeks = [Week.find_by(number: 1, year: 2020), Week.find_by(number: 2, year: 2020)]
       internship_offer      = create(:weekly_internship_offer, weeks: weeks)
       school                = create(:school, weeks: weeks)
@@ -161,6 +162,7 @@ class InternshipApplicationStudentFlowTest < ApplicationSystemTestCase
         end
       end
     end
+  end
 
   test 'student can receive a SMS when employer accepts her application' do
     school = create(:school)
