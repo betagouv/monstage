e7b91ec2174be8b037c68161ec555ac326cc8f21
Etienne Weil <weil.etienne@hotmail.fr>
2022-02-18 17:49:07 +0100

    fix(test): non system test - search label is now Filieres

--- test/controllers/internship_offers/search_test.rb
+++ test/controllers/internship_offers/search_test.rb
@@ -7,7 +7,7 @@ class SearchTest < ActionDispatch::IntegrationTest
     get search_internship_offers_path
 
     assert_response :success
-    assert_select 'label', text:  'Filtrer par filière'
+    assert_select 'label', text: 'Filières'
     ClassRoom.school_tracks.map do |(enum)|
       assert_select "select option[value=\"#{enum}\"]"
     end
