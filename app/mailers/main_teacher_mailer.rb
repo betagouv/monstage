ea79233e7d54246bace831c28440af436fb2d8c7
Etienne Weil <weil.etienne@hotmail.fr>
2022-03-04 11:16:54 +0100

    fix(main_teacher's mail): possibility not to have any school_manager nor main teacher

--- app/mailers/main_teacher_mailer.rb
+++ app/mailers/main_teacher_mailer.rb
@@ -6,19 +6,28 @@ class MainTeacherMailer < ApplicationMailer
     @student = @internship_application.student
     @student_presenter = Presenters::User.new(@student)
 
+    school_manager_email = @student.school_manager&.email
+    main_teacher_email = @student.main_teacher&.email
+
     if @student.troisieme_generale?
-      to = @student.school_manager.email
+      to = school_manager_email
+      return if to.nil?
+
       subject = "Convention de stage à renseigner: #{@student_presenter.civil_name}"
-      cc = @student.main_teacher.email
+      cc = main_teacher_email
       @url = edit_dashboard_internship_agreement_url(@internship_application.internship_agreement)
       @message = "La convention dématérialisée peut être renseignée dès maintenant par le chef d'établissement ou le professeur principal"
     else
-      to = @student.main_teacher.email
+      to = main_teacher_email
+      return if to.nil?
+
       subject = "Stage accepté pour #{@student_presenter.civil_name}"
-      cc = @student.school_manager.email
+      cc = school_manager_email
       @message = "Aucune convention n'est prévue sur ce site, bon stage à #{@student_presenter.civil_name} !"
     end
 
-    mail(to: to, subject: subject, cc: cc)
+    params = { to: to, subject: subject }
+    params.merge!(cc: cc) unless cc.nil?
+    mail(params)
   end
 end
