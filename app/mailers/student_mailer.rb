b335c4b69f806aa7097e7649a5738d4cc22e108f
Etienne Weil <weil.etienne@hotmail.fr>
2022-03-07 17:40:54 +0100

    feat(mail update): rejected message email shows reasons why internship was rejected

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -12,7 +12,7 @@ class StudentMailer < ApplicationMailer
     @internship_application = internship_application
 
     mail(to: @internship_application.student.email,
-         subject: "Une de vos candidatures a été rejetée")
+         subject: "Une de vos candidatures a été refusée")
   end
 
   def internship_application_canceled_by_employer_email(internship_application:)
