dcf7524c854ce4b47fe89629094d5c01e69f3a32
Etienne Weil <weil.etienne@hotmail.fr>
2022-03-25 19:09:29 +0100

    feat(config): showing smtp errors when existing

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.show_previews = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: HOST }
 
   config.action_mailer.smtp_settings = {
