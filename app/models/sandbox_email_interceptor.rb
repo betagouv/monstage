8d085a6cdcff25be44595de5133658441ec7ad85
Etienne Weil <weil.etienne@hotmail.fr>
2022-03-28 09:18:38 +0200

    feat(review intercepted emails): message subject now contains original mail's recipient

--- app/models/sandbox_email_interceptor.rb
+++ app/models/sandbox_email_interceptor.rb
@@ -1,6 +1,9 @@
 class SandboxEmailInterceptor
   def self.delivering_email(message)
     message.to = [ENV['RECETTE_EMAIL']] if Rails.env.staging?
+
+    original_to = message.to
+    message.subject = "[Adresssé initialement à #{original_to}] | #{message.subject}"
     message.to = [ENV['REVIEW_EMAIL']] if Rails.env.review?
   end
 end
