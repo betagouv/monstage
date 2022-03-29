42a3c488224ae7e2a3285720d80897f83f659ff7
Etienne Weil <weil.etienne@hotmail.fr>
2022-03-29 16:33:58 +0200

    fix(typo and wording) in review's subjects

--- app/models/sandbox_email_interceptor.rb
+++ app/models/sandbox_email_interceptor.rb
@@ -1,6 +1,6 @@
 class SandboxEmailInterceptor
   def self.delivering_email(message)
-    message.subject = "[Adresssé initialement à #{message.to}] | #{message.subject}"
+    message.subject = "Adressé initialement à : #{message.to.join(',')} | #{message.subject}"
     message.to = [ENV['RECETTE_EMAIL']] if Rails.env.staging?
     message.to = [ENV['REVIEW_EMAIL']] if Rails.env.review?
   end
