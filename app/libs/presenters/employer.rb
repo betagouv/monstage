734638443ab184a0b71a10d3a54014cb47da9097
Etienne Weil <weil.etienne@hotmail.fr>
2022-04-08 14:25:05 +0200

    fix(presenters): refacto

--- app/libs/presenters/employer.rb
+++ app/libs/presenters/employer.rb
@@ -1,12 +1,4 @@
 module Presenters
-  class Employer
-    include Humanable
-
-    private
-    attr_reader :user
-
-    def initialize(employer)
-      @user = employer
-    end
+  class Employer < User
   end
 end
