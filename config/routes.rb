109b649568f964a3e806ceca5bcdc839c44ef436
Etienne Weil <weil.etienne@hotmail.fr>
2022-04-16 11:13:14 +0200

    feat(newsletter): backend with test

--- config/routes.rb
+++ config/routes.rb
@@ -118,6 +118,7 @@ Rails.application.routes.draw do
   get '/partenaires', to: 'pages#partenaires'
   get '/politique-de-confidentialite', to: 'pages#politique_de_confidentialite'
   get '/statistiques', to: 'pages#statistiques'
+  post '/newsletter', to: 'newsletter#subscribe'
 
   # Redirects
   get '/dashboard/internship_offers/:id', to: redirect('/internship_offers/%{id}', status: 302)
---
+++ test/controllers/newsletter_controller_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class NewsletterControllerTest < ActionDispatch::IntegrationTest
+  test "post should not subscribe when email is faulty" do
+    test_email = 'test@free@.fr'
+
+    post newsletter_path, params: {newsletter_email: test_email}
+    assert_redirected_to root_path
+    assert_equal "Votre email a l'air erroné", flash[:warning]
+  end
+
+  test "post should subscribe" do
+    test_email = 'test@free.fr'
+    expected_result = ["S9Dm", "2022-04-17T14:57:56Z", "2022-04-17T14:57:56Z", test_email, ""]
+
+    Services::SyncEmailCampaigns.stub_any_instance(:add_contact, expected_result) do
+      post newsletter_path, params: {newsletter_email: test_email}
+      assert_redirected_to root_path
+      assert_equal 'Votre email a bien été enregistrée', flash[:notice]
+    end
+  end
+  
+  test "post should not subscribe when already subcribed" do
+    test_email = 'test@free.fr'
+    expected_result = :previously_existing_email
+
+    Services::SyncEmailCampaigns.stub_any_instance(:add_contact, expected_result) do
+      post newsletter_path, params: {newsletter_email: test_email}
+      assert_redirected_to root_path
+      assert_equal 'Votre email était déjà enregistrée. :-) .', flash[:warning]
+    end
+  end
+
+  test "post should not subscribe when api is faulty" do
+    test_email = 'test@free.fr'
+    expected_result = "not expected"
+
+    Services::SyncEmailCampaigns.stub_any_instance(:add_contact, expected_result) do
+      post newsletter_path, params: {newsletter_email: test_email}
+      assert_redirected_to root_path
+      err_message = "Une erreur s'est produite et nous n'avons pas " \
+                  "pu enregistrer votre email"
+      assert_equal err_message, flash[:warning]
+    end
+  end
+end
