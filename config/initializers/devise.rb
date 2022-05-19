a2be95170c5f3bbb8909bd4e5fba6df2e48b4610
Etienne Weil <weil.etienne@hotmail.fr>
2022-05-19 08:45:15 +0200

    feat(devise with turbo): as per gorails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
+# as per : https://gorails.com/episodes/devise-hotwire-turbo?autoplay=1
+class TurboController < ApplicationController
+  class Responder < ActionController::Responder
+    def to_turbo_stream
+      controller.render(options.merge(formats: :html))
+    rescue ActionView::MissingTemplate => e
+      if get?
+        raise e
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      else
+        redirect_to navigation_location
+      end
+    end
+  end
+  self.responder = Responder
+  respond_to :html, :turbo_stream
+end
+
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
@@ -12,7 +31,7 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  # config.parent_controller = 'DeviseController'
+  config.parent_controller = 'TurboController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -250,7 +269,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -289,6 +308,7 @@ Devise.setup do |config|
   ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   end
+  
 
   # ==> Configuration for :registerable
 
