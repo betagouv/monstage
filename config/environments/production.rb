be225d61717820def5b9e00ed30201c2be1ce298
Etienne WEIL <fitchMitch@users.noreply.github.com>
2022-05-20 16:06:10 +0200

    Hotfix/rails 7 iteration 3 (#685)
    * fix(migration): configuring active_storage to comply to rails 7 migration given through rails update command

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :local
+  config.active_storage.service = :production
+
   # Mount Action Cable outside main process or domain
   host_or_default = ENV.fetch('HOST')
   host_uri = URI(host_or_default)
