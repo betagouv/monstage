bf7c8ec6a5d15eb59fbe376143b77ab9e0f58188
Etienne Weil <weil.etienne@hotmail.fr>
2022-05-30 12:21:03 +0200

    Merge avec un commit de résolution de tests

--- test/system/dashboard/missing_weeks_notification_test.rb
+++ test/system/dashboard/missing_weeks_notification_test.rb
@@ -14,46 +14,47 @@ class MissingWeeksNotificationTest < ApplicationSystemTestCase
   end
 
   test 'student troisieme try to apply to an offer while school manager has not open any internship week' do
-    if ENV['RUN_BRITTLE_TEST']
-      internship_offer = create(:weekly_internship_offer)
-      school = create(:school, :with_school_manager, weeks: [])
-      school_manager = school.school_manager
-      class_room = create(:class_room, school: school)
-      student = create(:student, school: school, class_room: class_room)
-      message_no_week = "Attention, votre établissement n'a pas encore renseigné ses dates de stage."
-      student_message = "Nous allons prévenir votre chef d'établissement pour que vous puissiez postuler"
-      explanation = "Attention, vérifiez bien que les dates de stage proposées dans l'annonce correspondent " \
-                    "à vos dates de stage. Votre chef d'établissement n'a en effet pas renseigné " \
-                    "les semaines de stage de votre établissement." \
-                    
-      school_message = "Etablissement mis à jour avec succès"
+    travel_to(Date.new(2021,9,1)) do
+      if ENV['RUN_BRITTLE_TEST']
+        internship_offer = create(:weekly_internship_offer)
+        school = create(:school, :with_school_manager, weeks: [])
+        school_manager = school.school_manager
+        class_room = create(:class_room, school: school)
+        student = create(:student, school: school, class_room: class_room)
+        message_no_week = "Attention, votre établissement n'a pas encore renseigné ses dates de stage."
+        # student_message = "Nous allons prévenir votre chef d'établissement pour que vous puissiez postuler"
+        explanation = "Attention, vérifiez bien que les dates de stage proposées dans l'annonce correspondent " \
+                      "à vos dates de stage. Votre chef d'établissement n'a en effet pas renseigné " \
+                      "les semaines de stage de votre établissement." \
 
-      InternshipOffer.stub :nearby, InternshipOffer.all do
-        sign_in(student)
+        school_message = "Etablissement mis à jour avec succès"
 
-        visit internship_offers_path
-        click_link("Voir l'annonce")
-        find("a[href='#internship-application-form']").click
-        find "label", text: "Quelle semaine ?"
-        find "p.test-missing-school-weeks", text: explanation
-        page.find "input[name='commit']", visible: true
-        sign_out(student)
+        InternshipOffer.stub :nearby, InternshipOffer.all do
+          sign_in(student)
+          visit internship_offers_path
+          click_link("Voir l'annonce")
+          click_on 'Postuler'
+          find ".fr-label", text: "Quelle semaine ?"
+          find "p.test-missing-school-weeks", text: explanation
+          page.find "input[name='commit']", visible: true
+          sign_out(student)
 
-        # Back to interfaces
-        sign_in(school_manager)
-        visit edit_dashboard_school_path(school)
-        all(".custom-control.custom-checkbox label").first.click
-        find('input[type="submit"]').click
-        find "#alert-text", text: school_message
-        sign_out(school_manager)
+          # Back to interfaces /!\ works alone
+          sign_in(school_manager)
+          visit edit_dashboard_school_path(school)
+          all(".fr-checkbox-group.fr-checkbox-group--sm label").first.click
+          find('input[type="submit"]').click
+          find "#alert-text", text: school_message
+          sign_out(school_manager)
 
-        sign_in(student)
-        visit internship_offers_path
-        assert_presence_of(internship_offer: internship_offer)
-        page.has_no_content?(message_no_week)
-        click_link("Voir l'annonce")
-        find("a[href='#internship-application-form']").click
-        page.has_no_content?(explanation)
+          sign_in(student)
+          visit internship_offers_path
+          assert_presence_of(internship_offer: internship_offer)
+          page.has_no_content?(message_no_week)
+          click_link("Voir l'annonce")
+          click_on 'Postuler'
+          page.has_no_content?(explanation)
+        end
       end
     end
   end
