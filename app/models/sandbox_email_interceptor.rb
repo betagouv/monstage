9470eb89c44b967f274c6f80560b6a210573df6c
Etienne Weil <weil.etienne@hotmail.fr>
2022-06-16 09:52:57 +0200

    fix(code cleaning): useless class withdrawn

--- app/models/sandbox_email_interceptor.rb
+++
@@ -1,7 +0,0 @@
-class SandboxEmailInterceptor
-  def self.delivering_email(message)
-    message.subject = "Adressé initialement à : #{message.to.join(',')} | #{message.subject}"
-    message.to = [ENV['RECETTE_EMAIL']] if Rails.env.staging?
-    message.to = [ENV['REVIEW_EMAIL']] if Rails.env.review?
-  end
-end
