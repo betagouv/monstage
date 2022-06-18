0a4fa6d1d9c7e7dcab952560864f9df4052da059
Etienne Weil <weil.etienne@hotmail.fr>
2022-06-18 10:46:32 +0200

    feat(js refacto): using functions as args

--- app/front_assets/controllers/digit_code_controller.js
+++ app/front_assets/controllers/digit_code_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from 'stimulus';
+
 export default class extends Controller {
   static targets = ['code'];
   static values = { position: Number }
@@ -12,17 +13,19 @@ export default class extends Controller {
 
   eraseBack() {
     this.eraseCurrentKey()
-    if (!this.isFirst()) { this.positionMove(-1); }
+    if (!this.firstPosition()) { this.positionMove(-1); }
     this.enableCurrent();
-    this.eraseCurrentKey()
+    this.eraseCurrentKey();
     this.setFocus();
   }
+
   enterKey(event) {
     this.validKey(event.key) ? this.withGoodKey(event.key) : this.eraseCurrentKey();
   }
+
   withGoodKey(key) {
     this.validateEnteredValue(key);
-    if (this.isLast()) {
+    if (this.lastPosition()) {
       this.enableAll();
       this.validateForm();
     } else {
@@ -32,39 +35,38 @@ export default class extends Controller {
     this.setFocus();
   }
 
-  validateEnteredValue(key) {
+  parseCodes(fn, key = '') {
     this.codeTargets.forEach((element, index) => {
-      if (index === this.positionValue) { element.value = key; }
-    })
+      if (index === this.positionValue) { fn(element, key); }
+    });
     return;
   }
 
-  eraseCurrentKey() {
-    this.codeTargets.forEach((element, index) => {
-      if (index === this.positionValue) { element.value = ''; }
-    })
+  assignKey(element, key) { element.value = key; }
+  validateEnteredValue(key) { this.parseCodes(this.assignKey, key); }
+
+  clearKey(element) { element.value = ''; }
+  eraseCurrentKey() { this.parseCodes(this.clearKey); }
+
+  enableField(element) { element.removeAttribute('disabled'); }
+  enableCurrent() { this.disableAll(); this.parseCodes(this.enableField); }
+
+  fieldFocus(element) { element.focus(); }
+  setFocus() { this.parseCodes(this.fieldFocus); }
+
+  disableAll() {
+    this.codeTargets.forEach(element => { element.setAttribute('disabled', true); });
   }
 
-  disableAll() { this.codeTargets.forEach(element => { element.setAttribute('disabled', true); }); }
-  enableAll() { this.codeTargets.forEach(element => { element.removeAttribute('disabled') }); }
+  enableAll() {
+    this.codeTargets.forEach(element => { element.removeAttribute('disabled') });
+  }
   positionMove(i) { this.positionValue += i }
-  isFirst() { return this.positionValue == 0; }
-  isLast() { return this.positionValue == this.codeTargets.length - 1; }
+  firstPosition() { return this.positionValue == 0; }
+  lastPosition() { return this.positionValue == this.codeTargets.length - 1; }
   validKey(val) { return (parseInt(val, 10) >= 0 && parseInt(val, 10) <= 9) }
-  setFocus() {
-    this.codeTargets.forEach((element, index) => {
-      if (index === this.positionValue) { element.focus(); }
-    });
-  }
 
-  enableCurrent() {
-    this.disableAll();
-    this.codeTargets.forEach((element, index) => {
-      if (index === this.positionValue) { element.removeAttribute('disabled') }
-    });
-  }
-
-  validateForm() { }
+  validateForm() { this.codeTargets[0].closest('form').submit(); }
 
   connect() {
     this.codeTargets.forEach((element, index) => {
