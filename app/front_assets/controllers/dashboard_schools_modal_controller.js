776622c45a03e73ff058fe3335a6fc18a427dcd4
Brice <brice.durand2@gmail.com>
2022-06-27 09:53:41 +0200

    Retrait du backdrop qui bloque une modal et réhausse de celle-ci pour pouvoir la fermer sur Firefox

--- app/front_assets/controllers/dashboard_schools_modal_controller.js
+++ app/front_assets/controllers/dashboard_schools_modal_controller.js
@@ -3,7 +3,7 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   openModal() {
-    $('#notice-school-manager-empty-weeks').modal('show');
+    $('#notice-school-manager-empty-weeks').modal({ show: true, backdrop: false });
   }
 
   closeNotice() {
