656a1d140bb9da62d70bc6281f674e7e49eeb9c6
Etienne Weil <weil.etienne@hotmail.fr>
2022-08-22 09:08:10 +0200

    fix(review apps on heroku): adding a patch to enable extensions

---
+++ config/initializers/patch_enable_extension.rb
@@ -0,0 +1,21 @@
+# config/initializers/patch_enable_extension.rb
+# see https://stackoverflow.com/questions/73214844/error-extension-btree-gist-must-be-installed-in-schema-heroku-ext
+
+require 'active_record/connection_adapters/postgresql_adapter'
+
+# NOTE: patch for https://devcenter.heroku.com/changelog-items/2446
+module EnableExtensionHerokuPatch
+  def enable_extension(name, **)
+    return super unless schema_exists?("heroku_ext")
+
+    exec_query("CREATE EXTENSION IF NOT EXISTS \"#{name}\" SCHEMA heroku_ext").tap { reload_type_map }
+  end
+end
+
+module ActiveRecord
+  module ConnectionAdapters
+    class PostgreSQLAdapter
+      prepend EnableExtensionHerokuPatch
+    end
+  end
+end
