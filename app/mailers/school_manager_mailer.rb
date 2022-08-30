d969e345036d72b32a9f57d4bfd4d9c299ee6372
Etienne Weil <weil.etienne@hotmail.fr>
2022-08-30 10:38:55 +0200

    fix(cleaning dead code): unused message deleted

--- app/mailers/school_manager_mailer.rb
+++ app/mailers/school_manager_mailer.rb
@@ -41,7 +41,6 @@ class SchoolManagerMailer < ApplicationMailer
     ).html_safe
 
     subject = "Un de vos élèves a été accepté à un stage"
-    @message = "La convention dématérialisée peut être renseignée dès maintenant par le chef d'établissement ou le professeur principal"
 
     send_email(to: to, subject: subject)
   end
