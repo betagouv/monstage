0e5b2c3fea3de910c8b48a9466c389be3269b5da
Etienne Weil <weil.etienne@hotmail.fr>
2022-09-14 09:25:50 +0200

    feat(school_manager tabs): now with tab pills counting pending actions

--- app/libs/finders/tab_school_manager.rb
+++ app/libs/finders/tab_school_manager.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 module Finders
   class TabSchoolManager
+    def pending_class_rooms_actions_count
+      school.students.without_class_room.count
+    end
 
     def pending_agreements_count
-      # internship_agreement approved with internship_agreement without terms_accepted
-      @pending_internship_agreement_count ||= school.internship_applications
-                                                    .approved
-                                                    .troisieme_generale
-                                                    .joins(:internship_agreement)
-                                                    .where(internship_agreement: {aasm_state: :completed_by_employer})
-                                                    .count
+      internship_agreements = InternshipAgreement.joins(internship_application: {student: :school})
+                                                 .where(school: {id: school.id})
+      states_with_actions= %i[completed_by_employer started_by_school_manager validated]
+      count = internship_agreements.where(aasm_state: states_with_actions).count
+      count += internship_agreements.signatures_started
+                                    .joins(:signatures)
+                                    .where.not(signatures: {signatory_role: :school_manager} )
+                                    .count
     end
 
     def student_without_class_room_count
-      @students_without_classs_room_count ||= school.students.without_class_room.count
+      school.students.without_class_room.count
     end
 
     private
-    attr_reader :school
 
+    attr_reader :school
 
     def initialize(school:)
       @school = school
