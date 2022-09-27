2821087496e3a539af789aacf00a1357eb03d19b
Etienne Weil <weil.etienne@hotmail.fr>
2022-09-27 15:23:31 +0200

    fix(invalid school members filtered): update now is possible, when every student has a birth_date

--- app/controllers/dashboard/schools/students_controller.rb
+++ app/controllers/dashboard/schools/students_controller.rb
@@ -10,7 +10,9 @@ module Dashboard
 
         students = params.select { |k, v| k.to_s.match(/\Astudent_\d+/) }
         students.each do |k,v|
-          student = @school.students.find(k.split('_').last.to_i)
+          student = @school.students
+                           .kept
+                           .find(k.split('_').last.to_i)
           if v.blank?
             student.update!(class_room_id: nil)
           elsif student.class_room_id.to_i != v.to_i
