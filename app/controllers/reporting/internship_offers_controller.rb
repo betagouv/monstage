347d52379d68a796224e1ecd5e49425865f1cff0
Etienne Weil <weil.etienne@hotmail.fr>
2022-10-14 14:05:28 +0200

    fix(tests): altered format now retrieved

--- app/controllers/reporting/internship_offers_controller.rb
+++ app/controllers/reporting/internship_offers_controller.rb
@@ -15,12 +15,13 @@ module Reporting
 
       respond_to do |format|
         format.xlsx do
-          response.headers['Content-Disposition'] = %(attachment; filename="#{export_filename('offres')}.xlsx")
           if dimension_is?('offers', params[:dimension])
             SendExportOffersJob.perform_later(current_user, offers_hash)
             redirect_back fallback_location: reporting_dashboards_path(offers_hash),
-                          flash: { success: "Votre fichier va vous être envoyé à l'adresse email : #{current_user.email}"}
+                          flash: { success: "Votre fichier va vous être envoyé " \
+                                            "à l'adresse email : #{current_user.email}" }
           else
+            response.headers['Content-Disposition'] = %(attachment; filename="#{export_filename('offres')}.xlsx")
             render :index_stats
           end
         end
@@ -77,10 +78,9 @@ module Reporting
       @finder ||= Finders::ReportingInternshipOffer.new(params: reporting_cross_view_params)
     end
 
-    def offers_hash(format: :html)
+    def offers_hash
       { department: params[:department],
-        school_year: params[:school_year] ,
-        format: format}
+        school_year: params[:school_year]}
     end
   end
 end
