bbe44b144a055e5d7272da9c505057dead053f57
Brice <brice.durand2@gmail.com>
2022-11-03 09:42:22 +0100

    résolutions de conflits sur un fichier de tests

--- test/controllers/internship_offers/index_test.rb
+++ test/controllers/internship_offers/index_test.rb
@@ -263,19 +263,11 @@ class IndexTest < ActionDispatch::IntegrationTest
     internship_offer_weekly = create(:weekly_internship_offer)
     # Visitor
     get internship_offers_path
-<<<<<<< HEAD
-=======
-    assert_presence_of(internship_offer: internship_offer_weekly)
->>>>>>> 150bbcb3c... fix(code and tests): no school_track anymore (#711)
     # Student
     school = create(:school, weeks: [])
     student = create(:student, school: school)
     sign_in(student)
     get internship_offers_path
-<<<<<<< HEAD
-=======
-    assert_presence_of(internship_offer: internship_offer_weekly)
->>>>>>> 150bbcb3c... fix(code and tests): no school_track anymore (#711)
   end
 
 
@@ -442,74 +434,6 @@ class IndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-<<<<<<< HEAD
-=======
-  test 'GET #index as student ' \
-       'includes forwardable_params for listable behaviour on show page' do
-    sign_in(create(:student))
-    internship_1 = create(:weekly_internship_offer)
-
-    InternshipOffer.stub :nearby, InternshipOffer.all do
-      InternshipOffer.stub :by_weeks, InternshipOffer.all do
-        forwarded_params = {
-          city: 'Paris',
-          latitude: 1,
-          longitude: 1,
-          page: 1,
-          radius: 1_000,
-          week_ids: [1, 2, 3]
-        }
-        get internship_offers_path, params: forwarded_params
-        assert_select(
-          'a[href=?]',
-          internship_offer_path(
-            forwarded_params.merge(id: internship_1, origin: 'search')
-          )
-        )
-      end
-    end
-  end
-
-  test 'GET #index as student includes forwardable_params ' do
-    school     = create(:school)
-    class_room = create(:class_room, school: school)
-    sign_in(create(:student, school: school, class_room: class_room))
-    # default school_track is troisieme_generale
-    # and it's an implicit filter for student's search
-    internship_1 = create(:weekly_internship_offer)
-    internship_2 = create(:weekly_internship_offer)
-
-    InternshipOffer.stub :nearby, InternshipOffer.all do
-      InternshipOffer.stub :by_weeks, InternshipOffer.all do
-        forwarded_params = {
-          city: 'Paris',
-          latitude: 1,
-          longitude: 1,
-          page: 1,
-          radius: 1_000,
-          week_ids: [1, 2, 3]
-        }
-        get internship_offers_path, params: forwarded_params
-        assert_select(
-          'a[href=?]',
-          internship_offer_path(
-            forwarded_params.merge( id: internship_1, origin: 'search' )
-          ),
-          count: 2 #Voir l'annonce and availibility link
-        )
-        assert_select(
-          'a[href=?]',
-          internship_offer_path(
-            forwarded_params.merge( id: internship_2, origin: 'search' )
-          ),
-          count: 2
-        )
-      end
-    end
-  end
-
-
->>>>>>> 150bbcb3c... fix(code and tests): no school_track anymore (#711)
   test 'search by location (radius) works' do
     internship_offer_at_paris = create(:weekly_internship_offer,
                                        coordinates: Coordinates.paris)
