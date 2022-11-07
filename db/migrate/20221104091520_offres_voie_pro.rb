311324abaae2a0039aadeb00ffdf3154c6e3f241
Brice Durand <brice.durand2@gmail.com>
2022-11-07 09:48:16 +0100

    Erreur de type dans une migration

--- db/migrate/20221104091520_offres_voie_pro.rb
+++ db/migrate/20221104091520_offres_voie_pro.rb
@@ -1,6 +1,6 @@
 class OffresVoiePro < ActiveRecord::Migration[7.0]
   def up
     InternshipOffer.where(type: "InternshipOffers::FreeDate").update_all(type: "InternshipOffers::WeeklyFramed")
-    InternshipOfferInfo.where(type: "InternshipOfferInfos::FreeDate").update_all(type: "InternshipOffers::WeeklyFramed")
+    InternshipOfferInfo.where(type: "InternshipOfferInfos::FreeDate").update_all(type: "InternshipOfferInfos::WeeklyFramed")
   end
 end
