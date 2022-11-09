be67511a6ad992f85406d718eeb53a27cd615f78
Etienne Weil <weil.etienne@hotmail.fr>
2022-11-09 12:26:58 +0100

    fix(migrations): data migration in tasks and no longer in migrations files

--- db/migrate/20221031083556_create_ministry_group_table.rb
+++ db/migrate/20221031083556_create_ministry_group_table.rb
@@ -7,21 +7,9 @@ class CreateMinistryGroupTable < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    EmailWhitelists::Ministry.all.each do |email_whitelist|
-      ministry_group = MinistryGroup.create(
-        group_id: email_whitelist.group_id,
-        email_whitelist_id: email_whitelist.id
-      )
-    end
   end
 
   def down
-    MinistryGroup.all.each do |ministry_group|
-      # with following code, group_id is set to the last occurence of a group with the same email_whitelist_id
-      email_whitelist = EmailWhitelists::Ministry.find_by(id: ministry_group.email_whitelist_id)
-      email_whitelist.update(group_id: ministry_group.group_id)
-    end
-
     remove_column :groups, :ministry_group_id if column_exists?(:groups, :ministry_group_id)
 
     drop_table :ministry_groups
