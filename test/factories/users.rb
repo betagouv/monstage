d3f5cdbb304eea323b88e3fd9bf3941f21804c22
Maxime Pierrot <maxime.pierrot@gmail.com>
2022-11-15 15:26:27 +0100

    Add Operators tests api access

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -120,6 +120,12 @@ FactoryBot.define do
       type { 'Users::Operator' }
       operator
       api_token { SecureRandom.uuid }
+
+      trait :fully_authorized do
+        after(:create) do |user|
+          user.operator.update(api_full_access: true)
+        end
+      end
     end
 
 
