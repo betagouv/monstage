c25f872bb7a38a1fde74e207b18398925cf93781
Etienne Weil <weil.etienne@hotmail.fr>
2022-11-22 17:05:21 +0100

    fix(after review): removing lots of code to be able to use the webinar platform several times

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,31 +11,33 @@ class PagesController < ApplicationController
 
   def register_to_webinar
     authorize! :subscribe_to_webinar, current_user
-    reset_old_participation
-    if current_user.subscribed_to_webinar_at.nil?
-      current_user.update(subscribed_to_webinar_at: Time.zone.now)
-      redirect_to WEBINAR_URL,
-                  allow_other_host: true,
-                  notice: 'Vous voilà inscrit au webinar !'
-    else
-      alert_text = "Vous êtes déjà inscrit au prochain webinar Monstage"
-      redirect_back fallback_location: root_path,
-                    flash: { alert: alert_text }
-    end
+    # Remove comments after #31/12/2022
+
+    # reset_old_participation
+    # if current_user.subscribed_to_webinar_at.nil?
+    current_user.update(subscribed_to_webinar_at: Time.zone.now)
+    redirect_to WEBINAR_URL,
+                allow_other_host: true
+    # else
+    #   alert_text = "Vous êtes déjà inscrit au prochain webinar Monstage"
+    #   redirect_back fallback_location: root_path,
+    #                 flash: { alert: alert_text }
+    # end
   end
 
-  def reset_old_participation
-    return if current_user.subscribed_to_webinar_at.nil?
+  # Remove comments after #31/12/2022
+  # def reset_old_participation
+  #   return if current_user.subscribed_to_webinar_at.nil?
 
-    if current_user.subscribed_to_webinar_at <= previous_friday
-      current_user.update(subscribed_to_webinar_at: nil)
-    end
-  end
+  #   if current_user.subscribed_to_webinar_at <= previous_friday
+  #     current_user.update(subscribed_to_webinar_at: nil)
+  #   end
+  # end
 
-  def previous_friday
-    today = Time.zone.today
-    today - today.wday - 2
-  end
+  # def previous_friday
+  #   today = Time.zone.today
+  #   today - today.wday - 2
+  # end
 
   def flyer
     respond_to do |format|
