79235a16434cec3cba0bedf593cefb9126e8e3c3
Etienne Weil <weil.etienne@hotmail.fr>
2022-11-24 17:49:03 +0100

    fix(after review): light updates

--- db/migrate/20221119132335_test_task_register.rb
+++ db/migrate/20221119132335_test_task_register.rb
@@ -2,18 +2,18 @@ class TestTaskRegister < ActiveRecord::Migration[7.0]
   def change
     TaskManager.new(
       allowed_environments: %w[development test],
-      task_name: 'say_something',
+      task_name: 'migrations:say_something',
       arguments: ['once_str, only_str']
     ).play_task_once(run_with_a_job: true)
 
     # TaskManager.new(
     #   allowed_environments: %w[staging test],
-    #   task_name: 'say_something'
+    #   task_name: 'migrations:say_something'
     # ).play_task_each_time
 
     # TaskManager.new(
     #   allowed_environments: %w[staging],
-    #   task_name: 'say_something_true', # this task does not exist
+    #   task_name: 'migrations:say_something_true', # this task does not exist
     #   arguments: ['balzac']
     # ).play_task_once
   end
