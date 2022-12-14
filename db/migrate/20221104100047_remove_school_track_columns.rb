32ebdf1bfa8cb00ac8518a65314ed34c2bbf7480
Brice <brice.durand2@gmail.com>
2022-12-14 13:17:46 +0100

    Correction d'une migration qui supprime le type school_track des classes

--- db/migrate/20221104100047_remove_school_track_columns.rb
+++ db/migrate/20221104100047_remove_school_track_columns.rb
@@ -5,21 +5,15 @@ class RemoveSchoolTrackColumns < ActiveRecord::Migration[7.0]
     remove_column :class_rooms, :school_track
     remove_column :internship_agreements, :school_track
     
-    execute <<-SQL
-      DROP TYPE class_room_school_track;
-    SQL
+    execute "DROP TYPE IF EXISTS class_room_school_track"
   end
 
 
   def down
-    execute <<-SQL
-      CREATE TYPE class_room_school_track AS ENUM('troisieme_generale','troisieme_prepa_metiers','troisieme_segpa','bac_pro');
-    SQL
-    
-    add_column :internship_offers, :school_track, :class_room_school_track, null: false, default: 'troisieme_generale'
-    add_column :internship_offer_infos, :school_track, :class_room_school_track, null: false, default: 'troisieme_generale'
-    add_column :internship_agreements, :school_track, :class_room_school_track, null: false,  default: 'troisieme_generale'
-    add_column :class_rooms, :school_track, :class_room_school_track, null: false, default: 'troisieme_generale'
+    add_column :internship_offers, :school_track, :string, null: false, default: 'troisieme_generale'
+    add_column :internship_offer_infos, :school_track, :string, null: false, default: 'troisieme_generale'
+    add_column :internship_agreements, :school_track, :string, null: false,  default: 'troisieme_generale'
+    add_column :class_rooms, :school_track, :string, null: false, default: 'troisieme_generale'
   end
  
 end
