9fdff34c6c8ded514eecec2aa9c138cc52d41b1d
Maxime Pierrot <maxime.pierrot@gmail.com>
2022-12-22 10:33:28 +0100

    Fix Save offers icons

--- app/front_assets/components/InternshipOfferFavorites.jsx
+++ app/front_assets/components/InternshipOfferFavorites.jsx
@@ -38,37 +38,30 @@ const InternshipOfferFavorites = ({ internshipOffers }) => {
           hideNotification={hideNotification} 
         />
       ) : '' }
-      <div className="row mx-0 fr-px-2w">
-        <div className={`${isMobile() ? 'col-12 px-0' : 'col-12 px-3'} d-flex`}>
-
-          <div className="fr-mt-2w">
-            <div className="row"> {/* Cards */}
-              { internshipOffers.length == 0 ? 
-                (
-                  <div className='col-12'>
-                    <h1>Vous n'avez aucune annonce sauvegardée.</h1>
-                    <p className="fr-py-1w">Pour en ajouter une, effectuez une recherche et cliquez sur le coeur pour enregistrer l'annonce qui vous intéresse.</p>
-                    <a class="fr-btn fr-raw-link fr-px-8w" href="/">Trouver un stage </a>
-                  </div>
-                ) : 
-                (
-                  internshipOffers.map((internshipOffer, i) => (
-                    <InternshipOfferCard
-                      internshipOffer={internshipOffer}
-                      key={internshipOffer.id}
-                      index={i}
-                      handleMouseOut={handleMouseOut}
-                      handleMouseOver={(value) => {handleMouseOver(value)}}
-                      sendNotification={(message) => {sendNotification(message)}}
-                      threeByRow
-                      />
-                  ))
-                )
-              }  
+      <div className="row mx-0 fr-px-2w fr-mt-2w">
+        {/* Cards */}
+        { internshipOffers.length == 0 ? 
+          (
+            <div className='col-12'>
+              <h1>Vous n'avez aucune annonce sauvegardée.</h1>
+              <p className="fr-py-1w">Pour en ajouter une, effectuez une recherche et cliquez sur le coeur pour enregistrer l'annonce qui vous intéresse.</p>
+              <a class="fr-btn fr-raw-link fr-px-8w" href="/internship_offers">Trouver un stage </a>
             </div>
-          </div>
-
-        </div>
+          ) : 
+          (
+            internshipOffers.map((internshipOffer, i) => (
+              <InternshipOfferCard
+                internshipOffer={internshipOffer}
+                key={internshipOffer.id}
+                index={i}
+                handleMouseOut={handleMouseOut}
+                handleMouseOver={(value) => {handleMouseOver(value)}}
+                sendNotification={(message) => {sendNotification(message)}}
+                threeByRow
+                />
+            ))
+          )
+        }
       </div>
     </div>
   );
