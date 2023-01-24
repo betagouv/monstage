07989b9ff558f9d793f10effb6af4db16a07405b
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-01-24 11:38:11 +0100

    Fix password reset

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -21,16 +21,16 @@ module Users
 
     def edit
       @current_user = User.with_reset_password_token(params['reset_password_token'])
-      @teacher = User.find(params['teacher_id'])
+      @teacher = User.find(params['teacher_id']) if params['teacher_id'].present?
       super
     end
 
     def update
       current_user = User.with_reset_password_token(params['user']['reset_password_token'])
-      if current_user
+      if current_user && current_user.created_by_teacher
         current_user.password = params['user']['password']
         current_user.save
-        current_user.confirm if current_user.created_by_teacher && current_user.confirmed_at.nil?
+        current_user.confirm if current_user.confirmed_at.nil?
         redirect_to new_user_session_path, flash: { success:  'Mot de passe enregistré !' }
       else
         super
