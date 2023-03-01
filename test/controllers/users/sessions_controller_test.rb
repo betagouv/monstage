275427c0aedcc536b919740d7f177b6801342d52
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-03-01 16:55:06 +0100

    Fix tests

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -30,7 +30,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     employer.confirm
     get new_user_session_path(params:{check_confirmation: true, id: employer.id})
     assert_response :success
-    assert_select('h1', text: 'Connexion à monstagedetroisième.fr')
+    assert_select('h1', text: 'Connexion')
   end
 
   test 'GET with prefilled email works' do
