59ca373114d7e1de8e4317763b23c5430e276b02
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-04-05 15:26:39 +0200

    Update sync script db

--- app/jobs/sync_production_databases_job.rb
+++ app/jobs/sync_production_databases_job.rb
@@ -8,7 +8,7 @@ class SyncProductionDatabasesJob < ApplicationJob
     system("pg_dump -Fc -d #{ENV['PRODUCTION_DATABASE_URI']} > ms3e.dump")
 
     Rails.logger.info("Updating production copy database...")
-    system("pg_restore --verbose --clean --no-acl --no-owner -d '#{ENV['COPY_DATABASE_URI']}' ms3e.dump")
+    system("pg_restore --create --clean --no-acl --no-owner -d '#{ENV['COPY_DATABASE_URI']}' ms3e.dump")
 
     Rails.logger.info("End")
     system("rm ms3e.dump")
