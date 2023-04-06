9e318dcded69edc814cbec0b0a445c745f50bcb1
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-04-06 11:09:32 +0200

    Rm drop before sync copy DB

--- app/jobs/sync_production_databases_job.rb
+++ app/jobs/sync_production_databases_job.rb
@@ -7,8 +7,8 @@ class SyncProductionDatabasesJob < ApplicationJob
     Rails.logger.info("Fetching production database...")
     system("pg_dump -Fc -d #{ENV['PRODUCTION_DATABASE_URI']} > ms3e.dump")
 
-    Rails.logger.info("Drop copy database...")
-    system("psql -d '#{ENV['COPY_DATABASE_URI']}' -c \"DROP SCHEMA public CASCADE; CREATE SCHEMA public;\"")
+    # Rails.logger.info("Drop copy database...")
+    # system("psql -d '#{ENV['COPY_DATABASE_URI']}' -c \"DROP SCHEMA public CASCADE; CREATE SCHEMA public;\"")
 
     Rails.logger.info("Updating production copy database...")
     system("pg_restore --create --clean --no-acl --no-owner -d '#{ENV['COPY_DATABASE_URI']}' ms3e.dump")
