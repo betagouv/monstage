6f838d877406559d4a73319fb2b3d9cb17c96fb1
Etienne Weil <weil.etienne@hotmail.fr>
2023-06-05 13:24:24 +0200

    fix(typo): emailj --> email

--- db/migrate/20230531094449_add_school_phone_to_schools.rb
+++ db/migrate/20230531094449_add_school_phone_to_schools.rb
@@ -2,6 +2,6 @@ class AddSchoolPhoneToSchools < ActiveRecord::Migration[7.0]
   def change
     add_column :schools, :fetched_school_phone, :string, limit: 20, null: true
     add_column :schools, :fetched_school_address, :string, limit: 300, null: true
-    add_column :schools, :fetched_school_emailj, :string, limit: 100, null: true
+    add_column :schools, :fetched_school_email, :string, limit: 100, null: true
   end
 end
