0ca1cd87fba2fe2a8c6577ee5f46cfc8ec875c21
Etienne Weil <weil.etienne@hotmail.fr>
2023-06-06 15:13:59 +0200

    fix(bug): method scope june_to_september now extended

--- app/libs/school_year/base.rb
+++ app/libs/school_year/base.rb
@@ -17,6 +17,18 @@ module SchoolYear
       end
     end
 
+    def january_to_may
+      1..MONTH_OF_YEAR_SHIFT
+    end
+
+    def june_to_august
+      6..8
+    end
+
+    def september_to_december
+      9..12
+    end
+
     # def between_june_to_august?
     #   june_to_august.member?(current_month)
     # end
@@ -41,17 +53,7 @@ module SchoolYear
       date.month
     end
 
-    def january_to_may
-      1..MONTH_OF_YEAR_SHIFT
-    end
 
-    def june_to_august
-      6..8
-    end
-
-    def september_to_december
-      9..12
-    end
 
     def last_week_of_may?
       last_day_of_may = Date.new(current_year, MONTH_OF_YEAR_SHIFT, DAY_OF_YEAR_SHIFT)
