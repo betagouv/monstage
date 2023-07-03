025f2cc0c33cb44adca901d862f06b51057cde75
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-07-03 15:24:15 +0200

    New home page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -167,7 +167,6 @@ class Ability
     can %i[supply_offers sign_with_sms choose_function subscribe_to_webinar] , User
     can :show, :account
 
-    can :see_minister_video, User
     can :create_remote_internship_request, SupportTicket # TO DO REMOVE
 
     can %i[create see_tutor], InternshipOffer
@@ -271,7 +270,6 @@ class Ability
             see_dashboard_administrations_summary
             see_dashboard_associations_summary], User
     can :manage, Operator
-    can :see_minister_video, User
   end
 
   def statistician_abilities(user:)
@@ -357,7 +355,6 @@ class Ability
            see_dashboard_administrations_summary], User
 
     as_employers_signatory_abilities(user: user) if user.employer_like?
-    can :see_minister_video, User
   end
 
   def common_school_management_abilities(user:)
