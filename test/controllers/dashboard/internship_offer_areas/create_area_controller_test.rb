a6733dd7cb2cc74c5cc9ce0e0719b302bb25b064
Etienne Weil <weil.etienne@hotmail.fr>
2023-08-07 17:54:55 +0200

    feat(test): move and increased test about areas

--- test/controllers/dashboard/internship_offer_areas/create_area_controller_test.rb
+++ test/controllers/dashboard/internship_offer_areas/create_area_controller_test.rb
@@ -44,6 +44,42 @@ module Dashboard
         assert_equal "User", area.employer_type
         assert_redirected_to edit_dashboard_internship_offer_area_path(area)
       end
+
+      test 'without team, employer cannot create an area with a name formerly employed' do
+        employer = create(:employer)
+        area_name = employer.current_area.name
+        sign_in(employer)
+
+        post  dashboard_internship_offer_areas_path(format: :turbo_stream),
+              params: { internship_offer_area: { name: area_name } }
+
+        assert_response :success
+        assert_select(
+          'p.fr-error-text',
+          text: "Nom d'espace : ce nom d'espace est déjà utilisé"
+        )
+      end
+
+      test 'when gathering in a team, formerly identical names are changed' do
+        employer_1 = create(:employer, first_name: "Jean", last_name: "Valjean")
+        employer_2 = create(:employer, first_name: "Bobby", last_name: "Lapointe")
+        formerly_common_name = employer_1.current_area.name
+        employer_2.current_area.update(name: formerly_common_name)
+        assert_equal formerly_common_name, employer_2.current_area.name
+        assert_equal formerly_common_name, employer_1.current_area.name
+        assert_equal 2, InternshipOfferArea.all.count
+        team_member_invitation = create(:team_member_invitation,
+                                        inviter: employer_1,
+                                        invitation_email: employer_2.email)
+
+        sign_in(employer_2)
+        patch join_dashboard_team_member_invitation_path( id: team_member_invitation.id),
+              params: { id: team_member_invitation.id, commit: "Oui" }
+        area_name = employer_1.current_area.reload.name
+        assert_equal "#{formerly_common_name}-J.V.", area_name
+        area_name = employer_2.current_area.reload.name
+        assert_equal "#{formerly_common_name}-B.L.", area_name
+      end
     end
   end
 end
