09f398268ee5bf45abbfe1dc13db0e85ff77f6ae
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-10-06 00:02:06 +0200

    Change read status application

--- app/libs/presenters/internship_application.rb
+++ app/libs/presenters/internship_application.rb
@@ -53,10 +53,11 @@ module Presenters
                     }]
         }
       when "read_by_employer"
+        label = reader.student? || reader.school_management? ? "Sans réponse de l'entreprise" : 'Lue'
         action_label = reader.student? || reader.school_management? ? 'Voir' : 'Répondre'
         action_level = reader.student? ? 'tertiary' : 'primary'
-        { label: "lue",
-          badge: 'warning',
+        { label: label,
+          badge: 'info',
           actions: [ { label: action_label,
                       path: internship_application_path,
                       level: action_level
