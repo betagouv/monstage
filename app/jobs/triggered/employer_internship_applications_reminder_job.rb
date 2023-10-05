f93c764735666dfec91b37579f74b15b76974e0c
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-10-05 16:19:22 +0200

    Employers global reminders

---
+++ app/jobs/triggered/employer_internship_applications_reminder_job.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Triggered
+  class EmployerInternshipApplicationsReminderJob < ApplicationJob
+    queue_as :batches
+
+    def perform(employer)
+      internship_applications = employer.internship_applications
+
+      pending_application_ids = internship_applications.pending.pluck(:id)
+      examined_application_ids = internship_applications.examined.pluck(:id)
+
+      if !pending_application_ids.empty? || !examined_application_ids.empty?
+        notify(employer: employer,
+                pending_application_ids: pending_application_ids,
+                examined_application_ids: examined_application_ids)
+      end
+    end
+
+    private  
+
+    def notify(employer:,
+                pending_application_ids:,
+                examined_application_ids:)
+      EmployerMailer.pending_internship_applications_reminder_email(employer: employer,
+                                                              pending_application_ids: pending_application_ids, 
+                                                              examined_application_ids: examined_application_ids)
+                    .deliver_now
+    end
+  end
+end
