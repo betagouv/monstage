aa064b65e24b27cd3b4a83b52dc37fa7d53ebe94
Etienne WEIL <fitchMitch@users.noreply.github.com>
2023-11-02 12:23:39 +0100

    MS3-877-feat(variable value change): EXPIRATION_DURATION (#1096)
    * fix(tests) : staging : constraints on common weeks (offer's/school weeks )now satisfied
    * feat(variable value change): EXPIRATION_DURATION

--- test/controllers/internship_applications/create_test.rb
+++ test/controllers/internship_applications/create_test.rb
@@ -138,30 +138,30 @@ module InternshipApplications
 
     # create internship application as student with class_room and check that counter are updated
     test 'POST #create internship application as student with greater max_candidates than hosting_info' do
-      internship_offer = create(:weekly_internship_offer, 
-        max_candidates: 3, 
-        max_students_per_group: 1, 
+      internship_offer = create(:weekly_internship_offer,
+        max_candidates: 3,
+        max_students_per_group: 1,
         weeks: Week.selectable_from_now_until_end_of_school_year.first(3))
       internship_offer.hosting_info.update(max_candidates: 3, max_students_per_group: 1, weeks: Week.selectable_from_now_until_end_of_school_year.first(3))
 
-      school = create(:school, weeks: [internship_offer.weeks.first])
+      school = create(:school, weeks: [internship_offer.weeks.first, internship_offer.weeks.last])
       class_room = create(:class_room, school: school)
       student_1 = create(:student, school: school, class_room: class_room)
       student_2 = create(:student, school: school, class_room: class_room)
-      
-      a1 = create(:weekly_internship_application, 
+
+      a1 = create(:weekly_internship_application,
         :approved,
         internship_offer: internship_offer,
         student: student_1,
         week: internship_offer.internship_offer_weeks.first.week
       )
-      
+
       InternshipOfferWeek.second.destroy
       # /!\ Now only 2 weeks are available for internship_offer, for 3 max_candidates
-      # Application should failed if Offer enough_weeks validation is not skipped
+      # Application should fail unless offer#enough_weeks validation is skipped as we expect
 
       sign_in(student_2)
-      
+
       valid_params = {
         internship_application: {
           week_id: internship_offer.internship_offer_weeks.last.week.id,
@@ -176,10 +176,10 @@ module InternshipApplications
             resume_other: 'resume_other',
             resume_languages: 'resume_languages'
           }
-        }   
+        }
       }
 
-      assert_difference('InternshipApplications::WeeklyFramed.count', 1) do
+      assert_difference('InternshipApplications::WeeklyFramed.count', 1) do # no failure since validation is not run
         post(internship_offer_internship_applications_path(internship_offer), params: valid_params)
         assert_redirected_to internship_offer_internship_application_path(
           internship_offer,
