55696371f97d6e9976dc218f488b0603c4a82f08
Etienne WEIL <fitchMitch@users.noreply.github.com>
2023-11-04 14:46:01 +0100

    MS3-883-fix(validation): removed validation for contact_phone, since internship_offer is updated with counters and invalidated as students apply ! (#1100)
    * fix(tests) : staging : constraints on common weeks (offer's/school weeks )now satisfied
    * fix(seed): adding contact_phone and moved application too
    * fix(validation): removed validation for contact_phone, since internship_offer is updated with counters and invalidated as students apply !

--- app/models/concerns/stepper_proxy/practical_info.rb
+++ app/models/concerns/stepper_proxy/practical_info.rb
@@ -8,14 +8,14 @@ module StepperProxy
       include Nearbyable
 
       # Validations
-      validates :contact_phone,
-                presence: true,
-                unless: :from_api?,
-                length: { minimum: 10 }
-      validates :contact_phone,
-                unless: :from_api?,
-                format: { with: /\A\+?[\d+\s]+\z/,
-                          message: 'Le numéro de téléphone doit contenir des caractères chiffrés uniquement' }
+      # validates :contact_phone,
+      #           presence: true,
+      #           unless: :from_api?,
+      #           length: { minimum: 10 }
+      # validates :contact_phone,
+      #           unless: :from_api?,
+      #           format: { with: /\A\+?[\d+\s]+\z/,
+      #                     message: 'Le numéro de téléphone doit contenir des caractères chiffrés uniquement' }
 
       # Relations
 
--- test/system/dashboard/stepper/manage_practical_infos_test.rb
+++ test/system/dashboard/stepper/manage_practical_infos_test.rb
@@ -34,25 +34,25 @@ class ManagePracticalInfosTest < ApplicationSystemTestCase
     end
   end
 
-  test 'cannot create PracticalInfos with badly formatted field' do
-    employer              = create(:employer)
-    organisation          = create(:organisation, employer: employer)
-    internship_offer_info = create(:weekly_internship_offer_info, employer: employer)
-    hosting_info          = create(:hosting_info, employer: employer)
-    sign_in(employer)
-    visit new_dashboard_stepper_practical_info_path(organisation_id: organisation.id, internship_offer_info_id: internship_offer_info.id, hosting_info_id: hosting_info.id)
-    find('span', text: 'Étape 4 sur 5')
-    find('h2', text: 'Informations pratiques')
-    fill_in_practical_infos_form
-    fill_in 'Votre numéro de téléphone de correspondance', with: '+330f623665555' # bad phone number
-    click_on "Suivant"
-    assert_equal 0, PracticalInfo.count
-    assert_equal 0, InternshipOffer.count
-    find "h2", text: "Informations pratiques"
-    within "fieldset.fr-mt-11v" do
-      find "legend", text: "Adresse du stage"
-    end
-    find "p#text-input-error-desc-error-contact_phone",
-         text: "Votre numéro de téléphone de correspondance : le numéro de téléphone doit contenir des caractères chiffrés uniquement"
-  end
+  # test 'cannot create PracticalInfos with badly formatted field' do
+  #   employer              = create(:employer)
+  #   organisation          = create(:organisation, employer: employer)
+  #   internship_offer_info = create(:weekly_internship_offer_info, employer: employer)
+  #   hosting_info          = create(:hosting_info, employer: employer)
+  #   sign_in(employer)
+  #   visit new_dashboard_stepper_practical_info_path(organisation_id: organisation.id, internship_offer_info_id: internship_offer_info.id, hosting_info_id: hosting_info.id)
+  #   find('span', text: 'Étape 4 sur 5')
+  #   find('h2', text: 'Informations pratiques')
+  #   fill_in_practical_infos_form
+  #   fill_in 'Votre numéro de téléphone de correspondance', with: '+330f623665555' # bad phone number
+  #   click_on "Suivant"
+  #   assert_equal 0, PracticalInfo.count
+  #   assert_equal 0, InternshipOffer.count
+  #   find "h2", text: "Informations pratiques"
+  #   within "fieldset.fr-mt-11v" do
+  #     find "legend", text: "Adresse du stage"
+  #   end
+  #   find "p#text-input-error-desc-error-contact_phone",
+  #        text: "Votre numéro de téléphone de correspondance : le numéro de téléphone doit contenir des caractères chiffrés uniquement"
+  # end
 end
