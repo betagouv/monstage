4b2727e24d385d69f00c8f9dbba1d34073d0b56f
Maxime Pierrot <maxime.pierrot@gmail.com>
2023-12-07 16:34:29 +0100

    Fix tests

--- test/controllers/navbar_test.rb
+++ test/controllers/navbar_test.rb
@@ -104,10 +104,9 @@ class NavbarTest < ActionDispatch::IntegrationTest
       student.presenter.default_internship_offers_path
     )
     assert_select('li a.fr-link.text-decoration-none.active', count: 1)
-    assert_select('li a.fr-link.text-decoration-none.active', text: student.dashboard_name, count: 1)
+    assert_select('li a.fr-link.text-decoration-none.active', text: "#{student.dashboard_name}0", count: 1)
     assert_select("li a.fr-link.mr-4", text: 'Accueil', count: 1)
     assert_select("li a.fr-link.mr-4", text: 'Recherche', count: 1)
-    assert_select("li a.fr-link.mr-4", text: 'Candidatures', count: 1)
     assert_select("li a.fr-link.mr-4", text: 'Mon profil', count: 1)
     assert_select("li a.fr-link.mr-4", text: 'Espaces', count: 0)
     assert_select("li a.fr-link.mr-4", text: 'équipe'.capitalize, count: 0)
