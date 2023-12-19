aa1dc3bc99cc08f2332e528311d4a3c9bc51e0f8
Etienne Weil <weil.etienne@hotmail.fr>
2023-12-19 17:25:16 +0100

    feat(Netsize as sms service provider): OVH replaced, service mutualized

---
+++ app/libs/services/sms_sender.rb
@@ -0,0 +1,77 @@
+module Services
+  class SmsSender
+    LINK_MOBILITY_SENDING_ENDPOINT_URL = "https://europe.ipx.com/restapi/v1/sms/send".freeze
+    def perform
+      response = get_request
+      if response.nil? || !response.respond_to?(:body)
+        Rails.logger.error("NetSize error: response is ko | phone_number: #{@phone_number} | content: #{@content}")
+        return nil
+      end
+      response_body = JSON.parse(response.body)
+      status?(0, response_body) ? log_success(response_body) : log_failure(response_body)
+    end
+
+    attr_reader :phone_number, :content , :sender_name, :user, :pass
+
+    private
+
+    def log_success(response_body)
+      info = "NetSize success for phone '#{@phone_number}', with content " \
+            "'#{@content}' | traceId: '#{response_body['traceId']}' | " \
+            "messageIds: '#{response_body['messageIds']}'"
+      Rails.logger.info(info)
+      true
+    end
+
+    def log_failure(response_body)
+      error_message = "NetSize error: '#{response_body['responseMessage']}', " \
+                      "with code #{response_body['responseCode']}, for phone" \
+                      " '#{@phone_number}', with content '#{@content}'"
+      Rails.logger.error(error_message)
+      false
+    end
+
+    def get_request
+      uri = get_request_uri
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri, default_headers)
+      http.request(request)
+    end
+
+    def get_request_uri
+      URI("#{LINK_MOBILITY_SENDING_ENDPOINT_URL}?#{making_body.to_query}")
+    end
+
+    def making_body
+      # maxConcatenatedMessages is 3 by default
+      {
+        destinationAddress: phone_number,
+        messageText: content,
+        username: user,
+        password: pass,
+        originatingAddress: sender_name,
+        originatorTON: 1 # 1 = Alphanumeric, 2 = Shortcode, 3 = MSISDN
+      }
+    end
+
+    #   # expected: Int|Array[Int],
+    #   # response: HttpResponse
+    def status?(expected, response_body)
+      actual = response_body['responseCode']&.to_i
+      Array(expected).include?(actual)
+    end
+
+    def default_headers
+      { 'Accept': 'application/json' }
+    end
+
+    def initialize(phone_number: , content: )
+      @phone_number = phone_number 
+      @content = content
+      @sender_name = 'Mon stage' # Max length: 16 chars
+      @user = ENV['LINK_MOBILITY_USER']
+      @pass = ENV['LINK_MOBILITY_SECRET']
+    end
+  end
+end
