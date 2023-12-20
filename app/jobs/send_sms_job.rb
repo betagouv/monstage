61f2adf19dfb33c6bccb58b199826ad0b89ecd06
Etienne Weil <weil.etienne@hotmail.fr>
2023-12-20 17:33:44 +0100

    fix(SendSmsJob signature call): using message instead of content

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -3,13 +3,13 @@
 class SendSmsJob < ApplicationJob
   queue_as :default
 
-  def perform(user: , content: , phone_number: nil)
+  def perform(user: , message: , phone_number: nil)
     return if phone_number.nil? && user.phone.blank?
-    return if content.blank? || content.length > 318
+    return if message.blank? || message.length > 318
 
     phone = phone_number || User.sanitize_mobile_phone_number(user.phone, '33')
 
-    Services::SmsSender.new(phone_number: phone, content: content)
+    Services::SmsSender.new(phone_number: phone, content: message)
                        .perform
   end
 end
