8b022b3041ea4beebb5230293bd70938c9f9537f
Etienne Weil <weil.etienne@hotmail.fr>
2024-01-09 11:52:40 +0100

    fix(ovh to disappear) : remaining uses isolated and removed

--- test/support/third_party_test_helpers.rb
+++ test/support/third_party_test_helpers.rb
@@ -13,17 +13,14 @@ module ThirdPartyTestHelpers
         to_return(status: 200, body: "", headers: {})
   end
 
-  def ovh_stub
-    stub_request(:post, "https://eu.api.ovh.com/1.0/sms/#{ENV['OVH_SMS_APPLICATION']}/jobs").
+  def sms_stub
+    stub_request(:get, "https://europe.ipx.com/restapi/v1/sms/send?destinationAddress=33606060606&messageText=Multipliez%20vos%20chances%20de%20trouver%20un%20stage%20!%20Envoyez%20au%20moins%203%20candidatures%20sur%20notre%20site%20:%20http://example.com/c/vwb94/o%20.%20L'%C3%A9quipe%20Mon%20stage%20de%20troisieme%20&originatingAddress=Mon%20stage&originatorTON=1&password=#{ENV.fetch("LINK_MOBILITY_SECRET")}&username=#{ENV.fetch("LINK_MOBILITY_SECRET")}").
       with(
-        body: "{\"message\":\"Vous êtes accepté à un stage. Confirmez votre présence : https://monstage-3e.fr/dashboard/students/1/internship_applications/1. L'équipe mon stage de troisième.\",\"noStopClause\":\"true\",\"receivers\":[\"+330612345678\"],\"sender\":\"MonStage3e\",\"validityPeriod\":2880}",
         headers: {
-          'Accept'=>'application/json',
-          'Content-Type'=>'application/json',
-          'X-Ovh-Application'=>'',
-          'X-Ovh-Consumer'=>'',
-          'X-Ovh-Signature'=>'',
-          'X-Ovh-Timestamp'=>''
+              'Accept'=>'application/json',
+              'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Host'=>'europe.ipx.com',
+              'User-Agent'=>'Ruby'
         }).
       to_return(status: 200, body: "", headers: {})
   end
