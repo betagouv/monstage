73343114bc7a5b1dd296b29aac4a2b883990b507
Etienne Weil <weil.etienne@hotmail.fr>
2024-01-12 12:31:41 +0100

    feat(campaignName) : campaign name of SMS now implemented as an option

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -3,13 +3,14 @@
 class SendSmsJob < ApplicationJob
   queue_as :default
 
-  def perform(user: , message: , phone_number: nil)
+  def perform(user: , message: , phone_number: nil, campaign_name: nil)
     return if phone_number.nil? && user.phone.blank?
     return if message.blank? || message.length > 318
+    return if campaign_name&.size&.to_i > 59
 
     phone = phone_number || User.sanitize_mobile_phone_number(user.phone, '33')
 
-    Services::SmsSender.new(phone_number: phone, content: message)
+    Services::SmsSender.new(phone_number: phone, content: message, campaign_name: campaign_name)
                        .perform
   end
 end
