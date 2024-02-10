fc6efa77665467559088a9c456a9714c91265df2
Max <max@MacBook-Pro-de-Max.local>
2024-02-10 15:33:52 +0100

    Fix API index discarded offers

--- test/controllers/api/internship_offers/index_test.rb
+++ test/controllers/api/internship_offers/index_test.rb
@@ -55,5 +55,40 @@ module Api
         assert_equal true, json_response['pagination']['isFirstPage']
       end
     end
+
+    test 'GET #index only returns operators offers not discarded' do
+      user = create(:user_operator, :with_current_area)
+
+      offer_1 = create(:api_internship_offer, employer: user, internship_offer_area_id: user.current_area.id)
+      offer_2 = create(:api_internship_offer, employer: user, internship_offer_area_id: user.current_area.id)
+
+
+      # Delete the first offer
+      delete api_internship_offer_path(
+        id: offer_1.remote_id,
+        params: {
+          token: "Bearer #{user.api_token}"
+        }
+      )
+      
+      offer_1.reload
+      assert offer_1.discarded?
+      
+      # Get only the second offer
+      documents_as(endpoint: :'internship_offers/index', state: :success) do
+        get api_internship_offers_path(
+          params: {
+            token: "Bearer #{user.api_token}",
+            page: 1
+          }
+        )
+
+        assert_response :success
+        assert_equal 1, json_response['internshipOffers'].count
+        assert_equal 1, json_response['pagination']['totalInternshipOffers']
+        assert_equal 1, json_response['pagination']['totalPages']
+        assert_equal true, json_response['pagination']['isFirstPage']
+      end
+    end
   end
 end
